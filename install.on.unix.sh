c6ae43b11710d8d3fca42bef5067c45245f98218
Martins Avots <martins.avots@splitcells.net>
2019-07-27 09:37:12 +0200

    Fix install script for base framework.
    Install script for dependent frameworks is still missing.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-# TODO This is an hack. The build process should create and executable installer file that contains all files.
+mvn clean install
 mkdir ~/bin/net.splitcells.os.state.interface.commands.managed
-find ~/bin/net.splitcells.os.state.interface.commands.managed  -maxdepth 1 -type f -delete
-chmod +x ./src/main/bash/user.commands.install
-find ./src/main -mindepth 1 -type d -exec ./src/main/bash/user.commands.install {} +
-chmod +x ~/bin/*
+	find ~/bin/net.splitcells.os.state.interface.commands.managed  -maxdepth 1 -type f -delete
+buildFolder=./target/classes
+	# TODO Define installation order.
+	installer=$buildFolder/src/main/bash/command/managed/command.managed.install
+	chmod +x $installer
+	find $buildFolder -mindepth 1 -type f -exec $installer {} \;
+	chmod +x ~/bin/*
 	chmod +x ~/bin/net.splitcells.os.state.interface.commands.managed/*
