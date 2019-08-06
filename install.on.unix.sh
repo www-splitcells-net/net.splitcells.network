b56ced6b284db091da61940228a64652b464ee41
Martins Avots <martins.avots@splitcells.net>
2019-08-06 22:50:59 +0200

    Fix creation of executable folder for managed commands.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn clean install
-mkdir ~/bin/net.splitcells.os.state.interface.commands.managed
+mkdir -p ~/bin/net.splitcells.os.state.interface.commands.managed
 	find ~/bin/net.splitcells.os.state.interface.commands.managed  -maxdepth 1 -type f -delete
 buildFolder=./target/classes
 	# TODO Define installation order.
