6117bfb2f65dbfd6c8d8debc6948b834d444906c
Martins Avots <martins.avots@splitcells.net>
2019-08-06 22:17:47 +0200

    Standardize folder structure.
    Move Python programs to own folder.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -4,7 +4,7 @@ mkdir ~/bin/net.splitcells.os.state.interface.commands.managed
 	find ~/bin/net.splitcells.os.state.interface.commands.managed  -maxdepth 1 -type f -delete
 buildFolder=./target/classes
 	# TODO Define installation order.
-	installer=$buildFolder/src/main/bash/command/managed/command.managed.install
+	installer=$buildFolder/src/main/python/command/managed/command.managed.install
 	chmod +x $installer
 	find $buildFolder -mindepth 1 -type f -exec $installer {} \;
 	chmod +x ~/bin/*
