849ae1d9b8aa0c4118d8f61c3c001ebdf644a154
Martins Avots <martins.avots@splitcells.net>
2019-08-08 00:06:16 +0200

    Register boostrap repo on first install.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+commandRepoRegister=./src/main/bash/command/managed/command.repository.register
+chmod +x $commandRepoRegister
+$commandRepoRegister $(pwd)
 installer=./src/main/bash/command/managed/command.repositories.install
 chmod +x $installer
 $installer
