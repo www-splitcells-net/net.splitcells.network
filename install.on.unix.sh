3811b14cf5f3d6c8ae23f3d309535f00e7b4ebc8
Martins Avots <martins.avots@splitcells.net>
2019-08-07 23:43:14 +0200

    Adapt standard license script to new config based installation.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-mvn clean install
-mkdir -p ~/bin/net.splitcells.os.state.interface.commands.managed
-	find ~/bin/net.splitcells.os.state.interface.commands.managed  -maxdepth 1 -type f -delete
-buildFolder=./target/classes
-	# TODO Define installation order.
-	installer=$buildFolder/src/main/python/command/managed/command.managed.install
-	chmod +x $installer
-	find $buildFolder -mindepth 1 -type f -exec $installer {} \;
-	chmod +x ~/bin/*
-	chmod +x ~/bin/net.splitcells.os.state.interface.commands.managed/*
+installer=./src/main/bash/command/managed/command.repositories.install
+chmod +x $installer
+$installer
