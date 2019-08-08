03360af115b1ada39053945a744afba8906f1ff5
Martins Avots <martins.avots@splitcells.net>
2019-08-08 11:18:53 +0200

    Fix errors regarding first setup.

--- doc/setup.md
+++ doc/setup.md
@@ -2,5 +2,7 @@
 
 ```sh
 git clone https://gitlab.com/splitcells/net.splitcells.os.state.interface.git
+cd net.splitcells.os.state.interface
+chmod +x ./install.on.unix.sh
 ./install.on.unix.sh
 ```
--- install.on.unix.sh
+++ install.on.unix.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+
+setupEnvironment=./src/main/bash/command/managed/command.managed.environment.configure
+chmod +x $setupEnvironment
+$setupEnvironment
+
 commandRepoRegister=./src/main/bash/command/managed/command.repository.register
 chmod +x $commandRepoRegister
 $commandRepoRegister $(pwd)
+
 installer=./src/main/bash/command/managed/command.repositories.install
 chmod +x $installer
 $installer
+
+echo You may need to restart the computer in order to access the installed programs.
+echo At the very least you need to open a new terminal in order to access these programs.
