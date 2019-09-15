ce6464b1ed66472071048e7bd94db2e4ecc22bd5
Martins Avots <martins.avots@splitcells.net>
2019-09-15 18:08:17 +0200

    Improve echo of after first installation.

--- install.on.unix.sh
+++ install.on.unix.sh
@@ -8,5 +8,10 @@ installer=./src/main/bash/command/managed/command.repositories.install
 chmod +x $installer
 $installer
 
-echo You may need to restart the computer in order to access the installed programs.
+echo
+	# We create a empty line in order to visually separate the execution output from the result output.
+echo OS state interface installed.
+echo
+echo Execute "'". ~/.profile"'" in order to access the installed programs on this terminal.
+echo You may need to restart the computer in order to access the installed programs without this command on new terminals.
 echo At the very least you need to open a new terminal in order to access these programs.
