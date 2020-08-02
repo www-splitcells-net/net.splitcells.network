643f789e2839d46f093c9fa99c78f818ad3ad9c7
Martins Avots <martins.avots@splitcells.net>
2020-08-02 14:19:09 +0200

    Add new set methods.

--- src/main/java/net/splitcells/dem/data/set/Set.java
+++ src/main/java/net/splitcells/dem/data/set/Set.java
@@ -2,41 +2,48 @@ package net.splitcells.dem.data.set;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 import static java.util.Arrays.asList;
 
 public interface Set<T> extends java.util.Set<T> {
 
-	@Deprecated
-	default boolean add(T e) {
-		if (contains(e)) {
-			throw new IllegalArgumentException("Element " + e + " already present in " + this);
-		}
-		ensureContains(e);
-		return true;
-	}
+    @Deprecated
+    default boolean add(T e) {
+        if (contains(e)) {
+            throw new IllegalArgumentException("Element " + e + " already present in " + this);
+        }
+        ensureContains(e);
+        return true;
+    }
 
-	void ensureContains(T e);
+    void ensureContains(T e);
 
-	default void addAll(T... objects) {
-		Arrays.stream(objects).forEach(e -> add(e));
-	}
+    default void addAll(T... objects) {
+        Arrays.stream(objects).forEach(e -> add(e));
+    }
 
-	default boolean containsAny(T... objects) {
-		boolean rVal = false;
-		final var containment = Arrays.stream(objects)
-				.map(e -> contains(e))
-				.reduce((a,b) -> a || b);
-		return rVal || containment.orElse(false);
-	}
+    default boolean containsAny(T... objects) {
+        boolean rVal = false;
+        final var containment = Arrays.stream(objects)
+                .map(e -> contains(e))
+                .reduce((a, b) -> a || b);
+        return rVal || containment.orElse(false);
+    }
 
-	default Set<T> with(T... args) {
-		addAll(asList(args));
-		return this;
-	}
+    default Set<T> with(T... args) {
+        addAll(asList(args));
+        return this;
+    }
 
-	default Set<T> with(Collection<T> args) {
-		addAll(args);
-		return this;
-	}
+    default Set<T> with(Collection<T> args) {
+        addAll(args);
+        return this;
+    }
+
+    default void delete(T arg) {
+        if (!remove(arg)) {
+            throw new IllegalArgumentException("" + arg);
+        }
+    }
 }
--- src/main/java/net/splitcells/dem/data/set/SetT.java
+++ src/main/java/net/splitcells/dem/data/set/SetT.java
@@ -2,14 +2,18 @@ package net.splitcells.dem.data.set;
 
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.data.set.list.Lists;
+import org.assertj.core.api.Assertions;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public interface SetT<T> extends Collection<T> {
     default <R> List<R> mapped(Function<T, R> mapper) {
@@ -26,5 +30,20 @@ public interface SetT<T> extends Collection<T> {
 
     default Optional<T> reduced(BinaryOperator<T> accumulator) {
         return stream().reduce(accumulator);
+    }
+
+    default boolean hasDuplicates() {
+        final java.util.Set<T> uniques = new HashSet<>();
+        for(T e : this) {
+            if(uniques.contains(e)) {
+                return true;
+            }
+            uniques.add(e);
+        }
+        return false;
+    }
+    
+    default void requireUniqueness() {
+        assertThat(!this.hasDuplicates());
     }
 }
