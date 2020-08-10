7b5efb40eec6c332b8a6f76e34d56994e75d88f0
Martins Avots <martins.avots@splitcells.net>
2020-08-10 23:08:21 +0200

    Fix definition of 'Configuration.process'.

--- src/main/java/net/splitcells/dem/environment/config/framework/Configuration.java
+++ src/main/java/net/splitcells/dem/environment/config/framework/Configuration.java
@@ -3,6 +3,7 @@ package net.splitcells.dem.environment.config.framework;
 import net.splitcells.dem.environment.resource.Resource;
 import net.splitcells.dem.lang.annotations.Private;
 import net.splitcells.dem.lang.annotations.Returns_this;
+import net.splitcells.dem.resource.communication.Closeable;
 import net.splitcells.dem.resource.communication.Flushable;
 
 import java.util.Map;
@@ -19,5 +20,12 @@ public interface Configuration extends ConfigurationV {
 
     <T> void subscribe(Class<? extends Option<T>> option, @Private BiConsumer<Object, Object> consumer);
 
+    /**
+     * Process a certain type of resource values.
+     *
+     * @param type Type of resource values, that will be processed.
+     * @param processor Function that replaces current resource values.
+     * @param <T> Type of resource values, that will be processed.
+     */
     <T> void process(Class<? extends T> type, Function<T, T> processor);
 }
