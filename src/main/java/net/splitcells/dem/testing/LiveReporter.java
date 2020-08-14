0126c82a4451f1525742294bb23ee2ac25494790
Martins Avots <martins.avots@splitcells.net>
2020-08-14 22:17:42 +0200

    Log exception cause during test.

--- src/main/java/net/splitcells/dem/testing/LiveReporter.java
+++ src/main/java/net/splitcells/dem/testing/LiveReporter.java
@@ -32,6 +32,9 @@ public class LiveReporter implements TestExecutionListener {
         } else {
             if (testExecutionResult.getThrowable().isPresent()) {
                 testExecutionResult.getThrowable().get().printStackTrace();
+                if (testExecutionResult.getThrowable().get().getCause() != null) {
+                    testExecutionResult.getThrowable().get().getCause().printStackTrace();
+                }
             }
             System.out.println("Failed: " + testIdentifier.getUniqueId());
         }
