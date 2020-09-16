99a530520658964a2fe548cdb6b92c8b32d33f10
Martins Avots <martins.avots@splitcells.net>
2020-09-16 23:19:40 +0200

    Veido nejaušo inicializētāju.

---
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/primitīvs/NejaušsInicializētājs.java
@@ -0,0 +1,37 @@
+package net.splitcells.gel.kodols.atrisinājums.optimizācija.primitīvs;
+
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.utils.random.Randomness;
+import net.splitcells.gel.kodols.atrisinājums.AtrisinājumaSkats;
+import net.splitcells.gel.kodols.atrisinājums.optimizācija.Optimizācija;
+import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijasNotikums;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.utils.random.RandomnessSource.randomness;
+import static net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijasNotikums.optimizacijasNotikums;
+import static net.splitcells.gel.kodols.atrisinājums.optimizācija.SoluTips.PIEŠĶIRŠANA;
+
+public class NejaušsInicializētājs implements Optimizācija {
+    public static NejaušsInicializētājs nejaušsInicialiyētājs() {
+        return new NejaušsInicializētājs(randomness());
+    }
+
+    private final Randomness nejaušiba;
+
+    private NejaušsInicializētājs(Randomness nejaušiba) {
+        this.nejaušiba = nejaušiba;
+    }
+
+    @Override
+    public List<OptimizācijasNotikums> optimizē(AtrisinājumaSkats atrisinājums) {
+        if (atrisinājums.prasības_nelietotas().navTukšs() && atrisinājums.piedāvājums_nelietots().navTukšs()) {
+            return list(
+                    optimizacijasNotikums
+                            (PIEŠĶIRŠANA
+                                    , nejaušiba.chooseOneOf(atrisinājums.prasības_nelietotas().gūtRindas()).uzRindaRādītājs()
+                                    , nejaušiba.chooseOneOf(atrisinājums.piedāvājums_nelietots().gūtRindas()).uzRindaRādītājs()));
+
+        }
+        return list();
+    }
+}
