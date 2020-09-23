5eb8ccc0a091009de161821fee984c619e4c994c
Martins Avots <martins.avots@splitcells.net>
2020-09-23 21:28:24 +0200

    Do not filter tests by default.

--- src/main/java/net/splitcells/dem/testing/Test.java
+++ src/main/java/net/splitcells/dem/testing/Test.java
@@ -11,13 +11,19 @@ import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNa
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectMethod;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
+/**
+ * Executes any test by default.
+ * This does not filter tests by names like Maven by default.
+ * If a developer wrote a test, than it most likely wants to also execute the test, if it is not stated otherwise.
+ * Tests are also not filtered, because the developer might not know, that such a thing happens.
+ * He needs at least a warning for such things during test execution.
+ */
 public class Test {
     public static void main(String... arg) {
         System.setProperty("net.splitcells.mode.build", "true");
         Dem.ensuredInitialized();
         final var testDiscovery = LauncherDiscoveryRequestBuilder.request()
                 .selectors(selectPackage(""))
-                .filters(includeClassNamePatterns(".*Test"))
                 .build();
         final var testExecutor = LauncherFactory.create();
         final var failureDetector = failureDetector();
@@ -32,7 +38,6 @@ public class Test {
         Dem.ensuredInitialized();
         final var testDiscovery = LauncherDiscoveryRequestBuilder.request()
                 .selectors(selectMethod(type, methodeName))
-                .filters(includeClassNamePatterns(".*Test"))
                 .build();
         final var testExecutor = LauncherFactory.create();
         final var failureDetector = failureDetector();
