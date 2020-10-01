a80f9a882a0dccd2474c5e23f7feb8c9175ee7b9
Martins Avots <martins.avots@splitcells.net>
2020-10-01 22:48:32 +0200

    Īsteno eskalācijas struktūru.

--- src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
@@ -2,14 +2,16 @@ package net.splitcells.gel.kodols.atrisinājums.optimizācija.refleksija;
 
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.gel.kodols.atrisinājums.AtrisinājumaSkats;
+import net.splitcells.gel.kodols.atrisinājums.Atrisinājums;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.Optimizācija;
+import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijaTiešsaistē;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijasNotikums;
 
 import java.util.function.Function;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
 
-public class Eskalācija implements Optimizācija {
+public class Eskalācija implements OptimizācijaTiešsaistē {
 
     public static Eskalācija eskalācija(Function<Integer, Optimizācija> optimzācijas) {
         return new Eskalācija(optimzācijas);
@@ -23,14 +25,21 @@ public class Eskalācija implements Optimizācija {
     }
 
     @Override
-    public List<OptimizācijasNotikums> optimizē(AtrisinājumaSkats atrisinājums) {
+    public List<OptimizācijasNotikums> optimizē(Atrisinājums atrisinājums) {
         final var saknesNovērtejums = atrisinājums.ierobežojums().novērtējums();
         final var sanknesVēsturesIndekss = atrisinājums.vēsture().momentansIndekss();
         if (eskalācijasLīmens < 0) {
             return list();
         }
         final var optimizācija = optimzācijas.apply(eskalācijasLīmens).optimizē(atrisinājums);
-        eskalācijasLīmens -= 1;
+        atrisinājums.optimizē(optimizācija);
+        final var momentansNovērtējums = atrisinājums.ierobežojums().novērtējums();
+        atrisinājums.vēsture().atiestatUz(sanknesVēsturesIndekss);
+        if (momentansNovērtējums.labākNekā(saknesNovērtejums)) {
+            eskalācijasLīmens += 1;
+        } else {
+            eskalācijasLīmens -= 1;
+        }
         return optimizācija;
     }
 }
