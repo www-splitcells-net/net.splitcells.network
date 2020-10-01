f47197877116778dedb5244c3eb036272080b815
Martins Avots <martins.avots@splitcells.net>
2020-10-01 22:13:27 +0200

    Vienkāršo paku optimizācijas struktūru.

--- src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
@@ -1,18 +1,13 @@
-package net.splitcells.gel.kodols.atrisinājums.optimizācija.agregāts;
+package net.splitcells.gel.kodols.atrisinājums.optimizācija.refleksija;
 
 import net.splitcells.dem.data.set.list.List;
-import net.splitcells.dem.resource.host.interaction.LogLevel;
 import net.splitcells.gel.kodols.atrisinājums.AtrisinājumaSkats;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.Optimizācija;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijasNotikums;
-import net.splitcells.gel.kodols.novērtējums.struktūra.Novērtējums;
 
-import java.util.Optional;
 import java.util.function.Function;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
-import static net.splitcells.dem.environment.config.StaticFlags.TRACING;
-import static net.splitcells.dem.resource.host.interaction.Domsole.domsole;
 
 public class Eskalācija implements Optimizācija {
 
