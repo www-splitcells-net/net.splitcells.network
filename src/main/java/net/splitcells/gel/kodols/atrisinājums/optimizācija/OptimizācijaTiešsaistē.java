8fb486d5182a6bf7489e7f1e103ed605bc0be43a
Martins Avots <martins.avots@splitcells.net>
2020-10-01 22:42:43 +0200

    Noveco mainīgu interfeiss.

--- src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/OptimizācijaTiešsaistē.java
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/OptimizācijaTiešsaistē.java
@@ -3,6 +3,7 @@ package net.splitcells.gel.kodols.atrisinājums.optimizācija;
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.gel.kodols.atrisinājums.Atrisinājums;
 
+@Deprecated
 @FunctionalInterface
 public interface OptimizācijaTiešsaistē {
 
