f7e9b0137c2ac4c4055c5af013f1ffa66e2e7ba0
Martins Avots <martins.avots@splitcells.net>
2020-10-05 00:16:45 +0200

    Integrē optimizācijas interfeissus.

--- src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/refleksija/Eskalācija.java
@@ -7,6 +7,7 @@ import net.splitcells.gel.kodols.atrisinājums.optimizācija.Optimizācija;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijaTiešsaistē;
 import net.splitcells.gel.kodols.atrisinājums.optimizācija.OptimizācijasNotikums;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
@@ -14,14 +15,22 @@ import static net.splitcells.dem.data.set.list.Lists.list;
 public class Eskalācija implements OptimizācijaTiešsaistē {
 
     public static Eskalācija eskalācija(Function<Integer, Optimizācija> optimzācijas) {
-        return new Eskalācija(optimzācijas);
+        return new Eskalācija(Optional.of(optimzācijas), Optional.empty());
     }
 
-    private final Function<Integer, Optimizācija> optimzācijas;
+    public static Eskalācija eskalācija2(Function<Integer, OptimizācijaTiešsaistē> optimzācijas) {
+        return new Eskalācija(Optional.empty(), Optional.of(optimzācijas));
+    }
+
+    private final Optional<Function<Integer, Optimizācija>> optimzācijas;
+    private final Optional<Function<Integer, OptimizācijaTiešsaistē>> optimzācijas2;
     private int eskalācijasLīmens = 0;
 
-    private Eskalācija(Function<Integer, Optimizācija> optimzācijas) {
+    private Eskalācija
+            (Optional<Function<Integer, Optimizācija>> optimzācijas
+                    , Optional<Function<Integer, OptimizācijaTiešsaistē>> optimzācijas2) {
         this.optimzācijas = optimzācijas;
+        this.optimzācijas2 = optimzācijas2;
     }
 
     @Override
@@ -31,7 +40,14 @@ public class Eskalācija implements OptimizācijaTiešsaistē {
         if (eskalācijasLīmens < 0) {
             return list();
         }
-        final var optimizācija = optimzācijas.apply(eskalācijasLīmens).optimizē(atrisinājums);
+        final List<OptimizācijasNotikums> optimizācija;
+        if (optimzācijas.isPresent()) {
+            optimizācija = optimzācijas.get().apply(eskalācijasLīmens).optimizē(atrisinājums);
+        } else if (optimzācijas2.isPresent()) {
+            optimizācija = optimzācijas2.get().apply(eskalācijasLīmens).optimizē(atrisinājums);
+        } else {
+            throw new IllegalStateException();
+        }
         atrisinājums.optimizē(optimizācija);
         final var momentansNovērtējums = atrisinājums.ierobežojums().novērtējums();
         atrisinājums.vēsture().atiestatUz(sanknesVēsturesIndekss);
