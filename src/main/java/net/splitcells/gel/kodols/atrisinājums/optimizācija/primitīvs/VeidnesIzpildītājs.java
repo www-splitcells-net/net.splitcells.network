2b326d0c7a398cb2a9dde68d6d5fd5ec7d9b7def
Martins Avots <martins.avots@splitcells.net>
2020-10-11 23:02:23 +0200

    Veido strukturu no VeidnesIzpildītājs.

--- src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/primitīvs/VeidnesIzpildītājs.java
+++ src/main/java/net/splitcells/gel/kodols/atrisinājums/optimizācija/primitīvs/VeidnesIzpildītājs.java
@@ -8,17 +8,22 @@ import net.splitcells.gel.kodols.dati.tabula.Tabula;
 
 import java.nio.file.Path;
 
+import static net.splitcells.dem.data.set.list.Lists.list;
+
 public class VeidnesIzpildītājs implements Optimizācija {
     public static VeidnesIzpildītājs veidnesIzpildītājs(Tabula veidne) {
         return new VeidnesIzpildītājs(veidne);
     }
 
-    private VeidnesIzpildītājs(Tabula veidne) {
+    private final Tabula veidne;
 
+    private VeidnesIzpildītājs(Tabula veidne) {
+        this.veidne = veidne;
     }
 
     @Override
     public List<OptimizācijasNotikums> optimizē(AtrisinājumaSkats atrisinājums) {
-        return null;
+        final List<OptimizācijasNotikums> optimicaija = list();
+        return optimicaija;
     }
 }
