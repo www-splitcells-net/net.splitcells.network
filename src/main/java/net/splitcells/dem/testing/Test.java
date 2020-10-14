54949c9df33bfe09ee49f545fc78bce58b007d19
Martins Avots <martins.avots@splitcells.net>
2020-10-14 21:29:42 +0200

    Create method to test a class.

--- src/main/java/net/splitcells/dem/testing/Test.java
+++ src/main/java/net/splitcells/dem/testing/Test.java
@@ -8,8 +8,7 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import static net.splitcells.dem.testing.FailureDetector.failureDetector;
 import static net.splitcells.dem.testing.LiveReporter.liveReporter;
 import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectMethod;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
 
 /**
  * Executes any test by default.
@@ -17,6 +16,8 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPacka
  * If a developer wrote a test, than it most likely wants to also execute the test, if it is not stated otherwise.
  * Tests are also not filtered, because the developer might not know, that such a thing happens.
  * He needs at least a warning for such things during test execution.
+ *
+ * TODO Simplify code by removing duplicate code.
  */
 public class Test {
     public static void main(String... arg) {
@@ -30,6 +31,19 @@ public class Test {
         Dem.ensuredInitialized();
         final var testDiscovery = LauncherDiscoveryRequestBuilder.request()
                 .selectors(selectPackage(""))
+                .build();
+        final var testExecutor = LauncherFactory.create();
+        final var failureDetector = failureDetector();
+        testExecutor.discover(testDiscovery);
+        testExecutor.execute(testDiscovery, liveReporter(), failureDetector);
+        return !failureDetector.hasWatchedErrors();
+    }
+
+    public static boolean test(Class<?> type) {
+        System.setProperty("net.splitcells.mode.build", "true");
+        Dem.ensuredInitialized();
+        final var testDiscovery = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(type))
                 .build();
         final var testExecutor = LauncherFactory.create();
         final var failureDetector = failureDetector();
