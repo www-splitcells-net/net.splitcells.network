16211de68f08b7bf4331e49eda843428e3f91950
Martins Avots <martins.avots@splitcells.net>
2020-10-19 22:17:28 +0200

    Document way XML is currently not replaced internally by Java.

--- src/main/java/net/splitcells/dem/lang/perspective/PerspectiveDocument.java
+++ src/main/java/net/splitcells/dem/lang/perspective/PerspectiveDocument.java
@@ -7,6 +7,17 @@ import net.splitcells.dem.object.Discoverable;
 import static net.splitcells.dem.data.set.list.Lists.list;
 
 /**
+ * Instances of this class represent the content of XML/JSON like files.
+ * XSD Schema functionality can be created via special classes, that wrap perspectives.
+ * <p>
+ * This currently not used. Real XML files are used instead, because XML provides things in
+ * order support needed functionality: it supports namespaces, XSL, XSD and XPATH.
+ * The only downside is, that defining schemas with XSD and validating files with these schemas is complicated
+ * and costly. Especially the integration of XSD files into editors is not as easy as one might think.
+ * The best way to solve this, is to automatically apply schema validation any time that XML files are processed.
+ * <p>
+ *     This class will probably be a replacement for XML, if it does cause more problems.
+ * <p>
  * Extending classes should always provide an empty constructor,
  * because this is just data and not something like a template.
  */
