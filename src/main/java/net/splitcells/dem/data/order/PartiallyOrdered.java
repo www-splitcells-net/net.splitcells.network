3715bdfdcb60fb282de20eb91cee902c49e6a367
Martins Avots <martins.avots@splitcells.net>
2021-01-29 00:23:58 +0100

    Update code guidelines.

--- src/main/java/net/splitcells/dem/data/order/PartiallyOrdered.java
+++ src/main/java/net/splitcells/dem/data/order/PartiallyOrdered.java
@@ -4,63 +4,62 @@ import java.util.Optional;
 
 /**
  * TODO Is equal to https://en.wikipedia.org/wiki/Partially_ordered_set
- * 
- * @author splitcells
  *
  * @param <T>
+ * @author splitcells
  */
 @FunctionalInterface
 public interface PartiallyOrdered<T> extends OrderingCheck<T> {
 
-	Optional<Ordering> compare_partially_to(T arg);
+    Optional<Ordering> compare_partially_to(T arg);
 
-	@Override
-	default boolean smallerThan(T other) {
-		var rBase = compare_partially_to(other);
-		if (!rBase.isPresent()) {
-			return false;
-		}
-		return rBase.get().equals(Ordering.LESSER_THAN);
-	}
+    @Override
+    default boolean smallerThan(T other) {
+        var rBase = compare_partially_to(other);
+        if (!rBase.isPresent()) {
+            return false;
+        }
+        return rBase.get().equals(Ordering.LESSER_THAN);
+    }
 
-	@Override
-	default boolean smallerThanOrEqual(T other) {
-		var rBase = compare_partially_to(other);
-		if (!rBase.isPresent()) {
-			return false;
-		}
-		return rBase.get().equals(Ordering.LESSER_THAN) || rBase.get().equals(Ordering.EQUAL);
-	}
+    @Override
+    default boolean smallerThanOrEqual(T other) {
+        var rBase = compare_partially_to(other);
+        if (!rBase.isPresent()) {
+            return false;
+        }
+        return rBase.get().equals(Ordering.LESSER_THAN) || rBase.get().equals(Ordering.EQUAL);
+    }
 
-	@Override
-	default boolean greaterThan(T other) {
-		var rBase = compare_partially_to(other);
-		if (!rBase.isPresent()) {
-			return false;
-		}
-		return rBase.get().equals(Ordering.GREATER_THAN);
-	}
+    @Override
+    default boolean greaterThan(T other) {
+        var rBase = compare_partially_to(other);
+        if (!rBase.isPresent()) {
+            return false;
+        }
+        return rBase.get().equals(Ordering.GREATER_THAN);
+    }
+	
+    @Override
+    default boolean greaterThanOrEqual(T other) {
+        var rBase = compare_partially_to(other);
+        if (!rBase.isPresent()) {
+            return false;
+        }
+        return rBase.get().equals(Ordering.GREATER_THAN) || rBase.get().equals(Ordering.EQUAL);
+    }
 
-	@Override
-	default boolean greaterThanOrEqual(T other) {
-		var rBase = compare_partially_to(other);
-		if (!rBase.isPresent()) {
-			return false;
-		}
-		return rBase.get().equals(Ordering.GREATER_THAN) || rBase.get().equals(Ordering.EQUAL);
-	}
-
-	/**
-	 * RENAME
-	 * 
-	 * @return
-	 */
-	default boolean equalz(T other) {
-		var rBase = compare_partially_to(other);
-		if (!rBase.isPresent()) {
-			return false;
-		}
-		return rBase.get().equals(Ordering.EQUAL);
-	}
+    /**
+     * RENAME
+     *
+     * @return
+     */
+    default boolean equalz(T other) {
+        var rBase = compare_partially_to(other);
+        if (!rBase.isPresent()) {
+            return false;
+        }
+        return rBase.get().equals(Ordering.EQUAL);
+    }
 
 }
