9df08039705cca1418f64d3f4cb7a60e4e615974
Martins Avots <martins.avots@splitcells.net>
2021-03-20 23:28:20 +0100

    '14 Remove not required scope.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/Server.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/Server.java
@@ -65,54 +65,52 @@ public class Server {
     }
 
     public void serveAsAuthenticatedHttpsAt(int port, Function<String, RenderingResult> renderer) {
-        {
-            System.setProperty("vertx.disableFileCaching", "true");
-            System.setProperty("log4j.rootLogger", "DEBUG, stdout");
-            Vertx vertx = Vertx.vertx();
-            vertx.deployVerticle(new AbstractVerticle() {
-                @Override
-                public void start() {
-                    // TODO Errors are not logged.
-                    final var webServerOptions = new HttpServerOptions()//
-                            .setSsl(true)//
-                            .setKeyCertOptions(new PfxOptions().setPath("src/main/resources.private/keystore.p12").setPassword("password"))//
-                            .setTrustOptions(new PfxOptions().setPath("src/main/resources.private/keystore.p12").setPassword("password"))//
-                            .setClientAuth(ClientAuth.REQUIRED)//
-                            .setLogActivity(true)//
-                            .setPort(port);//
-                    final var router = Router.router(vertx);
-                    router.route("/favicon.ico").handler(a -> {
+        System.setProperty("vertx.disableFileCaching", "true");
+        System.setProperty("log4j.rootLogger", "DEBUG, stdout");
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new AbstractVerticle() {
+            @Override
+            public void start() {
+                // TODO Errors are not logged.
+                final var webServerOptions = new HttpServerOptions()//
+                        .setSsl(true)//
+                        .setKeyCertOptions(new PfxOptions().setPath("src/main/resources.private/keystore.p12").setPassword("password"))//
+                        .setTrustOptions(new PfxOptions().setPath("src/main/resources.private/keystore.p12").setPassword("password"))//
+                        .setClientAuth(ClientAuth.REQUIRED)//
+                        .setLogActivity(true)//
+                        .setPort(port);//
+                final var router = Router.router(vertx);
+                router.route("/favicon.ico").handler(a -> {
+                });
+                router.route("/*").handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    vertx.<byte[]>executeBlocking((promise) -> {
+                        final String requestPath;
+                        if ("".equals(routingContext.request().path()) || "/".equals(routingContext.request().path())) {
+                            requestPath = "index.html";
+                        } else {
+                            requestPath = routingContext.request().path();
+                        }
+                        final var result = renderer.apply(requestPath);
+                        response.putHeader("content-type", result.getFormat());
+                        promise.complete(result.getContent());
+                    }, (result) -> {
+                        if (result.failed()) {
+                            result.cause().printStackTrace();
+                            response.setStatusCode(500);
+                            response.end();
+                        } else {
+                            response.end(Buffer.buffer().appendBytes(result.result()));
+                        }
                     });
-                    router.route("/*").handler(routingContext -> {
-                        HttpServerResponse response = routingContext.response();
-                        vertx.<byte[]>executeBlocking((promise) -> {
-                            final String requestPath;
-                            if ("".equals(routingContext.request().path()) || "/".equals(routingContext.request().path())) {
-                                requestPath = "index.html";
-                            } else {
-                                requestPath = routingContext.request().path();
-                            }
-                            final var result = renderer.apply(requestPath);
-                            response.putHeader("content-type", result.getFormat());
-                            promise.complete(result.getContent());
-                        }, (result) -> {
-                            if (result.failed()) {
-                                result.cause().printStackTrace();
-                                response.setStatusCode(500);
-                                response.end();
-                            } else {
-                                response.end(Buffer.buffer().appendBytes(result.result()));
-                            }
-                        });
-                    });
-                    router.errorHandler(500, e -> {
-                        e.failure().printStackTrace();
-                    });
-                    final var server = vertx.createHttpServer(webServerOptions);//
-                    server.requestHandler(router);//
-                    server.listen();
-                }
-            });
-        }
+                });
+                router.errorHandler(500, e -> {
+                    e.failure().printStackTrace();
+                });
+                final var server = vertx.createHttpServer(webServerOptions);//
+                server.requestHandler(router);//
+                server.listen();
+            }
+        });
     }
 }
