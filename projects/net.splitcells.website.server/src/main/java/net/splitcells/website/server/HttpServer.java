787952004637110551791256d0af6041399dd3f6
Martins Avots <martins.avots@splitcells.net>
2021-03-20 23:26:17 +0100

    #14 Create HTTP serve method.

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/HttpServer.java
@@ -0,0 +1,16 @@
+package net.splitcells.website.server;
+
+import net.splitcells.dem.resource.host.Files;
+
+import java.nio.file.Paths;
+
+import static net.splitcells.website.server.renderer.ProjectsRenderer.publicProjectsRenderer;
+
+public class HttpServer {
+    public static void main(String... args) {
+        final var target = Paths.get("target/public").toAbsolutePath();
+        Files.createDirectory(target);
+        publicProjectsRenderer().serveToHttpAt(8443, true);
+        publicProjectsRenderer().serveToHttpAt(8080, false);
+    }
+}
