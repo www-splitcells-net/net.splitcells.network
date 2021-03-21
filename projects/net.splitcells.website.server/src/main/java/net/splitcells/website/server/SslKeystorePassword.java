7f70d541bd5c18ebc2549a18ec482452d6e1d071
Martins Avots <martins.avots@splitcells.net>
2021-03-21 03:02:59 +0100

    #14 Deprecate options.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/SslKeystorePassword.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/SslKeystorePassword.java
@@ -5,6 +5,10 @@ import net.splitcells.dem.environment.config.framework.OptionI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * TODO Make this an program argument.
+ */
+@Deprecated
 public class SslKeystorePassword extends OptionI<String> {
     public SslKeystorePassword() {
         super(() -> "password");
--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/StaticFileServer.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/StaticFileServer.java
@@ -6,6 +6,10 @@ import java.nio.file.Paths;
 
 import static net.splitcells.website.Projects.projectsRenderer;
 
+/**
+ * TODO Make this an program argument.
+ */
+@Deprecated
 public class StaticFileServer {
     public static void main(String... args) {
         final var target = Paths.get("target/public").toAbsolutePath();
