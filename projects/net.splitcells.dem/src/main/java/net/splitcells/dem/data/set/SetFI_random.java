a5b92dfad3366f74d03d0d004eea937a114589b3
Martins Avots <martins.avots@splitcells.net>
2021-04-08 23:51:09 +0200

    #7 Implement determinstic set builder.

--- projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/set/SetFI_random.java
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/set/SetFI_random.java
@@ -3,27 +3,28 @@ package net.splitcells.dem.data.set;
 import net.splitcells.dem.utils.Not_implemented_yet;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public class SetFI_random implements SetF {
 
-	@Override
-	public <T> Set<T> lagacySet() {
-		throw new Not_implemented_yet();
-	}
+    @Override
+    public <T> Set<T> lagacySet() {
+        return new HashSet<>();
+    }
 
-	@Override
-	public <T> Set<T> legacySet(Collection<T> arg) {
-		throw new Not_implemented_yet();
-	}
+    @Override
+    public <T> Set<T> legacySet(Collection<T> arg) {
+        return new HashSet<>(arg);
+    }
 
-	@Override
-	public <T> net.splitcells.dem.data.set.Set<T> set() {
-		throw new Not_implemented_yet();
-	}
+    @Override
+    public <T> net.splitcells.dem.data.set.Set<T> set() {
+        return SetI.make();
+    }
 
-	@Override
-	public <T> net.splitcells.dem.data.set.Set<T> set(Collection<T> arg) {
-		throw new Not_implemented_yet();
-	}
+    @Override
+    public <T> net.splitcells.dem.data.set.Set<T> set(Collection<T> arg) {
+        return SetI.make().with(arg);
+    }
 }
