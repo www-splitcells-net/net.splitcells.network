bb3d1b5733e33e69ac903ea692e385bb4adf7062
Martins Avots <martins.avots@splitcells.net>
2021-04-10 01:28:30 +0200

    #37 Implement legacy map wrapper.

--- projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/set/map/MapLegacyWrapper.java
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/set/map/MapLegacyWrapper.java
@@ -5,71 +5,73 @@ import java.util.Set;
 
 public class MapLegacyWrapper<Key, Value> implements Map<Key, Value> {
 
-    public static <Key, Value> Map<Key, Value> mapLegacyWrapper(java.util.Map<Key, Value> map) {
-        return new MapLegacyWrapper();
+    public static <Key, Value> Map<Key, Value> mapLegacyWrapper(java.util.Map<Key, Value> content) {
+        return new MapLegacyWrapper(content);
     }
 
-    private MapLegacyWrapper() {
+    private final java.util.Map<Key, Value> content;
 
+    private MapLegacyWrapper(java.util.Map<Key, Value> content) {
+        this.content = content;
     }
 
     @Override
     public int size() {
-        return 0;
+        return content.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return content.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        return content.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return content.containsValue(value);
     }
 
     @Override
     public Value get(Object key) {
-        return null;
+        return content.get(key);
     }
 
     @Override
     public Value put(Key key, Value value) {
-        return null;
+        return content.put(key, value);
     }
 
     @Override
     public Value remove(Object key) {
-        return null;
+        return content.remove(key);
     }
 
     @Override
     public void putAll(java.util.Map<? extends Key, ? extends Value> m) {
-
+        content.putAll(m);
     }
 
     @Override
     public void clear() {
-
+        content.clear();
     }
 
     @Override
     public Set<Key> keySet() {
-        return null;
+        return content.keySet();
     }
 
     @Override
     public Collection<Value> values() {
-        return null;
+        return content.values();
     }
 
     @Override
     public Set<Entry<Key, Value>> entrySet() {
-        return null;
+        return content.entrySet();
     }
 }
