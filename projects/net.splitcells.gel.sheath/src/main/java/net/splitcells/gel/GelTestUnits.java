c20d77b53f4455b526b5b57dbdc9aa234a61a449
Martins Avots <martins.avots@splitcells.net>
2021-04-11 03:46:52 +0200

    #46 Disable integration tests by default,
    because of performance requirementrs for such tests.

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/GelTestUnits.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/GelTestUnits.java
@@ -4,6 +4,8 @@ import net.splitcells.dem.Dem;
 import net.splitcells.dem.lang.Xml;
 import net.splitcells.dem.resource.host.Files;
 import net.splitcells.dem.resource.host.ProcessPath;
+import net.splitcells.dem.resource.host.interaction.MessageFilter;
+import net.splitcells.dem.testing.Test;
 import net.splitcells.gel.solution.SolutionAspect;
 import net.splitcells.gel.solution.Solutions;
 import net.splitcells.gel.solution.history.Histories;
@@ -13,6 +15,7 @@ import static net.splitcells.dem.Dem.environment;
 import static net.splitcells.dem.lang.namespace.NameSpaces.SEW;
 import static net.splitcells.dem.resource.host.Files.writeToFile;
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
+import static net.splitcells.gel.GelEnv.standardConfigurator;
 
 public final class GelTest {
     private GelTest() {
@@ -20,6 +23,13 @@ public final class GelTest {
     }
 
     public static void main(String... arg) {
-        GelEnv.process(() -> net.splitcells.dem.testing.Test.main());
+        GelEnv.process(() -> {
+                    if (Test.testIntegration()) {
+                        System.exit(1);
+                    }
+                }
+                , standardConfigurator().andThen(env -> {
+                    env.config().withConfigValue(MessageFilter.class, a -> false);
+                }));
     }
 }
