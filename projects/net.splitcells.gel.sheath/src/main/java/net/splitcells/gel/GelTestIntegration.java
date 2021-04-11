f62a5f2329a0e0627f600258ff49e16cb73d41bc
Martins Avots <martins.avots@splitcells.net>
2021-04-11 03:53:09 +0200

    #46 Support testing of any type combinations.

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/GelTestIntegration.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/GelTestIntegration.java
@@ -1,21 +1,24 @@
 package net.splitcells.gel;
 
+import net.splitcells.dem.resource.Paths;
+import net.splitcells.dem.resource.host.ProcessHostPath;
+import net.splitcells.dem.resource.host.interaction.IsEchoToFile;
 import net.splitcells.dem.resource.host.interaction.MessageFilter;
 import net.splitcells.dem.testing.Test;
 
-import static net.splitcells.dem.resource.host.Files.writeToFile;
-import static net.splitcells.dem.testing.Test.testUnits;
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.testing.Test.testIntegration;
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
 import static net.splitcells.gel.GelEnv.standardConfigurator;
 
-public final class GelTestUnits {
-    private GelTestUnits() {
+public final class GelTestIntegration {
+    private GelTestIntegration() {
         throw constructorIllegal();
     }
 
     public static void main(String... arg) {
         GelEnv.process(() -> {
-                    if (testUnits()) {
+                    if (testIntegration()) {
                         System.exit(1);
                     }
                 }
