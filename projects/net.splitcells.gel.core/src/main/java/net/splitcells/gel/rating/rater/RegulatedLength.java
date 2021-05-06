c48b0ff0429b7a98bc5a66e4c0ae286cd5488a4f
Martins Avots <martinsavots@hotmail.de>
2021-05-06 23:40:01 +0200

    #8 Improve constraint overview.

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/RegulatedLength.java
@@ -0,0 +1,32 @@
+package net.splitcells.gel.rating.rater;
+
+import net.splitcells.gel.data.table.attribute.Attribute;
+
+import static net.splitcells.dem.utils.MathUtils.distance;
+import static net.splitcells.dem.utils.Not_implemented_yet.not_implemented_yet;
+import static net.splitcells.gel.rating.rater.RaterBasedOnLineGroup.groupRater;
+import static net.splitcells.gel.rating.type.Cost.cost;
+
+public class RegulatedLength {
+    private RegulatedLength() {
+        throw not_implemented_yet();
+    }
+
+    public static Rater regulatedLength(Attribute<Integer> targetLength, Attribute<Integer> lengthElement) {
+        return groupRater((lines, addition, removal) -> {
+            final int requiredLength = addition
+                    .map(e -> e.value(targetLength))
+                    .orElseGet(() -> removal.get().value(targetLength));
+            final var currentLength = lines.getLines()
+                    .stream()
+                    .filter(e -> removal.map(line -> e.index() != line.index()).orElse(true))
+                    .map(line -> line.value(lengthElement))
+                    .reduce(Integer::sum)
+                    .map(sum -> sum + addition.map(a -> a.value(lengthElement)).orElse(0))
+                    .orElse(0);
+            final var totalCost = distance(requiredLength, currentLength);
+            return addition.map(a -> cost(totalCost / (lines.getLines().size())))
+                    .orElseGet(() -> cost(totalCost / (lines.getLines().size() - 1)));
+        });
+    }
+}
--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/test/functionality/SchoolSchedulingTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/test/functionality/SchoolSchedulingTest.java
@@ -1,21 +1,11 @@
 package net.splitcells.gel.test.functionality;
 
-import net.splitcells.dem.data.set.list.List;
-import net.splitcells.dem.lang.dom.Domable;
-import net.splitcells.dem.object.Discoverable;
-import net.splitcells.dem.utils.MathUtils;
-import net.splitcells.gel.constraint.Constraint;
-import net.splitcells.gel.data.table.Line;
-import net.splitcells.gel.data.table.Table;
 import net.splitcells.gel.data.table.attribute.Attribute;
 import net.splitcells.gel.problem.Problem;
-import net.splitcells.gel.rating.rater.Rater;
-import net.splitcells.gel.rating.rater.RatingEvent;
+import net.splitcells.gel.rating.rater.RegulatedLength;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collection;
 
 import static net.splitcells.dem.testing.TestTypes.INTEGRATION_TEST;
 import static net.splitcells.dem.utils.MathUtils.distance;
@@ -23,10 +13,7 @@ import static net.splitcells.dem.utils.Not_implemented_yet.not_implemented_yet;
 import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 import static net.splitcells.gel.rating.rater.AllDifferent.allDifferent;
 import static net.splitcells.gel.rating.rater.AllSame.allSame;
-import static net.splitcells.gel.rating.rater.RaterBasedOnLineGroup.groupRater;
 import static net.splitcells.gel.rating.rater.RaterBasedOnLineValue.*;
-import static net.splitcells.gel.rating.rater.RatingEventI.ratingEvent;
-import static net.splitcells.gel.rating.structure.LocalRatingI.localRating;
 import static net.splitcells.gel.rating.type.Cost.cost;
 import static net.splitcells.gel.solution.SolutionBuilder.defineProblem;
 import static org.assertj.core.api.Assertions.fail;
@@ -85,22 +72,7 @@ public class SchoolSchedulingTest {
                     r.forAll(SCHOOL_SUBJECT)
                             .forAll(lineValueSelector(line -> line.value(RAIL) != 0))
                             .then(allDifferent(RAIL));
-                    r.forAll(COURSE_ID)
-                            .then(groupRater((lines, addition, removal) -> {
-                                final int requiredHours = addition
-                                        .map(e -> e.value(REQUIRED_HOURS))
-                                        .orElseGet(() -> removal.get().value(REQUIRED_HOURS));
-                                final var allocatedHours = lines.getLines()
-                                        .stream()
-                                        .filter(e -> removal.map(line -> e.index() != line.index()).orElse(true))
-                                        .map(line -> line.value(ALLOCATED_HOURS))
-                                        .reduce(Integer::sum)
-                                        .map(sum -> sum + addition.map(a -> a.value(ALLOCATED_HOURS)).orElse(0))
-                                        .orElse(0);
-                                final var totalCost = distance(requiredHours, allocatedHours);
-                                return addition.map(a -> cost(totalCost / (lines.getLines().size())))
-                                        .orElseGet(() -> cost(totalCost / (lines.getLines().size() - 1)));
-                            }));
+                    r.forAll(COURSE_ID).then(RegulatedLength.regulatedLength(REQUIRED_HOURS, ALLOCATED_HOURS));
                     r.forAll(RAIL).then(allSame(ALLOCATED_HOURS));
                     return r;
                 }).toProblem();
