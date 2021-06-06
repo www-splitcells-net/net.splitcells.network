685647e78bee31e6cb56f1266dfbe99f40144de3
Martins Avots <martinsavots@hotmail.de>
2021-06-07 00:21:24 +0200

    Add inspirational quote to documentation.

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/Optimization.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/Optimization.java
@@ -3,6 +3,9 @@ package net.splitcells.gel.solution.optimization;
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.gel.solution.SolutionView;
 
+/**
+ * Perfect is the enemy of good -Voltaire
+ */
 @FunctionalInterface
 public interface Optimization {
 
