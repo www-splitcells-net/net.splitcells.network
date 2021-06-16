ae878b75be2543a354f5aac25156281852aceb94
Martins Avots <martinsavots@hotmail.de>
2021-06-16 20:17:58 +0200

    #66 Implement layout merging.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/extension/ExtensionMerger.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/extension/ExtensionMerger.java
@@ -1,13 +1,17 @@
 package net.splitcells.website.server.renderer.extension;
 
 import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.dem.lang.perspective.Perspective;
 import net.splitcells.website.server.renderer.ProjectRenderer;
 import net.splitcells.website.server.renderer.RenderingResult;
 
+import java.nio.file.Path;
 import java.util.Optional;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.website.server.renderer.ProjectRenderer.extendPerspectiveWithPath;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExtensionMerger implements ProjectRendererExtension {
     public static ExtensionMerger extensionMerger() {
@@ -22,12 +26,12 @@ public class ExtensionMerger implements ProjectRendererExtension {
 
     @Override
     public Optional<RenderingResult> renderFile(String path, ProjectRenderer projectRenderer) {
-        return Optional.empty();
     }
 
     @Override
     public Perspective extendProjectLayout(Perspective layout, ProjectRenderer projectRenderer) {
-        return null;
+        extensions.forEach(e -> e.extendProjectLayout(layout, projectRenderer));
+        return layout;
     }
 
     public void registerExtension(ProjectRendererExtension extension) {
