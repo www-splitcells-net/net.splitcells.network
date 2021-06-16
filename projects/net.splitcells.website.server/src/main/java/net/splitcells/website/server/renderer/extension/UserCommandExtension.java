6a6e4450052b9680b285330d53e8e62010fe4c0b
Martins Avots <martinsavots@hotmail.de>
2021-06-16 21:02:55 +0200

    #66 Define content.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/extension/UserCommandExtension.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/extension/UserCommandExtension.java
@@ -1,15 +1,19 @@
 package net.splitcells.website.server.renderer.extension;
 
+import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.dem.lang.namespace.NameSpaces;
 import net.splitcells.dem.lang.perspective.Perspective;
 import net.splitcells.dem.resource.host.Files;
+import net.splitcells.website.server.renderer.LayoutRenderer;
 import net.splitcells.website.server.renderer.ProjectRenderer;
 import net.splitcells.website.server.renderer.RenderingResult;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
 import static io.vertx.core.http.HttpHeaders.TEXT_HTML;
+import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
 import static net.splitcells.dem.lang.perspective.PerspectiveI.perspective;
 import static net.splitcells.dem.resource.Paths.userHome;
 import static net.splitcells.website.server.renderer.RenderingResult.renderingResult;
@@ -30,6 +34,14 @@ public class UserCommandExtension implements ProjectRendererExtension {
     public Optional<RenderingResult> renderFile(String path, ProjectRenderer projectRenderer) {
         if (RENDERING_PATH.equals(path)) {
             final var layout = perspective(NameSpaces.VAL, NameSpaces.NATURAL);
+            try {
+                java.nio.file.Files.walk(BIN_FOLDER).forEach(command ->
+                        LayoutRenderer.extend(layout
+                                , listWithValuesOf(command.getFileName().toString().split("\\.")))
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             return projectRenderer.renderString(layout.toString())
                     .map(r -> renderingResult(r, TEXT_HTML.toString()));
         }
