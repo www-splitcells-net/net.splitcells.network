c166520ec00746712250ca68c4ca23c8eebd38a6
Martins Avots <martinsavots@hotmail.de>
2021-06-22 23:28:48 +0200

    #8 Fix RegulatedLength.

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/RegulatedLength.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/RegulatedLength.java
@@ -1,9 +1,11 @@
 package net.splitcells.gel.rating.rater;
 
+import net.splitcells.gel.constraint.Constraint;
 import net.splitcells.gel.data.table.attribute.Attribute;
 
 import static net.splitcells.dem.utils.MathUtils.distance;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
+import static net.splitcells.gel.constraint.Constraint.LINE;
 import static net.splitcells.gel.rating.rater.RaterBasedOnLineGroup.groupRater;
 import static net.splitcells.gel.rating.type.Cost.cost;
 
@@ -15,14 +17,14 @@ public class RegulatedLength {
     public static Rater regulatedLength(Attribute<Integer> targetLength, Attribute<Integer> lengthElement) {
         return groupRater((lines, addition, removal) -> {
             final int requiredLength = addition
-                    .map(e -> e.value(targetLength))
-                    .orElseGet(() -> removal.get().value(targetLength));
+                    .map(e -> e.value(LINE).value(targetLength))
+                    .orElseGet(() -> removal.get().value(LINE).value(targetLength));
             final var currentLength = lines.getLines()
                     .stream()
                     .filter(e -> removal.map(line -> e.index() != line.index()).orElse(true))
-                    .map(line -> line.value(lengthElement))
+                    .map(line -> line.value(LINE).value(lengthElement))
                     .reduce(Integer::sum)
-                    .map(sum -> sum + addition.map(a -> a.value(lengthElement)).orElse(0))
+                    .map(sum -> sum + addition.map(a -> a.value(LINE).value(lengthElement)).orElse(0))
                     .orElse(0);
             final var totalCost = distance(requiredLength, currentLength);
             return addition.map(a -> cost(totalCost / (lines.getLines().size())))
