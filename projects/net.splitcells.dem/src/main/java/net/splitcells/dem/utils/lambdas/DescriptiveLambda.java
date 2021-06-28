296bebc9a0f497147475e53c25900d6ee324277f
Mārtiņš Avots <martins.avots@splitcells.net>
2021-06-29 01:13:26 +0200

    #8 Get lambda constructor with description.

---
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/utils/lambdas/DescriptiveLambda.java
@@ -0,0 +1,26 @@
+package net.splitcells.dem.utils.lambdas;
+
+import java.util.function.Predicate;
+
+import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
+
+public class DescriptiveLambda {
+    private DescriptiveLambda() {
+        throw constructorIllegal();
+    }
+
+    public <T> Predicate<T> describedPredicate(Predicate<T> predicate, String description) {
+        return new Predicate<>() {
+
+            @Override
+            public boolean test(T t) {
+                return predicate.test(t);
+            }
+
+            @Override
+            public String toString() {
+                return description;
+            }
+        };
+    }
+}
