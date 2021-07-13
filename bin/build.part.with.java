0e734b58118d4a2613a7bdd162a703da57ba65c4
Mārtiņš Avots <martins.avots@splitcells.net>
2021-07-13 22:09:58 +0200

    Document build command.

--- bin/build.part.with.java
+++ bin/build.part.with.java
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+# This command exists, so that the project can be build with one command.
+# This makes usage of tools building this project easier,
+# as it can be hard to understand in such tools,
+# how to chain commands calls.
 set -e
 cd projects/net.splitcells.system
 ./bin/build.part.with.java
