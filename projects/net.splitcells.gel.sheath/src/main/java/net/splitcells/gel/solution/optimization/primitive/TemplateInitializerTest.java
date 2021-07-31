863df8877ce4bbfae31e000a3e6a17853eb87160
Mārtiņš Avots <martins.avots@splitcells.net>
2021-07-31 12:07:17 +0200

    #2 Add copyright headers for gel.sheath.

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/solution/optimization/primitive/TemplateInitializerTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/solution/optimization/primitive/TemplateInitializerTest.java
@@ -48,6 +48,22 @@ public class TemplateInitializerTest {
         assertThat(testProduct.getLines().get(0).values()).isEqualTo(list(2, 1));
         assertThat(testProduct.getLines().get(1).values()).isEqualTo(list(2, 1));
         assertThat(testProduct.getLines().get(2).values()).isEqualTo(list(1, 2));
+        /*
+         * Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+         *
+         * This program and the accompanying materials are made available under the
+         * terms of the Eclipse Public License 2.0, which is available at
+         * http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+         * which is available at https://spdx.org/licenses/MIT.html.
+         *
+         * This Source Code may also be made available under the following Secondary
+         * Licenses when the conditions for such availability set forth in the Eclipse
+         * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+         * or any later versions with the GNU Classpath Exception which is
+         * available at https://www.gnu.org/software/classpath/license.html.
+         *
+         * SPDX-License-Identifier: EPL-2.0 OR MIT OR GPL-2.0-or-later WITH Classpath-exception-2.0
+         */
         assertThat(testProduct.getLines().get(3).values()).isEqualTo(list(1, 2));
         assertThat(testProduct.getLines().get(4).values()).isEqualTo(list(1, 1));
         assertThat(testProduct.getLines().get(5).values()).isEqualTo(list(1, 1));
