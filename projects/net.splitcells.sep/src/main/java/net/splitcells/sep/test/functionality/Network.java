4ec3ab54c59a1230d04cace5eff80a5739776e1c
Mārtiņš Avots <martins.avots@splitcells.net>
2021-09-30 20:33:41 +0200

    #s6 Register nodes with another node as a dependency.

--- projects/net.splitcells.sep/src/main/java/net/splitcells/sep/test/functionality/Network.java
+++ projects/net.splitcells.sep/src/main/java/net/splitcells/sep/test/functionality/Network.java
@@ -4,6 +4,8 @@ import net.splitcells.dem.data.set.map.Map;
 import net.splitcells.dem.data.set.map.Maps;
 import net.splitcells.gel.solution.Solution;
 
+import java.util.function.Function;
+
 import static net.splitcells.dem.data.set.map.Maps.map;
 
 public class Network {
@@ -16,12 +18,20 @@ public class Network {
     private Network() {
 
     }
-    
+
     public Network withNode(String key, Solution solution) {
         if (solutions.containsKey(key)) {
             throw new IllegalArgumentException(key);
         }
         solutions.put(key, solution);
+        return this;
+    }
+
+    public Network withNode(String key, Function<Solution, Solution> constructor, String dependencyKey) {
+        if (solutions.containsKey(key)) {
+            throw new IllegalArgumentException(key);
+        }
+        withNode(key, constructor.apply(solutions.get(dependencyKey)));
         return this;
     }
 }
