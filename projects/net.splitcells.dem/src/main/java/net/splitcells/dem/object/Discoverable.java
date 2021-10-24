b2b7c24e569920cc01a50a53887568c1c4bc9301
Mārtiņš Avots <martins.avots@splitcells.net>
2021-10-24 23:13:34 +0200

    #10 Remove task,
    that cannot be done meaningfully.

--- projects/net.splitcells.dem/src/main/java/net/splitcells/dem/object/Discoverable.java
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/object/Discoverable.java
@@ -16,9 +16,6 @@ import static net.splitcells.dem.data.set.list.Lists.list;
 
 public interface Discoverable {
 
-    /**
-     * RENAME Find better name.
-     */
     Discoverable NO_CONTEXT = () -> list();
 
     static Discoverable discoverable(List<String> path) {
