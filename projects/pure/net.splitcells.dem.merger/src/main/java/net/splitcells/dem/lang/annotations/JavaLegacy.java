d5f3bcbaf27d3dd1961f8fa0670fcd1f5e5c5db4
Mārtiņš Avots <martins.avots@splitcells.net>
2021-10-24 23:21:39 +0200

    #10 Move annotations to pure project.

--- projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/lang/annotations/JavaLegacy.java
+++ projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/lang/annotations/JavaLegacy.java
@@ -10,13 +10,9 @@
  */
 package net.splitcells.dem.lang.annotations;
 
+/**
+ * Marks code that is only present for Java compatibility.
+ */
 public @interface JavaLegacy {
-	/**
-	 * Marks code that is present because of insufficiencies of the Java programming
-	 * language, This is important in order to support the porting of this library.
-	 * This annotation should not be used on code related to abstract side effect
-	 * management.
-	 * 
-	 * TODO When to use {@link JavaLegacy}.
-	 */
+
 }
--- projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/lang/annotations/Secret.java
+++ projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/lang/annotations/Secret.java
@@ -11,18 +11,18 @@
 package net.splitcells.dem.lang.annotations;
 
 /**
- * This values is only used for documentation purposes.
+ * This is only used for documentation purposes.
  * <p/>
  * Values marked with {@link Secret}, have security relevance and can be seen as private keys,
  * in order to get additional access.
- * These values should not be passed around outside of their context.
+ * These values should not be passed around outside their context.
  * <p/>
  * This could be something like a password, where only a limited part of code should have access to such a password.
  * <p/>
  * In publisher and subscriber models, the identity of a subscriber can be seen as a private key,
  * in order to remove an existing subscriber from its subscription.
- * Such a system can require the identity of the subscriber as an prove, that
- * an accessor has the right to remove the subscription from the subscriber.
+ * Such a system can require the identity of the subscriber as a prove,
+ * that an accessor has the right to remove the subscription from the subscriber.
  * In such a case the subscriber value can also be marked as {@link Secret}.
  */
 public @interface Secret {
