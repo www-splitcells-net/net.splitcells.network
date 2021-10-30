9e441224b76b22e4b28e1404c24ae261376dffaf
Mārtiņš Avots <martins.avots@splitcells.net>
2021-10-30 02:08:47 +0200

    #10 Move exception to pure project.

--- projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/atom/Bools.java
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/data/atom/Bools.java
@@ -10,8 +10,6 @@
  */
 package net.splitcells.dem.data.atom;
 
-import org.assertj.core.api.Assert;
-
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
 
 public class Bools {
---
+++ projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/utils/ConstructorIllegal.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+ * which is available at https://spdx.org/licenses/MIT.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ */
+package net.splitcells.dem.utils;
+
+public final class ConstructorIllegal extends AssertionError {
+
+	public static ConstructorIllegal constructorIllegal() {
+		return new ConstructorIllegal();
+	}
+
+	@Deprecated
+	public ConstructorIllegal() {
+	}
+}
