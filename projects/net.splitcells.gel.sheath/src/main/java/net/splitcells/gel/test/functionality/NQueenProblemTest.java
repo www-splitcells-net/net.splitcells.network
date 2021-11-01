7134bf45586ce3198343aee14a7f58f17bf16939
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-01 21:04:25 +0100

    Format code.

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/test/functionality/NQueenProblemTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/test/functionality/NQueenProblemTest.java
@@ -79,8 +79,8 @@ public class NQueenProblemTest extends TestSuiteI {
         // The temperature functions was determined by trial and error with universal allocation program's temperature functions.
         testSubject.optimize(linearInitialization());
         SimplifiedAnnealingProblem.simplifiedAnnealingProblem(testSubject,
-                i -> max((float) (log(4.0) / pow(log(i + 3), 15))
-                        , 0))
+                        i -> max((float) (log(4.0) / pow(log(i + 3), 15))
+                                , 0))
                 .optimizeOnce(functionalHillClimber(
                         linearIterator(
                                 list(
@@ -116,8 +116,7 @@ public class NQueenProblemTest extends TestSuiteI {
                             r.forAll(ascDiagonals(rows, columns)).then(hasSize(1));
                             r.forAll(descDiagonals(rows, columns)).then(hasSize(1));
                             return r;
-                        }
-                )
+                        })
                 .toProblem();
     }
 
