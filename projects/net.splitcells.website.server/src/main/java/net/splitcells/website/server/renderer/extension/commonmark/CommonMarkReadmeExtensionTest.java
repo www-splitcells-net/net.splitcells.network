e24d2cd032b9baa1eac9d2505dfb45fd8d1515ae
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-13 12:50:35 +0100

    #23 Create README test.

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/extension/commonmark/CommonMarkReadmeExtensionTest.java
@@ -0,0 +1,26 @@
+package net.splitcells.website.server.renderer.extension.commonmark;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static net.splitcells.dem.testing.TestTypes.INTEGRATION_TEST;
+import static net.splitcells.website.server.renderer.ProjectRenderer.projectRenderer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommonMarkReadmeExtensionTest {
+    @Tag(INTEGRATION_TEST)
+    @Test
+    public void testReadMeLayout() {
+        final var testSubject = projectRenderer
+                ("test"
+                        , Path.of("../net.splitcells.dem")
+                        , Path.of("../net.splitcells.website.default.content/src/main/xsl/net/splitcells/website/den/translation/to/html/")
+                        , Path.of("net.splitcells.website.default.content/src/main/resources/html")
+                        , "/net/splitcells/dem"
+                        , a -> Optional.empty());
+        assertThat(testSubject.projectPaths()).contains(Path.of("net/splitcells/dem/README.html"));
+    }
+}
