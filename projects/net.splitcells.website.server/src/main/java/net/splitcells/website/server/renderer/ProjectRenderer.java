f3d703b1e5c0341ac941801fb56d5f1f984ce375
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-20 00:16:49 +0100

    #23 Document project renderer idea.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/ProjectRenderer.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/renderer/ProjectRenderer.java
@@ -26,6 +26,14 @@ import static net.splitcells.dem.lang.namespace.NameSpaces.STRING;
 import static net.splitcells.dem.lang.perspective.PerspectiveI.perspective;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+/**
+ * Renders a folder, that is interpreted as a folder containing a project.
+ * This can also be a build system, where a source code project is compiled to deployable artifact.
+ *
+ * This is often used to provide a virtual path system, where file paths relative to the project's root folder
+ * are mapped to a set of path's.
+ * These paths can be queried or projected to another filesystem.
+ */
 public interface ProjectRenderer extends Renderer {
 
     static ProjectRenderer projectRenderer(String renderer, Path projectFolder, Path xslLibs, Path resources
