9ab977a50009c8d27846fe7ea23f866d93b6025e
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-21 22:06:58 +0100

    #22 Define online optimization interface.

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/OnlineOptimization.java
@@ -0,0 +1,12 @@
+package net.splitcells.gel.solution.optimization;
+
+import net.splitcells.gel.solution.Solution;
+
+/**
+ * <p>What benefit is there in reviewing past sufferings, and in being unhappy, just because once you were unhappy?
+ * - Seneca</p>
+ */
+@FunctionalInterface
+public interface OnlineOptimization {
+    void optimize(Solution solution);
+}
