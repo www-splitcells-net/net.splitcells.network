98af37ad25b7717142c9f964bc874fae14c5adae
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-24 00:07:10 +0100

    #22 Create backtracking base structure.

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/meta/Backtracking.java
@@ -0,0 +1,19 @@
+package net.splitcells.gel.solution.optimization.meta;
+
+import net.splitcells.gel.solution.Solution;
+import net.splitcells.gel.solution.optimization.OnlineOptimization;
+
+public class Backtracking implements OnlineOptimization {
+    public static Backtracking backtracking() {
+        return new Backtracking();
+    }
+
+    private Backtracking() {
+
+    }
+
+    @Override
+    public void optimize(Solution solution) {
+
+    }
+}
