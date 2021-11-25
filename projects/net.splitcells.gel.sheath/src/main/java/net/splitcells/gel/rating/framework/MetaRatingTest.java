acfaa20721a05220a15e9dc4d85530a959199edc
Mārtiņš Avots <martins.avots@splitcells.net>
2021-11-25 17:44:35 +0100

    #22 Create test for found bug.

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/framework/MetaRatingTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/framework/MetaRatingTest.java
@@ -13,6 +13,9 @@ package net.splitcells.gel.rating.framework;
 import net.splitcells.gel.rating.type.Profit;
 import org.junit.jupiter.api.Test;
 
+import static net.splitcells.gel.rating.framework.MetaRatingI.metaRating;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
 import static net.splitcells.gel.rating.type.Profit.profit;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,17 +23,25 @@ public class MetaRatingTest {
 
     @Test
     public void testCombinationOfPrimitives() {
-        final var testSubject = MetaRatingI.metaRating().combine(profit(1));
-        final var otherTestSubject = MetaRatingI.metaRating().combine(profit(3));
+        final var testSubject = metaRating().combine(profit(1));
+        final var otherTestSubject = metaRating().combine(profit(3));
         MetaRating testProduct = testSubject.combine(otherTestSubject);
         assertThat(testProduct.content().get(Profit.class)).isEqualTo(profit(4));
         assertThat(testProduct.translate(Profit.class)).isEqualTo(profit(4));
     }
 
     @Test
+    public void testBetterThan() {
+        final var testSubject = metaRating().combine(cost(1));
+        final var otherTestSubject = metaRating().combine(noCost());
+        assertThat(otherTestSubject.betterThan(testSubject)).isTrue();
+        assertThat(testSubject.betterThan(otherTestSubject)).isFalse();
+    }
+
+    @Test
     public void testUnequalContent() {
-        final var testSubject = MetaRatingI.metaRating().combine(profit(1));
-        final var otherTestSubject = MetaRatingI.metaRating().combine(profit(3));
+        final var testSubject = metaRating().combine(profit(1));
+        final var otherTestSubject = metaRating().combine(profit(3));
         assertThat(testSubject).isNotEqualTo(otherTestSubject);
     }
 }
