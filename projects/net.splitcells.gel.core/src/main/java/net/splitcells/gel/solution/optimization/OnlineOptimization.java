193395b36b9d0f97b895df37d00d516caa677c5e
Mārtiņš Avots <martins.avots@splitcells.net>
2021-12-13 22:51:51 +0100

    Document new optimizer implementation idea.

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/OnlineOptimization.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/OnlineOptimization.java
@@ -5,6 +5,7 @@ import net.splitcells.gel.solution.Solution;
 /**
  * <p>What benefit is there in reviewing past sufferings, and in being unhappy, just because once you were unhappy?
  * - Seneca</p>
+ * <p>TODO IDEA Implement random forest.</p>
  */
 @FunctionalInterface
 public interface OnlineOptimization {
