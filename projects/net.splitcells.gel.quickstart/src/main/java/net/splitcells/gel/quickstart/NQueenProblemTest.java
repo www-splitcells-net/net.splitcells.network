c923f083127c590c8a31d14bed914752795e7cf6
Mārtiņš Avots <martins.avots@splitcells.net>
2022-01-05 00:32:05 +0100

    #138 Create minimal implementation of introduction.

---
+++ projects/net.splitcells.gel.quickstart/src/main/java/net/splitcells/gel/quickstart/NQueenProblemTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+ * which is available at https://spdx.org/licenses/MIT.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ */
+package net.splitcells.gel.quickstart;
+
+import net.splitcells.dem.testing.TestSuiteI;
+import net.splitcells.gel.Gel;
+import net.splitcells.gel.GelDev;
+import net.splitcells.gel.data.table.attribute.Attribute;
+import net.splitcells.gel.problem.Problem;
+import net.splitcells.gel.rating.rater.Rater;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
+import static net.splitcells.dem.testing.TestTypes.CAPABILITY_TEST;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+import static net.splitcells.gel.rating.rater.HasSize.hasSize;
+import static net.splitcells.gel.rating.rater.RaterBasedOnLineValue.raterBasedOnLineValue;
+import static net.splitcells.gel.solution.optimization.meta.Backtracking.backtracking;
+import static net.splitcells.gel.solution.optimization.primitive.LinearInitialization.linearInitialization;
+import static net.splitcells.gel.solution.optimization.primitive.repair.ConstraintGroupBasedRepair.simpleConstraintGroupBasedRepair;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NQueenProblemTest {
+    public static final Attribute<Integer> COLUMN = attribute(Integer.class, "column");
+    public static final Attribute<Integer> ROW = attribute(Integer.class, "row");
+
+    @Tag(CAPABILITY_TEST)
+    @Test
+    public void test_8_queen_problem_with_backtracking() {
+        final var testSubject = nQueenProblem(8, 8).asSolution();
+        backtracking().optimize(testSubject);
+        assertThat(testSubject.isOptimal()).isTrue();
+    }
+
+    private Problem nQueenProblem(int rows, int columns) {
+        final var demands = listWithValuesOf(
+                rangeClosed(1, columns)
+                        .mapToObj(i -> list((Object) i))
+                        .collect(toList()));
+        final var supplies = listWithValuesOf(
+                rangeClosed(1, rows)
+                        .mapToObj(i -> list((Object) i))
+                        .collect(toList()));
+        return Gel.defineProblem()
+                .withDemandAttributes(COLUMN)
+                .withDemands(demands)
+                .withSupplyAttributes(ROW)
+                .withSupplies(supplies)
+                .withConstraint(
+                        r -> {
+                            r.forAll(ROW).forAll(COLUMN).then(hasSize(1));
+                            r.forAll(ROW).then(hasSize(1));
+                            r.forAll(COLUMN).then(hasSize(1));
+                            r.forAll(ascDiagonals(rows, columns)).then(hasSize(1));
+                            r.forAll(descDiagonals(rows, columns)).then(hasSize(1));
+                            return r;
+                        })
+                .toProblem();
+    }
+
+    /**
+     * The ascending diagonal with the number 0 represents the diagonal in the middle.
+     */
+    private static Rater ascDiagonals(int rows, int columns) {
+        return raterBasedOnLineValue("ascDiagonals", line -> line.value(ROW) - line.value(COLUMN));
+    }
+
+    /**
+     * The descending diagonal with the number 0 represents the diagonal in the middle.
+     */
+    private static Rater descDiagonals(int rows, int columns) {
+        return raterBasedOnLineValue("descDiagonals", line -> line.value(ROW) - Math.abs(line.value(COLUMN) - columns - 1));
+    }
+}
