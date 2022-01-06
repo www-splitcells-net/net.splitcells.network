0ee1d3ed0768f474852480eaae37104c4fc8a93b
Mārtiņš Avots <martins.avots@splitcells.net>
2022-01-06 13:25:58 +0100

    #138 Fix links with protocols in CommonMark.

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/commonmark/LinkTranslator.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/commonmark/LinkTranslator.java
@@ -1,8 +1,11 @@
 package net.splitcells.website.server.project.renderer.commonmark;
 
+import net.splitcells.dem.utils.CommonFunctions;
 import org.commonmark.node.AbstractVisitor;
 import org.commonmark.node.Image;
 import org.commonmark.node.Link;
+
+import java.util.regex.Pattern;
 
 /**
  * <p>This class translates links inside CommonMark documents.
@@ -17,6 +20,8 @@ import org.commonmark.node.Link;
  * that the heuristics are good enough for now.</p>
  */
 public class LinkTranslator extends AbstractVisitor {
+
+    private static Pattern PROTOCOL = Pattern.compile("([a-z]://)(.*)");
 
     public static LinkTranslator linkTranslator() {
         return new LinkTranslator();
@@ -35,12 +40,23 @@ public class LinkTranslator extends AbstractVisitor {
     }
 
     public void visit(Link link) {
-        link.setDestination(link.getDestination()
+        final var destination = link.getDestination();
+        final var protocolMatch= PROTOCOL.matcher(destination);
+        final String protocol;
+        if (protocolMatch.matches()) {
+            protocol = protocolMatch.group(1);
+        } else {
+            protocol = "";
+        }
+        final var destinationWithoutProtocol = destination
+                .substring(protocol.length())
                 .replace("../", "")
                 .replaceAll("src\\/main\\/[a-z]+\\/", "/")
                 .replaceAll("projects\\/[a-z\\.]+\\/", "/")
-                .replace("//", "/"));
-        if (link.getDestination().endsWith(".md")) {
+                .replace("//", "/");
+
+        link.setDestination(protocol + destinationWithoutProtocol);
+        if (link.getDestination().endsWith(".md") && protocol.isEmpty()) {
             link.setDestination(link.getDestination().substring(0, link.getDestination().length() - 3) + ".html");
         }
         this.visitChildren(link);
