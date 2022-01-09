daaf9ce70d013e5189b4ec3e92c44dce8d9dd1a1
Mārtiņš Avots <martins.avots@splitcells.net>
2022-01-09 01:10:53 +0100

    Document 'run.and.show.if.failed.py'.

--- projects/net.splitcells.os.state.interface/src/main/python/run.and.show.if.failed.py
+++ projects/net.splitcells.os.state.interface/src/main/python/run.and.show.if.failed.py
@@ -4,6 +4,24 @@ This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0, which is available at
 http://www.eclipse.org/legal/epl-2.0, or the MIT License,
 which is available at https://spdx.org/licenses/MIT.html.
+
+This command executes a given command in such a way,
+that only stderr is printed to the shell,
+if the given command exited successfully by returning the exit code 0.
+
+The given command is executed on the system's shells and is monitored:
+* Content written to stdout is written to the output,
+  but only one line at a time is visible on parents shell.
+  Previous lines are not shown.
+  In other words only the current output line of stdout is visible.
+* Content written to stderr is echoed normally.
+* If the executed command ends with an error,
+  all contents of stderr and stdout are echoed again,
+  so it is possible to understand the error's cause.
+  
+TODO Print stderr via "echo.error".
+TODO IDEA Add an option where normal output is not filtered via 'echo.fitlered'.
+This will not work well, if this command is called recursively.
 """
 
 __author__ = "Mārtiņš Avots"
@@ -17,8 +35,6 @@ from threading import Thread
 from time import time
 isRunning = True
 output=list()
-
-# TODO Print stderr via "echo.error".
 
 def readAndOutput(stream, outputAsError):
 	lastEchoTime = -1
