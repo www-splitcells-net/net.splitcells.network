2664b2fd675c2db85abf79ea18e3c0e26fc8f335
Mārtiņš Avots <martins.avots@splitcells.net>
2022-02-05 01:51:01 +0100

    #163 Create new push command.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.os.state.interface/src/main/python/repo/repo.push.at.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0, which is available at
+http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+which is available at https://spdx.org/licenses/MIT.html.
+"""
+
+__author__ = "Mārtiņš Avots"
+__authors__ = ["and other"]
+__copyright__ = "Copyright 2021"
+__license__ = "EPL-2.0 OR MIT"
+
+import argparse
+import logging
+import subprocess
+import sys
+
+from os import environ
+
+if __name__ == '__main__':
+	if environ.get('log_level') == 'debug':
+		logging.basicConfig(level = logging.DEBUG)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--remote-repo-name'
+        		, dest = 'remoteRepoName'
+        		, required = True
+        		, help = 'This is the local name of the remote repository,to which data is pushed.')
+	parser.add_argument('--remote-repo-URL'
+		, dest = 'remoteRepoUrl'
+		, required = True
+		, help = 'This is the URL of the remote repository,to which data is pushed.')
+	parsedArgs = parser.parse_args()
+	commandToExecute = "command.managed.execute disjunction repo.push.at"\
+		+ " --remote-repo-name='" + parsedArgs.remoteRepoName + "'"\
+		+ " --remote-repo-URL='" + parsedArgs.remoteRepoUrl + "'"
+	logging.debug("Executing: " + commandToExecute)
+	subprocess.call(commandToExecute, shell='True')
