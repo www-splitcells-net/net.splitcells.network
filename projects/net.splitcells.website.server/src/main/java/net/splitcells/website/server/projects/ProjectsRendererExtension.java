591d751a4d04fd006c6ebd8b1045979b42f02c82
Mārtiņš Avots <martins.avots@splitcells.net>
2022-02-28 01:15:10 +0100

    #125 Create interface to projects rendering extension.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/projects/ProjectsRendererExtension.java
@@ -0,0 +1,27 @@
+package net.splitcells.website.server.projects;
+
+import net.splitcells.dem.data.set.Set;
+import net.splitcells.dem.lang.perspective.Perspective;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.project.ProjectRenderer;
+import net.splitcells.website.server.project.ProjectsRenderer;
+import net.splitcells.website.server.project.RenderingResult;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static net.splitcells.dem.data.set.Sets.setOfUniques;
+
+public interface ProjectsRendererExtension {
+    Optional<RenderingResult> renderFile(String path, ProjectsRenderer projectsRenderer, Config config);
+
+    default Perspective extendProjectLayout(Perspective layout, ProjectsRenderer projectsRenderer) {
+        return layout;
+    }
+
+    Set<Path> projectPaths(ProjectsRenderer projectsRenderer);
+
+    default Set<Path> relevantProjectPaths(ProjectsRenderer projectsRenderer) {
+        return projectPaths(projectsRenderer);
+    }
+}
