c606ac8e6451560b545400b26f7a62fd4c6ece1e
Mārtiņš Avots <martins.avots@splitcells.net>
2022-03-20 03:14:06 +0100

    #162 Create base class for worker.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.network.worker/src/main/java/net/splitcells/network/worker/Logger.java
@@ -0,0 +1,10 @@
+package net.splitcells.network.worker;
+
+public class Logger {
+    public static Logger logger() {
+        return new Logger();
+    }
+    private Logger() {
+        
+    }
+}
