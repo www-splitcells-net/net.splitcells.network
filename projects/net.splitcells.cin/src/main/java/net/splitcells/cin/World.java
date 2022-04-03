a4160fecfc81fdfb83091e39ec6f4bbe73d4c322
Mārtiņš Avots <martins.avots@splitcells.net>
2022-04-04 01:52:03 +0200

    #170 Define base data.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/java/net/splitcells/cin/World.java
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/World.java
@@ -3,10 +3,12 @@ package net.splitcells.cin;
 import net.splitcells.dem.data.atom.Bools;
 import net.splitcells.dem.environment.config.IsDeterministic;
 import net.splitcells.gel.GelDev;
+import net.splitcells.gel.data.table.attribute.Attribute;
 import net.splitcells.gel.solution.Solution;
 
 import java.util.Optional;
 
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 import static net.splitcells.gel.rating.rater.AllDifferent.allDifferent;
 import static net.splitcells.gel.rating.rater.AllSame.allSame;
 import static net.splitcells.gel.rating.rater.RaterBasedOnLineValue.lineValueRater;
@@ -16,6 +18,10 @@ import static net.splitcells.sep.Network.network;
 
 public class World {
     private static final String WORLD_HISTORY = "world-history";
+    private static final Attribute<Integer> WORLD_TIME = attribute(Integer.class, "world-time");
+    private static final Attribute<Integer> OBJECT_ID = attribute(Integer.class, "object-id");
+    private static final Attribute<Integer> POSITION_X = attribute(Integer.class, "position-x");
+    private static final Attribute<Integer> POSITION_Y = attribute(Integer.class, "position-y");
 
     public static void main(String... args) {
         GelDev.process(() -> {
@@ -26,8 +32,8 @@ public class World {
 
     private static Solution worldHistory() {
         return defineProblem("Rails For School Scheduling")
-                .withDemandAttributes()
-                .withSupplyAttributes()
+                .withDemandAttributes(WORLD_TIME, OBJECT_ID)
+                .withSupplyAttributes(POSITION_X, POSITION_Y)
                 .withConstraint(r -> {
                     return r;
                 }).toProblem()
