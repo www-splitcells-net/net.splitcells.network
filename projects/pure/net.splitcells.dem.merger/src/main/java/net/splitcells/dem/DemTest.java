0d645b2ea0c1eda9dbadb5e81076cfad538fbf69
Mārtiņš Avots <martins.avots@splitcells.net>
2022-04-17 00:19:56 +0200

    #10 Move additional sources to pure project.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/DemTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+ * which is available at https://spdx.org/licenses/MIT.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ */
+package net.splitcells.dem;
+
+import net.splitcells.dem.resource.communication.log.MessageFilter;
+
+/**
+ * Used for experiments.
+ * <p>
+ * maven.execute net.splitcells.dem.DemTest
+ */
+public class DemTest {
+    public static void main(String... args) {
+        Dem.process(() -> {
+            
+        }, (env) -> {
+            env.config()
+                    .withConfigValue(MessageFilter.class, (message) -> true);
+        });
+    }
+}
---
+++ projects/pure/net.splitcells.dem.merger/src/main/java/net/splitcells/dem/utils/StreamUtils.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+ * which is available at https://spdx.org/licenses/MIT.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ */
+package net.splitcells.dem.utils;
+
+import java.util.Collections;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
+
+public final class StreamUtils {
+
+    private StreamUtils() {
+        throw constructorIllegal();
+    }
+
+    public static <T> Stream<T> reverse(Stream<T> stream) {
+        return stream.collect(
+                collectingAndThen(
+                        toList(), l -> {
+                            Collections.reverse(l);
+                            return l;
+                        }))
+                .stream();
+    }
+
+    public static <T, E extends RuntimeException> BinaryOperator<T> ensureSingle() {
+        return (element, otherElement) -> {
+            throw new IllegalArgumentException();
+        };
+    }
+}
