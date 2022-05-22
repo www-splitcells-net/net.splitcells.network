98af6fadce9f047fb7404899edd70c9e75f006e8
Mārtiņš Avots <martins.avots@splitcells.net>
2022-05-23 00:39:29 +0200

    #162 Fix build deploy command.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/python/net/splitcells/network/net.splitcells.network.deploy.build.at.py
+++ src/main/python/net/splitcells/network/net.splitcells.network.deploy.build.at.py
@@ -16,20 +16,21 @@ __license__ = "EPL-2.0 OR MIT"
 
 import argparse
 import subprocess
+import logging
 
 if __name__ == '__main__':
 	argsParser = argparse.ArgumentParser(
 		description='Deploys the build process to a different server.')
 	argsParser.add_argument('--target-server', dest='targetServer', type = str, required = True)
 	argsParser.add_argument('--user', type = str, required = True)
-    parsedArgs = parser.parse_args()
-    buildScript = """
-        git push {0}:/home/ubuntu/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network master:net-splitcells-martins-avots-connection\\
-        ssh -t {0} systemd-run --uid={1} --unit=build --working-directory='/home/{1}/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network' 'sh -c "git merge net-splitcells-martins-avots-connection && ./bin/test.via.network.worker"'\\
-        cd ../net.splitcells.network.log/\\
-        git pull {0}:/home/ubuntu/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network.log master\\
-    	""".format(parsedArgs.targetServer, parsedArgs.user)
-    logging.debug('Executing: ' + buildScript)
-    returnCode = subprocess.call(buildScript, shell='True')
-    if returnCode != 0:
-        exit(1)
+	parsedArgs = argsParser.parse_args()
+	buildScript = """
+		git push {0}:/home/{1}/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network master:net-splitcells-martins-avots-connection
+		ssh -t {0} systemd-run --uid={1} --unit=build --working-directory='/home/{1}/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network' 'sh -c "git merge net-splitcells-martins-avots-connection && ./bin/test.via.network.worker"'
+		cd ../net.splitcells.network.log/
+		git pull {0}:/home/{1}/Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network.log master
+		""".format(parsedArgs.targetServer, parsedArgs.user)
+	logging.debug('Executing: ' + buildScript)
+	returnCode = subprocess.call(buildScript, shell='True')
+	if returnCode != 0:
+		exit(1)
