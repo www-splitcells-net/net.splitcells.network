d3093711f859d01b953334e4ce0ae8235c6d4230
Mārtiņš Avots <martins.avots@splitcells.net>
2022-06-06 23:37:14 +0200

    #8 Document index relations of supplies and demands.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationsLiveView.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationsLiveView.java
@@ -19,16 +19,44 @@ import net.splitcells.gel.data.database.Database;
 import static net.splitcells.dem.data.set.Sets.setOfUniques;
 
 public interface AllocationsLiveView extends Table {
+    /**
+     * Returns a {@link Database} of all supplies.
+     * The {@link Line#index()} of one supply is the same across this database, {@link #suppliesFree()} and {@link #suppliesUsed()}
+     * 
+     * @return Database Of All Supplies
+     */
     Database supplies();
 
+    /**
+     * 
+     * @return This database contains all {@link #supplies()}, that are allocated to at least one of {@link #demands()}.
+     */
     Database suppliesUsed();
 
+    /**
+     * 
+     * @return This database contains all {@link #supplies()} that are not allocated to any {@link #demands()}
+     */
     Database suppliesFree();
 
+    /**
+     * Returns a {@link Database} of all demands.
+     * The {@link Line#index()} of one demand is the same across this database, {@link #demandsFree()} and {@link #demandsUsed()}
+     *
+     * @return Database Of All Supplies
+     */
     Database demands();
 
+    /**
+     *
+     * @return This database contains all {@link #demands()}, that are allocated to at least one of {@link #supplies()}.
+     */
     Database demandsUsed();
 
+    /**
+     *
+     * @return This database contains all {@link #demands()}, that are not allocated to any {@link #supplies()}.
+     */
     Database demandsFree();
 
     /**
