51a38ddc8614202dcbdccfe88478bd4b08bfbedb
Mārtiņš Avots <martins.avots@splitcells.net>
2022-06-10 22:33:35 +0200

    #8 Document supply selector for repair.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/repair/SupplySelector.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/repair/SupplySelector.java
@@ -14,5 +14,11 @@ import java.util.function.Function;
  */
 @FunctionalInterface
 public interface SupplySelector extends Function<Map<GroupId, Set<Line>>, OnlineOptimization> {
+    /**
+     *
+     * @param freeDemandGroups These are free demands grouped according to some unknown function.
+     * @return This is an optimization function that can be applied to the {@link net.splitcells.gel.solution.Solution},
+     * that contains the {@link Line}s of the {@param freeDemandGroups}.
+     */
     OnlineOptimization apply(Map<GroupId, Set<Line>> freeDemandGroups);
 }
