3f79a6cdfd20db424970eca50a8b464fb859e789
Mārtiņš Avots <martins.avots@splitcells.net>
2022-07-15 22:12:41 +0200

    #8 Add a very basic lookup test.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/lookup/LookupTableTest.java
@@ -0,0 +1,35 @@
+package net.splitcells.gel.data.lookup;
+
+import net.splitcells.gel.Gel;
+import org.junit.jupiter.api.Test;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.gel.constraint.type.ForAlls.forAll;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+import static net.splitcells.gel.solution.optimization.primitive.LinearDeinitializer.linearDeinitializer;
+import static net.splitcells.gel.solution.optimization.primitive.LinearInitialization.linearInitialization;
+
+public class LookupTableTest {
+    @Test
+    public void testLookup() {
+        final var a = attribute(Integer.class, "a");
+        final var b = attribute(Integer.class, "b");
+        final var testSubject = Gel.defineProblem("testLookup")
+                .withDemandAttributes(a, b)
+                .withDemands(list(list(1, 1), list(1, 2)))
+                .withSupplyAttributes()
+                .withNoSupplies()
+                .withConstraint(forAll())
+                .toProblem()
+                .asSolution();
+        testSubject.lookup(a, 1).lookup(b, 2).getLines();
+        testSubject.optimize(linearInitialization());
+        testSubject.lookup(a, 1).lookup(b, 2).getLines();
+        testSubject.optimize(linearDeinitializer());
+        testSubject.lookup(a, 1).lookup(b, 2).getLines();
+        testSubject.optimize(linearInitialization());
+        testSubject.lookup(a, 1).lookup(b, 2).getLines();
+        testSubject.optimize(linearDeinitializer());
+        testSubject.lookup(a, 1).lookup(b, 2).getLines();
+    }
+}
