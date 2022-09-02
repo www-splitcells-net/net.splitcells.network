fa96056a15868c13e2ffc716229f022355a605de
Mārtiņš Avots <martins.avots@splitcells.net>
2022-09-03 01:11:41 +0200

    #168 Create base structure for repo process tests.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0, which is available at
+http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+which is available at https://spdx.org/licenses/MIT.html.
+"""
+
+__author__ = "Mārtiņš Avots"
+__authors__ = ["and other"]
+__copyright__ = "Copyright 2022"
+__license__ = "EPL-2.0 OR MIT"
+
+if __name__ == '__main__':
+    exit(0)
