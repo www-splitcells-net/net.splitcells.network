4556449100cd69b85102d92e7a89860f314bf66e
Mārtiņš Avots <martins.avots@splitcells.net>
2022-09-11 01:29:11 +0200

    #168 Log execution of dependency tests.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/bash/user/user.bin.configure.sh
+++ projects/net.splitcells.os.state.interface/src/main/bash/user/user.bin.configure.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
 #
 # This program and the accompanying materials are made available under the
@@ -7,9 +7,15 @@
 # which is available at https://spdx.org/licenses/MIT.html.
 #
 # SPDX-License-Identifier: EPL-2.0 OR MIT
-
+set -e
 this.requires user.bin.configure.0 && command.managed.execute conjunction user.bin.configure
 command.repositories.install
 command.managed.install.project.commands
   chmod +x ~/bin/net.splitcells.os.state.interface.commands.managed/*
-user.bin.configure.uses.dependencies.test && dependencies.test || echo "Dependencies are not validated during this installation."
+if user.bin.configure.uses.dependencies.test
+then
+  dependencies.test
+  echo Dependency tests executed.
+else
+  echo "Dependencies are not validated during this installation."
+fi
