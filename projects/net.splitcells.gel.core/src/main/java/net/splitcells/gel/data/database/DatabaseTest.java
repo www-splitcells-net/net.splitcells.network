4e2ca5ab88902b5ae42120aba10c4b903565a616
Mārtiņš Avots <martins.avots@splitcells.net>
2022-09-13 23:43:59 +0200

    #8 Create test for query system init.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/database/DatabaseTest.java
@@ -0,0 +1,30 @@
+package net.splitcells.gel.data.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.rangeClosed;
+import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
+import static net.splitcells.gel.constraint.Constraint.LINE;
+import static net.splitcells.gel.data.database.Databases.database;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+
+public class DatabaseTest {
+    @Test
+    public void testQueryInitialization() {
+        final var index = attribute(Integer.class);
+        final var testSubject = database(listWithValuesOf(index));
+        rangeClosed(1, 10).forEach(i -> {
+            testSubject.addTranslated(listWithValuesOf(i));
+        });
+        testSubject
+                .query()
+                .forAllCombinationsOf(listWithValuesOf(index))
+                .currentConstraint()
+                .lines()
+                .columnView(LINE)
+                .values()
+                .requireSizeOf(10);
+    }
+}
