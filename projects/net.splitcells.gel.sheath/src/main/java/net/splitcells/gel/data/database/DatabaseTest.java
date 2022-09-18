aafc976b6029a105dabaec6a938e86b9f64744de
Mārtiņš Avots <martins.avots@splitcells.net>
2022-09-18 22:37:42 +0200

    #8 Avoid Duplicate classes.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/data/database/DatabaseTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/data/database/DatabaseTest.java
@@ -12,21 +12,26 @@ package net.splitcells.gel.data.database;
 
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.testing.TestSuiteI;
+import net.splitcells.dem.testing.TestTypes;
 import net.splitcells.gel.data.table.Line;
 import net.splitcells.gel.data.table.attribute.Attribute;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import static java.util.stream.IntStream.rangeClosed;
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
 import static net.splitcells.dem.testing.Mocking.anyObject;
 import static net.splitcells.dem.testing.Mocking.anyString;
 import static net.splitcells.dem.testing.TestTypes.UNIT_TEST;
+import static net.splitcells.gel.constraint.Constraint.LINE;
+import static net.splitcells.gel.data.database.Databases.database;
 import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -40,6 +45,24 @@ import static org.mockito.Mockito.when;
  * TODO Test consistency, when adding or removing many things
  */
 public class DatabaseTest extends TestSuiteI {
+    @Test
+    @Tag(TestTypes.EXPERIMENTAL_TEST)
+    public void testQueryInitialization() {
+        final var index = attribute(Integer.class);
+        final var testSubject = database(listWithValuesOf(index));
+        rangeClosed(1, 10).forEach(i -> {
+            testSubject.addTranslated(listWithValuesOf(i));
+        });
+        testSubject
+                .query()
+                .forAllCombinationsOf(listWithValuesOf(index))
+                .currentConstraint()
+                .lines()
+                .columnView(LINE)
+                .values()
+                .requireSizeOf(10);
+    }
+
     @Tag(UNIT_TEST)
     @TestFactory
     public Stream<DynamicTest> incorrectly_typed_values_addition_tests() {
