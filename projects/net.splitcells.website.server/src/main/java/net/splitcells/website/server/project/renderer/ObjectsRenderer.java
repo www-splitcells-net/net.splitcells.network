9ed7e52a4476003c71641b6120773eb4754d3c87
Mārtiņš Avots <martins.avots@splitcells.net>
2022-09-25 00:29:04 +0200

    #170 Create registration method for convenience.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/ObjectsRenderer.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/ObjectsRenderer.java
@@ -5,10 +5,15 @@ import net.splitcells.dem.environment.config.framework.OptionI;
 import java.nio.file.Path;
 import java.util.function.Supplier;
 
+import static net.splitcells.dem.Dem.configValue;
 import static net.splitcells.website.server.project.renderer.ObjectsRendererI.objectsRenderer;
 
 public class ObjectsRenderer extends OptionI<ObjectsRendererI> {
     public ObjectsRenderer() {
         super(() -> objectsRenderer(Path.of("/net/splitcells/run/")));
+    }
+
+    public static void registerObject(DiscoverableRenderer object) {
+        configValue(ObjectsRenderer.class).withObject(object);
     }
 }
