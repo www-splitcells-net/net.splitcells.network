0135b655ae0b88c4f023fcd4b9f5f6107179f26e
Mārtiņš Avots <martins.avots@splitcells.net>
2022-10-16 03:09:36 +0200

    #170 Extend SolutionFactory with connection feature.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/SolutionFactory.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/SolutionFactory.java
@@ -13,8 +13,9 @@ package net.splitcells.gel.solution;
 import net.splitcells.dem.environment.resource.Resource;
 import net.splitcells.dem.resource.AspectOrientedConstructor;
 import net.splitcells.dem.resource.AspectOrientedConstructorBase;
+import net.splitcells.dem.resource.ConnectingConstructor;
 import net.splitcells.gel.problem.Problem;
 
-public interface SolutionFactory extends AspectOrientedConstructor<Solution>, Resource {
+public interface SolutionFactory extends AspectOrientedConstructor<Solution>, Resource, ConnectingConstructor<Solution> {
     Solution solution(Problem problem);
 }
