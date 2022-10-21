7cb48c587797f39064a5d6c123ce6345c96640b1
Mārtiņš Avots <martins.avots@splitcells.net>
2022-10-21 18:06:39 +0200

    #10 Adjust code to grammar.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/rater/AllDifferentTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/rater/AllDifferentTest.java
@@ -11,14 +11,12 @@
 package net.splitcells.gel.rating.rater;
 
 import net.splitcells.dem.testing.TestSuiteI;
-import net.splitcells.gel.constraint.type.Then;
 import net.splitcells.gel.data.database.Database;
-import net.splitcells.gel.data.database.Databases;
 import net.splitcells.gel.data.table.attribute.Attribute;
-import net.splitcells.gel.data.table.attribute.AttributeI;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static net.splitcells.dem.data.set.Sets.setOfUniques;
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.testing.TestTypes.INTEGRATION_TEST;
 import static net.splitcells.gel.constraint.type.Then.then;
@@ -27,7 +25,6 @@ import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 import static net.splitcells.gel.rating.rater.AllDifferent.allDifferent;
 import static net.splitcells.gel.rating.type.Cost.cost;
 import static net.splitcells.gel.rating.type.Cost.noCost;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class AllDifferentTest extends TestSuiteI {
 
@@ -37,12 +34,12 @@ public class AllDifferentTest extends TestSuiteI {
         final Attribute<Integer> attribute = attribute(Integer.class);
         final Database lineSupplier = database(attribute);
         final var testSubject = then(allDifferent(attribute));
-        assertThat(testSubject.rating().equalz(noCost())).isTrue();
-        assertThat(testSubject.defying()).isEmpty();
+        testSubject.rating().requireEqualsTo(noCost());
+        setOfUniques(testSubject.defying()).requireEmptySet();
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(1)));
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(2)));
-        assertThat(testSubject.defying()).isEmpty();
-        assertThat(testSubject.rating().equalz(noCost())).isTrue();
+        setOfUniques(testSubject.defying()).requireEmptySet();
+        testSubject.rating().requireEqualsTo(noCost());
         // TODO Test removal.
     }
 
@@ -52,10 +49,10 @@ public class AllDifferentTest extends TestSuiteI {
         final Attribute<Integer> attribute = attribute(Integer.class);
         final Database lineSupplier = database(attribute);
         final var testSubject = then(allDifferent(attribute));
-        assertThat(testSubject.rating().equalz(noCost())).isTrue();
+        testSubject.rating().requireEqualsTo(noCost());
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(1)));
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(1)));
-        assertThat(testSubject.rating().equalz(cost(2.0))).isTrue();
+        testSubject.rating().requireEqualsTo(cost(2.0));
         // TODO Test removal.
     }
 
@@ -65,11 +62,11 @@ public class AllDifferentTest extends TestSuiteI {
         final var attribute = attribute(Integer.class);
         final var lineSupplier = database(attribute);
         final var testSubject = then(allDifferent(attribute));
-        assertThat(testSubject.rating().equalz(noCost())).isTrue();
+        testSubject.rating().requireEqualsTo(noCost());
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(1)));
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(1)));
         testSubject.register_papildinājumi(lineSupplier.addTranslated(list(2)));
-        assertThat(testSubject.rating().equalz(cost(2.0))).isTrue();
+        testSubject.rating().requireEqualsTo(cost(2.0));
         // TODO Test removal.
     }
 }
