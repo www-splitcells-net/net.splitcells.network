73fe464080908c7a6da1aa1732245e350b7e60ba
Mārtiņš Avots <martins.avots@splitcells.net>
2022-10-26 22:24:04 +0200

    #170 Clean up renderer code.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/js/net/splitcells/cin/renderer.js
+++ projects/net.splitcells.cin/src/main/js/net/splitcells/cin/renderer.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 const movement = 1;
 
 const renderer = new THREE.WebGLRenderer();
@@ -11,8 +12,6 @@ renderer.domElement.style.height = "50%";
 renderer.domElement.style.width = "100%";
 document.getElementById("net-splitcells-canvas-main").appendChild(renderer.domElement);
 
-camera.position.z = 5;
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
@@ -20,13 +19,14 @@ function animate() {
     renderer.render(scene, camera);
 };
 
-function updateWorldData(updatedData) {
+function addWorldData(updatedData) {
     var rowIndex;
     for (rowIndex = 1; rowIndex < updatedData.length; rowIndex++) {
         var row = updatedData[rowIndex];
         console.log(row);
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        // Wireframe is disabled for now.
         // material.wireframe = true;
         // material.wireframeLinecap = 'square';
         const cube = new THREE.Mesh(geometry, material);
@@ -40,7 +40,7 @@ Papa.parse("https://raw.githubusercontent.com/www-splitcells-net/net.splitcells.
         download: true
         , dynamicTyping: true
         , complete: function (results) {
-            updateWorldData(results.data);
+            addWorldData(results.data);
         }
     });
 
