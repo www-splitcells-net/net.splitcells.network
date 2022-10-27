1cace390fb40bfd8d582a805d5fb38e4e3de005f
Mārtiņš Avots <martins.avots@splitcells.net>
2022-10-27 21:55:14 +0200

    #170 Provide solution content as CSV file.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/java/net/splitcells/cin/Cin.java
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/Cin.java
@@ -12,14 +12,21 @@ package net.splitcells.cin;
 
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.environment.Environment;
+import net.splitcells.dem.resource.ContentType;
 import net.splitcells.gel.data.database.Databases;
 import net.splitcells.gel.solution.Solutions;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.project.ProjectRenderer;
+import net.splitcells.website.server.project.RenderingResult;
+import net.splitcells.website.server.project.renderer.DiscoverableMediaRenderer;
 import net.splitcells.website.server.project.renderer.DiscoverableRenderer;
+import net.splitcells.website.server.project.renderer.ObjectsMediaRenderer;
 import net.splitcells.website.server.project.renderer.ObjectsRenderer;
 
 import java.util.Optional;
 
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
+import static net.splitcells.website.server.project.RenderingResult.renderingResult;
 
 public class Cin {
     private Cin() {
@@ -76,6 +83,19 @@ public class Cin {
                     @Override
                     public List<String> path() {
                         return solution.path().withAppended("constraint");
+                    }
+                }));
+        env.config().configValue(Solutions.class)
+                .withConnector(solution -> ObjectsMediaRenderer.registerMediaObject(new DiscoverableMediaRenderer() {
+                    @Override
+                    public Optional<RenderingResult> render(ProjectRenderer projectRenderer, Config config) {
+                        return Optional.of(renderingResult(solution.toCSV().getBytes(), ContentType.CSV.codeName()));
+                    }
+
+                    @Override
+                    public List<String> path() {
+                        final var path = solution.path().shallowCopy();
+                        return path.withAppended(path.removeAt(path.size() - 1) + ".csv");
                     }
                 }));
     }
