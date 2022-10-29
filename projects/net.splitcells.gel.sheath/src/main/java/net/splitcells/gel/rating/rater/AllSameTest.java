1321be6103313c88f8ca0ddee2f976a9b335486f
Mārtiņš Avots <martins.avots@splitcells.net>
2022-10-29 13:54:49 +0200

    #10 Add missing license info.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/rater/AllSameTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/rater/AllSameTest.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
 package net.splitcells.gel.rating.rater;
 
 import net.splitcells.gel.constraint.type.Then;
