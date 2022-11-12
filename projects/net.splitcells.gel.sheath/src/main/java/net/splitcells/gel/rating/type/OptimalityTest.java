3d772dba2d1d6d7cd9a25dcba72c81c981bc459b
Mārtiņš Avots <martins.avots@splitcells.net>
2022-11-13 00:11:24 +0100

    #10 Adjust code to grammar.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/type/OptimalityTest.java
+++ projects/net.splitcells.gel.sheath/src/main/java/net/splitcells/gel/rating/type/OptimalityTest.java
@@ -13,16 +13,15 @@ package net.splitcells.gel.rating.type;
 import org.junit.jupiter.api.Test;
 
 import static net.splitcells.dem.data.order.Ordering.*;
+import static net.splitcells.dem.testing.Assertions.assertThrows;
 import static net.splitcells.gel.rating.type.Optimality.optimality;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OptimalityTest {
     @Test
     public void testOrder() {
-        assertThat(optimality(0).compare_partially_to(optimality(.1)).get()).isEqualTo(LESSER_THAN);
-        assertThat(optimality(.1).compare_partially_to(optimality(0)).get()).isEqualTo(GREATER_THAN);
-        assertThat(optimality().compare_partially_to(optimality()).get()).isEqualTo(EQUAL);
+        optimality(0).compare_partially_to(optimality(.1)).get().requireEqualsTo(LESSER_THAN);
+        optimality(.1).compare_partially_to(optimality(0)).get().requireEqualsTo(GREATER_THAN);
+        optimality().compare_partially_to(optimality()).get().requireEqualsTo(EQUAL);
     }
 
     @Test
