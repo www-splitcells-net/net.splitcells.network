404836b29d510b14591652d24c173fed361b748a
Mārtiņš Avots <martins.avots@splitcells.net>
2022-11-20 23:08:22 +0100

    #170 Use name for allocations database.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationsI.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationsI.java
@@ -70,7 +70,7 @@ public class AllocationsI implements Allocations {
     @Deprecated
     protected AllocationsI(String name, Database demand, Database supply) {
         this.names = name;
-        allocations = database2(Language.ALLOCATIONS.value(), demand, concat(demand.headerView(), supply.headerView()));
+        allocations = database2(Language.ALLOCATIONS.value() + "/" + name, demand, concat(demand.headerView(), supply.headerView()));
         // TODO Remove code and comment duplications.
         {
             this.demands = demand;
