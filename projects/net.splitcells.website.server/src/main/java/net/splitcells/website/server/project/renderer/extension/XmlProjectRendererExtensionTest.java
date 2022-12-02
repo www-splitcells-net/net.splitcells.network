0461fb8fa5a907174142b4e16ce8410985f97565
Mārtiņš Avots <martins.avots@splitcells.net>
2022-12-03 00:56:36 +0100

    #196 Create test for XMLs without closing elements.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/extension/XmlProjectRendererExtensionTest.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/extension/XmlProjectRendererExtensionTest.java
@@ -10,15 +10,18 @@
  */
 package net.splitcells.website.server.project.renderer.extension;
 
+import net.splitcells.dem.data.atom.Bools;
 import net.splitcells.website.server.Config;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 import static net.splitcells.dem.testing.TestTypes.INTEGRATION_TEST;
 import static net.splitcells.website.server.project.ProjectRenderer.projectRenderer;
+import static net.splitcells.website.server.project.renderer.extension.XmlProjectRendererExtension.XML_OPENING_ELEMENT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class XmlProjectRendererExtensionTest {
@@ -29,12 +32,19 @@ public class XmlProjectRendererExtensionTest {
     @Test
     public void testXmlLayout() {
         final var testSubject = projectRenderer("public"
-                        , Path.of("../net.splitcells.dem")
-                        , Path.of("../net.splitcells.website.default.content/src/main/xsl/net/splitcells/website/den/translation/to/html/")
-                        , Path.of("net.splitcells.website.default.content/src/main/resources/html")
-                        , "/net/splitcells/dem"
-                        , a -> Optional.empty()
-                        , Config.create());
+                , Path.of("../net.splitcells.dem")
+                , Path.of("../net.splitcells.website.default.content/src/main/xsl/net/splitcells/website/den/translation/to/html/")
+                , Path.of("net.splitcells.website.default.content/src/main/resources/html")
+                , "/net/splitcells/dem"
+                , a -> Optional.empty()
+                , Config.create());
         assertThat(testSubject.projectPaths()).contains(Path.of("net/splitcells/dem/history.html"));
+    }
+
+    @Test
+    public void test() {
+        Bools.require(XML_OPENING_ELEMENT.matcher("<start>").matches());
+        Bools.require(XML_OPENING_ELEMENT.matcher("<start>1 234 abcd").matches());
+        Bools.require(XML_OPENING_ELEMENT.matcher("<start name=\"value\">1 234 abcd").matches());
     }
 }
