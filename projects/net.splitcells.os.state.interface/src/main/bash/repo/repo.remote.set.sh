eb944655471ac8b68fa7e639c734c67b3fd20b3d
Mārtiņš Avots <martins.avots@splitcells.net>
2023-01-08 22:52:52 +0100

    Document `repo.remote.set` requirement.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/bash/repo/repo.remote.set.sh
+++ projects/net.splitcells.os.state.interface/src/main/bash/repo/repo.remote.set.sh
@@ -8,4 +8,11 @@
 #
 # SPDX-License-Identifier: EPL-2.0 OR MIT
 
+# Any implementation of `repo.remote.set`, should be an offline command.
+# Therefore, the command should not create an error, if the remote repo does not exist.
+# For one, there might be temporal connectivity issues,
+# that should not cause problems by default, when repos are configured locally.
+# Secondly, if the remote repo hoster deletes a repo and updates the `repo.process` data accordingly,
+# an online version of `repo.remote.set` would cause problems with a very limited number of benefits. 
+
 repo.process --command "command.managed.execute disjunction repo.remote.set $@/\$subRepo"
