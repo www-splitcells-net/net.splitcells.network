bffed8724a80b10fc6880859f323311a33ec151f
Mārtiņš Avots <martins.avots@splitcells.net>
2023-01-10 23:34:10 +0100

    Fix CI error.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.test.py
+++ projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.test.py
@@ -24,7 +24,7 @@ def store_output(stream, output):
             else:
                 return
 if __name__ == '__main__':
-    process = subprocess.Popen("cd projects/net.splitcells.os.state.interface/src/test/repo.process.test && repo.process --command='echo $subRepo' --command-for-missing='exit'",
+    process = subprocess.Popen("repo.process --command='echo $subRepo' --command-for-missing='exit'",
                                shell = True,
                                bufsize = 100000,
                                stdout = subprocess.PIPE,
