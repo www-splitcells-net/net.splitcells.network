9195692f261bde828d108ce23403a3bde836afe3
Mārtiņš Avots <martins.avots@splitcells.net>
2023-01-18 22:54:31 +0100

    #159 Create alternative test tag.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/testing/IntegrationTest.java
@@ -0,0 +1,19 @@
+package net.splitcells.dem.testing;
+
+
+import net.splitcells.dem.lang.annotations.JavaLegacyArtifact;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag(TestTypes.INTEGRATION_TEST)
+@Test
+@JavaLegacyArtifact
+public @interface IntegrationTest {
+}
---
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/testing/UnitTest.java
@@ -0,0 +1,20 @@
+package net.splitcells.dem.testing;
+
+import net.splitcells.dem.lang.annotations.JavaLegacyArtifact;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static net.splitcells.dem.testing.TestTypes.UNIT_TEST;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag(UNIT_TEST)
+@Test
+@JavaLegacyArtifact
+public @interface UnitTest {
+}
