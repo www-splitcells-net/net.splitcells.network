0be62c5a6a04cdbeb5a958bb722d8f70275e403c
Mārtiņš Avots <martins.avots@splitcells.net>
2023-01-18 22:59:00 +0100

    #170 Test structure of new draft's result.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/constraint/QueryTest.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/constraint/QueryTest.java
@@ -1,11 +1,15 @@
 package net.splitcells.gel.constraint;
 
+import net.splitcells.dem.testing.UnitTest;
 import net.splitcells.gel.Gel;
+import net.splitcells.gel.constraint.type.ForAlls;
+import net.splitcells.gel.rating.rater.classification.ForAllAttributeValues;
 import org.junit.jupiter.api.Test;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.gel.Gel.defineProblem;
 import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+import static net.splitcells.gel.rating.rater.classification.ForAllAttributeValues.forAllAttributeValues;
 
 public class QueryTest {
     @Test
@@ -28,4 +32,27 @@ public class QueryTest {
                 , testData.constraint().childrenView().get(0).childrenView().get(0)
                 , testData.constraint().childrenView().get(0).childrenView().get(0).childrenView().get(0)));
     }
+
+    @UnitTest
+    public void testForWithMultipleClassifiers() {
+        final var d = attribute(Integer.class, "d");
+        final var s = attribute(Integer.class, "s");
+        final var testSubject = defineProblem("testForWithMultipleClassifiers")
+                .withDemandAttributes(d)
+                .withNoDemands()
+                .withSupplyAttributes(s)
+                .withNoSupplies()
+                .withConstraint(r -> {
+                    r.forAll(list(forAllAttributeValues(d), forAllAttributeValues(s))).forAll(forAllAttributeValues(d));
+                    return r;
+                }).toProblem()
+                .asSolution();
+        final var testResult = testSubject.constraint();
+        testResult.childrenView().requireSizeOf(2);
+        testResult.childrenView().get(0).childrenView().requireSizeOf(1);
+        testResult.childrenView().get(1).childrenView().requireSizeOf(1);
+        testResult.childrenView().get(0).childrenView().get(0).childrenView().requireSizeOf(1);
+        testResult.childrenView().get(0).childrenView().get(0).childrenView().get(0).childrenView().requireSizeOf(0);
+    }
+
 }
