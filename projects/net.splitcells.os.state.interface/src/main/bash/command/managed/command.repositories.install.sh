6a475003b8d771353ab021c7821d33f0e7d66133
Mārtiņš Avots <martins.avots@splitcells.net>
2023-02-12 13:22:44 +0100

    #218 Update extended shell style header.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/bash/command/managed/command.repositories.install.sh
+++ projects/net.splitcells.os.state.interface/src/main/bash/command/managed/command.repositories.install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021 Mārtiņš Avots (Martins Avots) and others
+# Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
 #
 # This program and the accompanying materials are made available under the
-# terms of the Eclipse Public License 2.0, which is available at
-# http://www.eclipse.org/legal/epl-2.0, or the MIT License,
-# which is available at https://spdx.org/licenses/MIT.html.
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
 #
-# SPDX-License-Identifier: EPL-2.0 OR MIT
+# This Source Code may also be made available under the following Secondary
+# Licenses when the conditions for such availability set forth in the Eclipse
+# Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+# which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+#
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 
 # TODO Document supported repository file structure.
 # Installs user commands.
