11f3199270b308801ea9dbb9aa82a03ea497e63e
Mārtiņš Avots <martins.avots@splitcells.net>
2023-03-28 23:34:08 +0200

    #170 Determine solver basis.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/repair/RepairConfig.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.solution.optimization.primitive.repair;
+
+import static net.splitcells.dem.utils.random.RandomnessSource.randomness;
+import static net.splitcells.gel.solution.optimization.primitive.repair.GroupSelectors.groupSelector;
+
+public class RepairConfig {
+    public static RepairConfig repairConfig() {
+        return new RepairConfig();
+    }
+
+    private int numberOfGroupsSelectedPerDefiance = 1;
+    private int minimum_constraint_group_path = 1;
+
+    private FluentGroupSelector groupSelector = GroupSelectors.groupSelector(randomness(), minimum_constraint_group_path
+            , numberOfGroupsSelectedPerDefiance);
+    private SupplySelector supplySelector = SupplySelectors.supplySelector();
+    private boolean repairCompliants = true;
+
+    private RepairConfig() {
+
+    }
+
+    public RepairConfig withRepairCompliants(boolean arg) {
+        repairCompliants = arg;
+        return this;
+    }
+
+    public FluentGroupSelector groupSelector() {
+        return groupSelector;
+    }
+
+    public SupplySelector supplySelector() {
+        return supplySelector;
+    }
+
+    public boolean repairCompliants() {
+        return repairCompliants;
+    }
+}
