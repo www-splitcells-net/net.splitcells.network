83cc10ab4e198f3538d7104027850cd2aa8b188e
Mārtiņš Avots <martins.avots@splitcells.net>
2023-04-16 14:35:01 +0200

    #183 Improve runtime of linear initialization for a common case.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/OnlineLinearInitialization.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/OnlineLinearInitialization.java
@@ -24,11 +24,8 @@ import static net.splitcells.dem.data.set.list.Lists.list;
  * Allocates {@link Solution#demandsFree()} and {@link Solution#suppliesFree()} in their respective order.
  */
 public class OnlineLinearInitialization implements OnlineOptimization {
-
-    /**
-     * Setting this bool to true, improves the runtime performance.
-     */
-    private static final boolean GET_NEXT_LINE_BY_STREAM = true;
+    private static final boolean IMPROVE_RUNTIME_GET_NEXT_LINE_BY_STREAM = true;
+    private static final boolean IMPROVE_RUNTIME_GET_NEXT_LINE_BY_CACHE = true;
 
     public static OnlineLinearInitialization onlineLinearInitialization() {
         return new OnlineLinearInitialization();
@@ -40,13 +37,24 @@ public class OnlineLinearInitialization implements OnlineOptimization {
 
     @Override
     public void optimize(Solution solution) {
-        while (solution.demandsFree().hasContent() && solution.suppliesFree().hasContent()) {
-            if (GET_NEXT_LINE_BY_STREAM) {
-                solution.allocate(solution.demandsFree().orderedLinesStream().findFirst().orElseThrow()
-                        , solution.suppliesFree().orderedLinesStream().findFirst().orElseThrow());
-            } else {
-                solution.allocate(solution.demandsFree().orderedLine(0)
-                        , solution.suppliesFree().orderedLine(0));
+        if (IMPROVE_RUNTIME_GET_NEXT_LINE_BY_CACHE) {
+            // This is fast, when free demand and supply set are of equal length.
+            final var freeDemands = solution.demandsFree().orderedLines();
+            final var freeSupplies = solution.suppliesFree().orderedLines();
+            int i = 0;
+            while (freeDemands.hasElements() && freeSupplies.hasElements()) {
+                solution.allocate(freeDemands.get(i++), freeSupplies.get(i++));
+            }
+        } else {
+            while (solution.demandsFree().hasContent() && solution.suppliesFree().hasContent()) {
+                if (IMPROVE_RUNTIME_GET_NEXT_LINE_BY_STREAM) {
+                    // TODO Create faster version of initialization, based on 2 unordered streams.
+                    solution.allocate(solution.demandsFree().orderedLinesStream().findFirst().orElseThrow()
+                            , solution.suppliesFree().orderedLinesStream().findFirst().orElseThrow());
+                } else {
+                    solution.allocate(solution.demandsFree().orderedLine(0)
+                            , solution.suppliesFree().orderedLine(0));
+                }
             }
         }
     }
