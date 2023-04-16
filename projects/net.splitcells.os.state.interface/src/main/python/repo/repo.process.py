ef05fe5738e20e3c389200c786d9ef1d99f4ee81
Mārtiņš Avots <martins.avots@splitcells.net>
2023-04-16 15:29:13 +0200

    #213 Improve commands help message.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.py
+++ projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.py
@@ -154,18 +154,36 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="""
 Generates and executes a set of commands over a set of repositories.
 
-Setting the environment variable 'log_level' to 'debug', enables debug level logging.
+Setting the environment variable `log_level` to `debug`, enables debug level logging.
 
-All command arguments support the variables $subRepo and $peerRepo.
-`$subRepo` is replaced with the relative path from the root --relative-path to the next child repo folder, that will be processed next.
-`$peerRepo` is replaced with the relative path from the root --relative-path to the next peer repo folder, that will be processed next.
-These variables make it easy to create commands, that pull or push a tree of repos to a remote server by constructing the appropriate URLs via these variables.
-The variable `$subRepo` is used mainly to push a tree of git repos to another server.
-This is often useful, when the target SSH server is hosted by oneself and supports complex repo structures.
-The variable `$peerRepo` is used mainly to push a single folder of a git repo.
-This is often useful, when the target server is a public server, where a user often can only manage a single folder of git repos. 
-""")
-	parser.add_argument('--relative-path', dest='relativePath', default='./')
+All arguments starting with `command` support the variables `$subRepo` and `$peerRepo` inside its value.
+These variables make it easy to create commands,
+that pull or push a set of repos to a remote server by constructing the appropriate URLs via these variables.
+
+For illustration purposes, let's assume the follow structure:
+* `~/Documents/main-repo`
+* `~/Documents/main-repo/sub-repo-1`
+* `~/Documents/main-repo/sub-repo-2`
+* `~/Documents/peer-repo-1`
+* `~/Documents/peer-repo-2`
+
+The meaning of the variables:
+* The variable `$subRepo` is used mainly to push a tree of git repos to another server.
+  This is often useful, when the target SSH server is hosted by oneself and the repositories are nested.
+  `$subRepo` is replaced with a relative path from `--relative-path` to the next child repo folder,
+  that will be processed next.
+  For example, given a relative-path of `~/Documents/main-repo` and the command `git clone ssh://user@remote/home/user/Documents/main-repo/$subRepo`,
+  the following commands would be generated `git clone ssh://user@remote/home/user/Documents/main-repo`,
+  `git clone ssh://user@remote/home/user/Documents/main-repo/sub-repo-1` and `git clone ssh://user@remote/home/user/Documents/main-repo/sub-repo-2`.
+* The variable `$peerRepo` is used mainly to push a single folder of git repos.
+  This is often useful, when the target server is a public server, where a user often can only manage a single folder of git repos.
+  `$peerRepo` is replaced with the relative path from `--relative-path` to the next peer repo folder,
+  that will be processed next.
+  For example, a relative-path of `~/Documents/main-repo` and the command `git clone ssh://user@remote/home/user/Documents/main-repo$peerRepo`,
+  could generate the commands `git clone ssh://user@remote/home/user/Documents/main-repo`,
+  `git clone ssh://user@remote/home/user/Documents/peer-repo-1` and `git clone ssh://user@remote/home/user/Documents/peer-repo-2`.
+""", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('--relative-path', dest='relativePath', default='./', help="This is path of the currently processed repo.")
 	parser.add_argument('--host', dest='host', default="''")
 	parser.add_argument('--command', dest='command', required=True)
 	parser.add_argument('--command-for-missing', dest='commandForMissing', required=False)
