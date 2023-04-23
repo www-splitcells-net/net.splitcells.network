02d9103154e87b5bec28d7c673ea623e8f08be05
Mārtiņš Avots <martins.avots@splitcells.net>
2023-04-23 23:30:43 +0200

    #213 Illustrate example in doc.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.py
+++ projects/net.splitcells.os.state.interface/src/main/python/repo/repo.process.py
@@ -152,10 +152,13 @@ if __name__ == '__main__':
 	if environ.get('log_level') == 'debug':
 		logging.basicConfig(level=logging.DEBUG)
 	parser = argparse.ArgumentParser(description="""
-Generates and executes a set of commands over a set of repositories.
+# Summary
+Generates and executes a set of commands over a set of repositories given a set of command patterns.
 
+# Settings via the environment
 Setting the environment variable `log_level` to `debug`, enables debug level logging.
 
+# Default environment variables inside patterns
 All arguments starting with `command` support the variables `$subRepo` and `$peerRepo` inside its value.
 These variables make it easy to create commands,
 that pull or push a set of repos to a remote server by constructing the appropriate URLs via these variables.
@@ -166,6 +169,17 @@ For illustration purposes, let's assume the follow structure:
 * `~/Documents/main-repo/sub-repo-2`
 * `~/Documents/peer-repo-1`
 * `~/Documents/peer-repo-2`
+
+Illustration of the repo structure:
+```
+User home folder
+└── Documents
+    ├── main-repo
+    │   ├── sub-repo-1
+    │   └── sub-repo-2
+    │ peer-repo-1
+    └peer-repo-2
+```
 
 The meaning of the variables:
 * The variable `$subRepo` is used mainly to push a tree of git repos to another server.
