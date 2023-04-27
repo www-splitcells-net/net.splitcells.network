ababfa86dc796da3e574ad1b2bc6c15f17aae051
Mārtiņš Avots <martins.avots@splitcells.net>
2023-04-27 21:33:30 +0200

    #170 Create test case for proposals.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/proposal/ProposalProcessorTest.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/proposal/ProposalProcessorTest.java
@@ -15,13 +15,46 @@
  */
 package net.splitcells.gel.proposal;
 
+import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.gel.Gel;
+import net.splitcells.gel.constraint.Constraint;
 
+import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.testing.Assertions.requireIllegalDefaultConstructor;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+import static net.splitcells.gel.proposal.ProposalProcessor.propose;
+import static net.splitcells.gel.rating.rater.lib.AllSame.allSame;
 
 public class ProposalProcessorTest {
     @UnitTest
     public void testIllegalConstructor() {
         requireIllegalDefaultConstructor(ProposalProcessor.class);
+    }
+
+    @UnitTest
+    public void testProposeViaConstraintPath() {
+        final var demandingAttribute = attribute(Integer.class, "demandingAttribute");
+        final var firstSuppliedAttribute = attribute(Integer.class, "firstSuppliedAttribute");
+        final var secondSuppliedAttribute = attribute(Integer.class, "secondSuppliedAttribute");
+        final var constraintPath = Lists.<Constraint>list();
+        final var testSubject = Gel.defineProblem("testProposeViaConstraintPath")
+                .withDemandAttributes(demandingAttribute)
+                .withDemands(list(list(1), list(1)))
+                .withSupplyAttributes(firstSuppliedAttribute, secondSuppliedAttribute)
+                .withSupplies(list(list(2, 3), list(2, 3), list(2, 4)))
+                .withConstraint(q -> {
+                    constraintPath.addAll(
+                            q.forAll(demandingAttribute)
+                                    .forAll(firstSuppliedAttribute)
+                                    .then(allSame(secondSuppliedAttribute)).constraintPath());
+                    return q;
+                })
+                .toProblem()
+                .asSolution();
+        testSubject.allocate(testSubject.demands().orderedLine(0)
+                , testSubject.supplies().orderedLine(0));
+        final var testProposal = propose(testSubject, list(), testSubject.demandsFree().unorderedLines());
+        testProposal.proposedAllocations().unorderedLines().requireSizeOf(1);
     }
 }
