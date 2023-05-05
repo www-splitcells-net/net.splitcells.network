d0d00cd15a0ab846b5c4c18ab83a3ab7eef1b9c9
Mārtiņš Avots <martins.avots@splitcells.net>
2023-05-05 17:37:07 +0200

    #170 Implement demand selector with proposal integration.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/repair/DemandSelectors.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/primitive/repair/DemandSelectors.java
@@ -17,17 +17,24 @@ package net.splitcells.gel.solution.optimization.primitive.repair;
 
 import net.splitcells.dem.data.set.Set;
 import net.splitcells.dem.data.set.Sets;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.dem.data.set.map.Map;
 import net.splitcells.dem.data.set.map.Maps;
 import net.splitcells.gel.constraint.Constraint;
 import net.splitcells.gel.constraint.GroupId;
+import net.splitcells.gel.constraint.Query;
 import net.splitcells.gel.data.table.Line;
+import net.splitcells.gel.proposal.Proposal;
 import net.splitcells.gel.solution.Solution;
 
+import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.map.Maps.map;
 import static net.splitcells.dem.data.set.map.Pair.pair;
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
 import static net.splitcells.gel.constraint.Constraint.*;
+import static net.splitcells.gel.proposal.ProposalProcessor.propose;
+import static net.splitcells.gel.proposal.Proposals.proposal;
 import static net.splitcells.gel.rating.type.Cost.noCost;
 
 public class DemandSelectors {
@@ -35,6 +42,16 @@ public class DemandSelectors {
         throw constructorIllegal();
     }
 
+    /**
+     * Builds a demand selector for {@link Solution}s,
+     * where {@link GroupId}s are considered,
+     * that contain defying {@link Line}s.
+     *
+     * @param repairCompliants If set to true, selects all {@link Line}s of a given {@link GroupId},
+     *                         if it contains at least one defying {@link Line}.
+     *                         Otherwise, only defying {@link Line}s are considered.
+     * @return
+     */
     public static DemandSelector demandSelector(boolean repairCompliants) {
         return (Constraint constraintGrouping, Solution solution) -> {
             final Map<GroupId, Set<Line>> demandGrouping = map();
@@ -77,5 +94,65 @@ public class DemandSelectors {
         };
     }
 
-
+    /**
+     * Builds a demand selector for {@link Solution}s,
+     * where {@link GroupId}s are considered,
+     * that contain defying {@link Line}s.
+     *
+     * @param repairCompliants          If set to true, selects all {@link Line}s of a given {@link GroupId},
+     *                                  if it contains at least one defying {@link Line}.
+     *                                  Otherwise, only defying {@link Line}s are considered.
+     * @param restrictingConstraintPath Uses this {@link Query#constraintPath} in order to guide the selection via
+     *                                  {@link Constraint#propose(Proposal)}.
+     * @return
+     */
+    public static DemandSelector demandSelector(boolean repairCompliants, List<Constraint> restrictingConstraintPath) {
+        return (Constraint constraintGrouping, Solution solution) -> {
+            final Map<GroupId, Set<Line>> demandGrouping = map();
+            final Map<GroupId, Set<Line>> defianceCache = Maps.map();
+            constraintGrouping
+                    .lineProcessing()
+                    .unorderedLines()
+                    .stream()
+                    /**
+                     * TODO HACK This is code duplication.
+                     * It reimplements part of {@link ConstraintGroupBasedRepair#freeDefyingGroupOfConstraintGroup}.
+                     */
+                    .filter(processing -> {
+                        final var group = processing.value(INCOMING_CONSTRAINT_GROUP);
+                        return !defianceCache.computeIfAbsent(group, g -> constraintGrouping.defying(g)).isEmpty();
+                    })
+                    /**
+                     * TODO HACK This is code duplication.
+                     * It reimplements part of {@link ConstraintGroupBasedOfflineRepair#freeDefyingGroupOfConstraintGroup}.
+                     */
+                    .filter(processing -> {
+                        if (!repairCompliants) {
+                            return !processing.value(RATING).equalz(noCost());
+                        }
+                        return true;
+                    })
+                    .filter(processing -> {
+                        final var demandProcessing = solution.demandOfAllocation(processing.value(Constraint.LINE));
+                        final var proposal = propose(solution, restrictingConstraintPath, list(demandProcessing));
+                        if (proposal.proposedAllocations().demands().orderedLines().contains(demandProcessing)) {
+                            return true;
+                        }
+                        return false;
+                    })
+                    .map(processing -> pair(processing.value(Constraint.RESULTING_CONSTRAINT_GROUP)
+                            , processing.value(LINE)))
+                    .forEach(processing -> {
+                        final Set<Line> group;
+                        if (!demandGrouping.containsKey(processing.getKey())) {
+                            group = Sets.setOfUniques();
+                            demandGrouping.put(processing.getKey(), group);
+                        } else {
+                            group = demandGrouping.get(processing.getKey());
+                        }
+                        group.with(solution.demandOfAllocation(processing.getValue()));
+                    });
+            return demandGrouping;
+        };
+    }
 }
