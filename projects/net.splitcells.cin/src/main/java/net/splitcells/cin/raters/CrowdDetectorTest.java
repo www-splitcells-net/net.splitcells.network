5c6561fe52eb1e4f95ab69750d9128bdb7fdaacf
Mārtiņš Avots <martins.avots@splitcells.net>
2023-05-19 17:52:26 +0200

    #170 Test crowd detector.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/java/net/splitcells/cin/raters/CrowdDetectorTest.java
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/raters/CrowdDetectorTest.java
@@ -16,12 +16,60 @@
 package net.splitcells.cin.raters;
 
 import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.gel.Gel;
 
+import static net.splitcells.cin.raters.CrowdDetector.crowdDetector;
+import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.testing.Assertions.requireIllegalDefaultConstructor;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
 
 public class CrowdDetectorTest {
     @UnitTest
     public void testIllegalDefaultConstructor() {
         requireIllegalDefaultConstructor(CrowdDetector.class);
+    }
+
+    @UnitTest
+    public void test() {
+        final var player = attribute(Integer.class, "player");
+        final var time = attribute(Integer.class, "time");
+        final var testSubject = Gel.defineProblem("testProposeViaConstraintPath")
+                .withDemandAttributes(player)
+                .withDemands(list(list(1)
+                        , list(1)
+                        , list(1)
+                        , list(1)
+                        , list(1)
+                ))
+                .withSupplyAttributes(time)
+                .withSupplies(list(list(2)
+                        , list(1)
+                        , list(2)
+                        , list(1)
+                        , list(1)
+                ))
+                .withConstraint(q -> {
+                    q.then(crowdDetector(1, player, time, s -> s == 2, "test"));
+                    return q;
+                })
+                .toProblem()
+                .asSolution();
+        testSubject.allocate(testSubject.demandsFree().orderedLine(0)
+                , testSubject.suppliesFree().orderedLine(0));
+        testSubject.constraint().rating().requireEqualsTo(cost(1));
+        testSubject.allocate(testSubject.demandsFree().orderedLine(0)
+                , testSubject.suppliesFree().orderedLine(0));
+        testSubject.constraint().rating().requireEqualsTo(cost(2));
+        testSubject.allocate(testSubject.demandsFree().orderedLine(0)
+                , testSubject.suppliesFree().orderedLine(0));
+        testSubject.constraint().rating().requireEqualsTo(cost(3));
+        testSubject.allocate(testSubject.demandsFree().orderedLine(0)
+                , testSubject.suppliesFree().orderedLine(0));
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        testSubject.allocate(testSubject.demandsFree().orderedLine(0)
+                , testSubject.suppliesFree().orderedLine(0));
+        testSubject.constraint().rating().requireEqualsTo(cost(5));
     }
 }
