602cb0532b51001172ceefa256c2595a98d7aa82
Mārtiņš Avots <martins.avots@splitcells.net>
2023-06-21 20:54:51 +0200

    #170 Prepare test for next implementation step.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationssTest.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/allocation/AllocationssTest.java
@@ -23,6 +23,7 @@ import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.object.Discoverable.NO_CONTEXT;
 import static net.splitcells.dem.testing.Assertions.requireIllegalDefaultConstructor;
 import static net.splitcells.gel.data.allocation.Allocationss.allocations;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 
 public class AllocationssTest {
     @UnitTest
@@ -32,12 +33,23 @@ public class AllocationssTest {
 
     @UnitTest
     public void testHeader() {
-        final var attribute1 = AttributeI.attribute(Integer.class, "1");
-        final var attribute2 = AttributeI.attribute(Integer.class, "2");
-        final var attribute3 = AttributeI.attribute(Integer.class, "3");
-        final var attribute4 = AttributeI.attribute(Integer.class, "4");
+        final var attribute1 = attribute(Integer.class, "1");
+        final var attribute2 = attribute(Integer.class, "2");
+        final var attribute3 = attribute(Integer.class, "3");
+        final var attribute4 = attribute(Integer.class, "4");
         final var testSubject = allocations("testHeader", NO_CONTEXT
                 , list(attribute1, attribute2), list(attribute3, attribute4));
         testSubject.headerView().requireEqualityTo(list(attribute1, attribute2, attribute3, attribute4));
+    }
+    
+    public void testAllocations() {
+        final var attribute1 = attribute(Integer.class, "1");
+        final var attribute2 = attribute(Integer.class, "2");
+        final var testSubject = allocations("testAllocations", NO_CONTEXT
+                , list(attribute1), list(attribute2));
+        testSubject.demands().addTranslated(list(1));
+        testSubject.supplies().addTranslated(list(1));
+        testSubject.allocate(testSubject.demands().orderedLine(0)
+                , testSubject.supplies().orderedLine(0));
     }
 }
