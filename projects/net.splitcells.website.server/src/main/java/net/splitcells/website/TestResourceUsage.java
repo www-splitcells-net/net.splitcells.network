6e1ffc711e8fcfe82908acc63f9eaa7508f1f42d
Mārtiņš Avots <martins.avots@splitcells.net>
2023-07-11 23:23:30 +0200

    #170 Demonstrate how to load resources.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/TestResourceUsage.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.website;
+
+import java.io.IOException;
+
+import static net.splitcells.dem.utils.ExecutionException.executionException;
+
+public class TestResourceUsage {
+    public static void main(String... args) {
+        try {
+            System.out.println(TestResourceUsage.class.getResourceAsStream("/net/splitcells/website/server/index.xml")
+                    .readAllBytes());
+        } catch (IOException e) {
+            throw executionException(e);
+        }
+    }
+}
