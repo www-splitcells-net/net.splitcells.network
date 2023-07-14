e61d5c3512be4b72c049ed335767bdd2a9cd856b
Mārtiņš Avots <martins.avots@splitcells.net>
2023-07-14 07:54:30 +0200

    #249 Remove deprecate usage of file access.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/translation/to/html/PathBasedUriResolver.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/translation/to/html/PathBasedUriResolver.java
@@ -55,49 +55,38 @@ public class PathBasedUriResolver implements URIResolver {
 
     @Override
     public Source resolve(String href, String base) throws TransformerException {
-        try {
-            final var extensionResult = extension.apply(href);
-            final InputStream inputStream;
-            if (extensionResult.isPresent()) {
-                final var extensionResolution = new StreamSource
-                        (new ByteArrayInputStream(extensionResult.get().getBytes(StandardCharsets.UTF_8)));
-                /*
-                 * Setting systemId to the underlying file in order to resolve relative paths
-                 * used in the return value.
-                 */
-                extensionResolution.setSystemId(Paths.get(href).toString());
-                return extensionResolution;
-            }
-            if (href.startsWith("/net.splitcells.website/current/xml")) {
-                /*
-                 * It is expected, that the website server is executed in a project.
-                 * This project may contain configs at `/src/main/xml//net.splitcells.website/current/xml/**`.
-                 * TODO The current implementation for this is a hack.
-                 */
-                inputStream = configFiles.inputStream(net.splitcells.dem.resource.Paths
-                        .path(href.substring("/net.splitcells.website/current/xml".length())));
-            } else if (Paths.get(href).isAbsolute()) {
-                /*
-                 * Absolute path are resolved to the host's filesystem.
-                 * TODO Remove this, because this will only cause problems in future.
-                 * Keep in mind, that this functionality is currently used in `variable.location.xsl`.
-                 */
-                inputStream = newInputStream(Paths.get(href));
-            } else {
-                final var rVal = new StreamSource(folder.inputStream(Paths.get(href)));
-                rVal.setSystemId(Paths.get(href).toString());
-                return rVal;
-            }
-            final var rVal = new StreamSource(inputStream);
+        final var extensionResult = extension.apply(href);
+        final InputStream inputStream;
+        if (extensionResult.isPresent()) {
+            final var extensionResolution = new StreamSource
+                    (new ByteArrayInputStream(extensionResult.get().getBytes(StandardCharsets.UTF_8)));
             /*
              * Setting systemId to the underlying file in order to resolve relative paths
              * used in the return value.
              */
+            extensionResolution.setSystemId(Paths.get(href).toString());
+            return extensionResolution;
+        }
+        if (href.startsWith("/net.splitcells.website/current/xml")) {
+            /*
+             * It is expected, that the website server is executed in a project.
+             * This project may contain configs at `/src/main/xml//net.splitcells.website/current/xml/**`.
+             * TODO The current implementation for this is a hack.
+             */
+            inputStream = configFiles.inputStream(net.splitcells.dem.resource.Paths
+                    .path(href.substring("/net.splitcells.website/current/xml".length())));
+        } else {
+            final var rVal = new StreamSource(folder.inputStream(Paths.get(href)));
             rVal.setSystemId(Paths.get(href).toString());
             return rVal;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        final var rVal = new StreamSource(inputStream);
+        /*
+         * Setting systemId to the underlying file in order to resolve relative paths
+         * used in the return value.
+         */
+        rVal.setSystemId(Paths.get(href).toString());
+        return rVal;
     }
 
 }
