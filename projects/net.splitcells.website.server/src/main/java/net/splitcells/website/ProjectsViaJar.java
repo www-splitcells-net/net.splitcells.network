93848eaff20bec1c8650edc2c6ceed9c0102e75a
Mārtiņš Avots <martins.avots@splitcells.net>
2023-07-26 21:39:06 +0200

    #249 Create minimal Projects alternative to work on API migration.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/ProjectsViaJar.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.website;
+
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.project.ProjectRenderer;
+import net.splitcells.website.server.project.validator.SourceValidator;
+import net.splitcells.website.server.projects.ProjectsRendererI;
+
+import java.nio.file.Path;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.resource.FileSystems.fileSystemOnLocalHost;
+import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
+import static net.splitcells.website.server.project.ProjectRenderer.projectRenderer;
+import static net.splitcells.website.server.project.validator.SourceValidator.VOID_VALIDATOR;
+import static net.splitcells.website.server.project.validator.SourceValidatorViaSchema.validatorViaSchema;
+
+public class ProjectsViaJar {
+    private ProjectsViaJar() {
+        throw constructorIllegal();
+    }
+
+    public static ProjectsRendererI projectsRenderer(Config config) {
+        final var profile = "public";
+        final var projectsRepository = config.mainProjectRepositoryPath().orElse(Path.of("../"));
+        final var validator = config.xmlSchema().map(s -> (SourceValidator) validatorViaSchema(s))
+                .orElse(VOID_VALIDATOR);
+        return projectsRenderer(projectsRepository
+                , profile
+                , fallbackProjectRenderer(profile, projectsRepository, validator, config)
+                , list()
+                , validator
+                , config);
+    }
+
+    public static ProjectsRendererI projectsRenderer(Path projectRepository, String profile
+            , ProjectRenderer fallbackProjectRenderer
+            , List<ProjectRenderer> additionalProjects
+            , SourceValidator sourceValidator
+            , Config config) {
+        final var xslLib = projectRepository
+                .resolve("net.splitcells.website.content.default/src/main/xsl/net/splitcells/website/den/translation/to/html/");
+        return ProjectsRendererI.projectsRenderer(profile, fallbackProjectRenderer, additionalProjects
+                , config);
+    }
+
+    public static ProjectRenderer fallbackProjectRenderer(String profile, Path projectRepositories
+            , SourceValidator sourceValidator
+            , Config config) {
+        return projectRenderer(profile
+                , fileSystemOnLocalHost(projectRepositories.resolve("net.splitcells.website.content.default/"))
+                , fileSystemOnLocalHost(projectRepositories.resolve("net.splitcells.website.content.default/src/main/xsl/net/splitcells/website/den/translation/to/html/"))
+                , fileSystemOnLocalHost(projectRepositories.resolve("net.splitcells.website.content.default/src/main/resources/content"))
+                , "/"
+                , sourceValidator
+                , config);
+    }
+}
