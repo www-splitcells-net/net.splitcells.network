6aa021a0ff34e8f9e04826e49603ba07da8aac55
Mārtiņš Avots <martins.avots@splitcells.net>
2023-08-15 15:50:32 +0200

    #249 Improve compatibility to Intellij,
    by liquidating system root project.
    
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/build.part.with.java
+++ bin/build.part.with.java
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+#
 # This command exists, so that the project can be build with one command.
 # This makes usage of tools building this project easier,
 # as it can be hard to understand in such tools,
 # how to chain commands calls.
 set -e
-cd projects/net.splitcells.system.root
-./bin/build.part.with.java
+chmod +x ./bin/*
+export JAVA_VERSION=11 # This is required on FreeBSD, if an older Java version is set as default.
+mvn clean install
