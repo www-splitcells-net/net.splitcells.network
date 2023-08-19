3bc844de110cb9fb541aa6bb591ac6ef9cfba101
Mārtiņš Avots <martins.avots@splitcells.net>
2023-08-20 00:56:21 +0200

    Fix logging of network builder.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.network.worker/src/main/java/net/splitcells/network/worker/Builder.java
+++ projects/net.splitcells.network.worker/src/main/java/net/splitcells/network/worker/Builder.java
@@ -17,9 +17,12 @@ package net.splitcells.network.worker;
 
 import net.splitcells.dem.Dem;
 import net.splitcells.dem.environment.config.ProgramName;
+import net.splitcells.dem.resource.FileSystems;
 import net.splitcells.dem.resource.host.SystemUtils;
 
+import static net.splitcells.dem.Dem.config;
 import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.resource.FileSystems.fileSystemOnLocalHost;
 import static net.splitcells.dem.resource.Paths.userHome;
 import static net.splitcells.dem.testing.Test.testUnits;
 import static net.splitcells.dem.utils.ConstructorIllegal.constructorIllegal;
@@ -34,7 +37,11 @@ public class Builder {
             throw new IllegalArgumentException("Exactly one argument is required, but " + args.length + " were given. The argument is the id of the test executor.");
         }
         Dem.process(() -> testUnits(list(logger()))
-                , env -> env.config().withConfigValue(ProgramName.class, args[0]));
+                , env -> env.config().withConfigValue(ProgramName.class, args[0])
+                        .withConfigValue(NetworkLog.class
+                                , fileSystemOnLocalHost(config()
+                                        .configValue(ProjectsFolder.class)
+                                        .resolve("net.splitcells.network.log"))));
     }
 
     private Builder() {
