6ed6224fd1da192dd9c2dded5723d29728f1f198
Mārtiņš Avots <martins.avots@splitcells.net>
2023-10-10 22:36:34 +0200

    #252 Parse demand attributes.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ext/src/main/java/net/splitcells/gel/problem/ProblemParser.java
+++ projects/net.splitcells.gel.ext/src/main/java/net/splitcells/gel/problem/ProblemParser.java
@@ -18,15 +18,18 @@ package net.splitcells.gel.problem;
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.gel.data.table.attribute.Attribute;
+import net.splitcells.gel.data.table.attribute.AttributeI;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.list.Lists.toList;
+import static net.splitcells.dem.utils.ExecutionException.executionException;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 import static net.splitcells.gel.constraint.type.ForAlls.forAll;
 import static net.splitcells.gel.data.assignment.Assignmentss.assignments;
 import static net.splitcells.gel.data.database.Databases.database;
+import static net.splitcells.gel.data.table.attribute.AttributeI.attribute;
 import static net.splitcells.gel.problem.ProblemI.problem;
 
 public class ProblemParser {
@@ -42,12 +45,39 @@ public class ProblemParser {
         names.requireSizeOf(1);
         final var name = names.get(0).getText();
         final List<Attribute<? extends Object>> demandAttributes = list();
+        final var demandDefinition = source_unit.statement().stream()
+                .filter(vd -> vd.variable_definition() != null)
+                .filter(vd -> vd.variable_definition().Name().getText().equals("demands"))
+                .collect(toList());
+        demandDefinition.requireSizeOf(1);
+        final var firstDemandAttribute = demandDefinition.get(0).variable_definition().map().variable_definition();
+        if (firstDemandAttribute != null) {
+            demandAttributes.add(
+                    parseAttribute(firstDemandAttribute.Name().getText()
+                            , firstDemandAttribute.function_call().Name().getText()));
+        }
+        final var additionalDemandAttributes = demandDefinition.get(0).variable_definition().map().statement_reversed();
+        additionalDemandAttributes.forEach(da -> demandAttributes
+                .add(parseAttribute(da.variable_definition().Name().getText()
+                        , da.variable_definition().function_call().Name().getText())));
         final List<Attribute<? extends Object>> supplyAttributes = list();
         final var demands = database(demandAttributes);
         final var supplies = database(supplyAttributes);
         final var assignments = assignments(name, demands, supplies);
         problem(assignments, constraints);
         return null;
+    }
+
+    private static Attribute<? extends Object> parseAttribute(String name, String type) {
+        if (type.equals("int")) {
+            return attribute(Integer.class, name);
+        } else if (type.equals("float")) {
+            return attribute(Float.class, name);
+        } else if (type.equals("string")) {
+            return attribute(String.class, name);
+        } else {
+            throw executionException(type);
+        }
     }
 
     private ProblemParser() {
