613aab471b53776dfe13c341f69a76d549202f9c
Mārtiņš Avots <martins.avots@splitcells.net>
2023-10-15 17:25:59 +0200

    #252 Migrate constraint parsing to dedicated parser.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ext/src/main/java/net/splitcells/gel/problem/ProblemParser.java
+++ projects/net.splitcells.gel.ext/src/main/java/net/splitcells/gel/problem/ProblemParser.java
@@ -17,6 +17,7 @@ package net.splitcells.gel.problem;
 
 import net.splitcells.dem.data.set.list.List;
 import net.splitcells.gel.constraint.Constraint;
+import net.splitcells.gel.constraint.ConstraintParser;
 import net.splitcells.gel.data.database.Database;
 import net.splitcells.gel.data.table.attribute.Attribute;
 import net.splitcells.dem.lang.perspective.antlr4.DenParserBaseVisitor;
@@ -27,6 +28,7 @@ import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.list.Lists.toList;
 import static net.splitcells.dem.testing.Assertions.requireEquals;
 import static net.splitcells.dem.utils.ExecutionException.executionException;
+import static net.splitcells.gel.constraint.ConstraintParser.parseConstraint;
 import static net.splitcells.gel.constraint.type.ForAlls.forAll;
 import static net.splitcells.gel.constraint.type.ForAlls.forEach;
 import static net.splitcells.gel.data.assignment.Assignmentss.assignments;
@@ -48,9 +50,10 @@ public class ProblemParser extends DenParserBaseVisitor<Problem> {
     }
 
     @Override
-    public Problem visitSource_unit(net.splitcells.dem.lang.perspective.antlr4.DenParser.Source_unitContext ctx) {
-        visitChildren(ctx);
+    public Problem visitSource_unit(net.splitcells.dem.lang.perspective.antlr4.DenParser.Source_unitContext sourceUnit) {
+        visitChildren(sourceUnit);
         final var assignments = assignments(name, demands, supplies);
+        parseConstraint(sourceUnit, assignments);
         return null;
     }
 
@@ -94,57 +97,8 @@ public class ProblemParser extends DenParserBaseVisitor<Problem> {
                     .add(parseAttribute(sa.variable_definition().Name().getText()
                             , sa.variable_definition().function_call().Name().getText())));
             supplies = database(supplyAttributes);
-        } else if (ctxName.equals("constraints")) {
-            constraints = parseConstraint(ctx);
         }
         return null;
-    }
-
-    private Constraint parseConstraint(net.splitcells.dem.lang.perspective.antlr4.DenParser.Variable_definitionContext variableDefinition) {
-        final var constraintName = variableDefinition.function_call().Name().getText();
-        if (!variableDefinition.function_call().access().isEmpty()) {
-            throw executionException("Function chaining is not supported for constraint definition yet.");
-        }
-        if (constraintName.equals("forAll")) {
-            if (variableDefinition.function_call().function_call_arguments().function_call_arguments_element() != null) {
-                throw executionException("ForAll does not support arguments");
-            }
-            return forAll();
-        } else if (constraintName.equals("forEach")) {
-            if (variableDefinition.function_call().function_call_arguments().function_call_arguments_element() != null
-                    && variableDefinition.function_call().function_call_arguments().function_call_arguments_next().isEmpty()) {
-                if (!variableDefinition.function_call().function_call_arguments().function_call_arguments_element().function_call().isEmpty()) {
-                    throw executionException("Function call argument are not supported for forEach constraint.");
-                }
-                final var attributeName = variableDefinition.function_call()
-                        .function_call_arguments()
-                        .function_call_arguments_element()
-                        .Name()
-                        .getText();
-                final var demandAttributeMatches = demands.headerView().stream()
-                        .filter(da -> da.name().equals(attributeName))
-                        .collect(toList());
-                final var supplyAttributeMatches = supplies.headerView().stream()
-                        .filter(sa -> sa.name().equals(attributeName))
-                        .collect(toList());
-                requireEquals(demandAttributeMatches.size() + supplyAttributeMatches.size(), 1);
-                final Attribute<? extends Object> matchedAttribute;
-                if (demandAttributeMatches.hasElements()) {
-                    matchedAttribute = demandAttributeMatches.get(0);
-                } else if (supplyAttributeMatches.hasElements()) {
-                    matchedAttribute = supplyAttributeMatches.get(0);
-                } else {
-                    throw executionException("Invalid program state.");
-                }
-                return forEach(matchedAttribute);
-            }
-            if (!variableDefinition.function_call().function_call_arguments().function_call_arguments_next().isEmpty()) {
-                throw executionException("ForEach does not support multiple arguments.");
-            }
-            throw executionException("Invalid program state.");
-        } else {
-            throw executionException("Unknown constraint name: " + constraintName);
-        }
     }
 
     private Attribute<? extends Object> parseAttribute(String name, String type) {
