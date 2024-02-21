933b1df3c56360273939de37791ba08ad53ae86e
Mārtiņš Avots <martins.avots@splitcells.net>
2024-02-22 00:25:04 +0100

    #252 Mark changelog as technical
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.system/src/main/java/net/splitcells/system/WebsiteViaJar.java
+++ projects/net.splitcells.system/src/main/java/net/splitcells/system/WebsiteViaJar.java
@@ -107,7 +107,7 @@ public class WebsiteViaJar {
                 .withAdditionalCssFile("net/splitcells/website/css/layout.default.css")
                 .withAdditionalCssFile("net/splitcells/website/css/theme.css")
                 .withAdditionalCssFile("net/splitcells/website/css/tabulator.min.css")
-                .withAdditionalProgramConfig(programConfig("Splitcells Network Project Documentation"
+                .withAdditionalProgramConfig(programConfig("Splitcells Network Technical Project Documentation"
                         , "/net/splitcells/network/README")
                         .withLogoPath(Optional.of("net/splitcells/website/images/thumbnail/medium/community.2016.12.11.chrom.0.dina4.jpg"))
                         .withDescription(Optional.of("We provide an open source ecosystem centered around optimization and operations research.")))
