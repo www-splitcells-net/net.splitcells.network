b8f4ac4e367b82cf699c4663c403892259547a40
Mārtiņš Avots <martins.avots@splitcells.net>
2024-03-09 00:48:03 +0100

    #170 Create basis for cell based web server
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/ServerService.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/ServerService.java
@@ -16,79 +16,83 @@
 package net.splitcells.website.server;
 
 import net.splitcells.dem.Dem;
+import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.environment.resource.ResourceOptionI;
 import net.splitcells.dem.environment.resource.Service;
+import net.splitcells.website.content.defaults.WebsiteContentDefaultsFileSystem;
+import net.splitcells.website.server.project.ProjectRenderer;
 import net.splitcells.website.server.project.renderer.ObjectsMediaRenderer;
 import net.splitcells.website.server.project.renderer.ObjectsRenderer;
+import net.splitcells.website.server.project.validator.SourceValidator;
+import net.splitcells.website.server.projects.ProjectsRenderer;
+import net.splitcells.website.server.projects.ProjectsRendererI;
 
+import java.nio.file.Path;
 import java.util.Optional;
+import java.util.function.Function;
 
+import static net.splitcells.dem.Dem.configValue;
 import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.data.set.list.Lists.toList;
 import static net.splitcells.dem.resource.FileSystems.fileSystemOnLocalHost;
 import static net.splitcells.dem.resource.Files.readFileAsString;
 import static net.splitcells.dem.resource.Paths.userHome;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 import static net.splitcells.website.Projects.projectsRenderer;
 import static net.splitcells.website.server.project.ProjectRenderer.projectRenderer;
+import static net.splitcells.website.server.project.validator.SourceValidator.VOID_VALIDATOR;
 import static net.splitcells.website.server.project.validator.SourceValidatorViaSchema.validatorViaSchema;
 
 public class ServerService extends ResourceOptionI<Service> {
     public ServerService() {
         super(() -> {
-            // TODO This is a hack, because the webserver still depends on private documents, in order to render the website.
-            final var publicProjectRepository = userHome(
-                    "Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network/projects");
-            final var privateProjectRepository = userHome("Documents/projects/net.splitcells.martins.avots.support.system/private");
-            final var xslLib = publicProjectRepository.resolve(
-                    "net.splitcells.website.content.default/src/main/xsl/net/splitcells/website/den/translation/to/html/");
-            final var validator = validatorViaSchema(
-                    publicProjectRepository.resolve("net.splitcells.website.server").resolve("src/main/xsd/den.xsd"));
-            final var config = Config.create()
-                    .withOpenPort(8448)
-                    .withIsSecured(false)
-                    .withSiteFolder(Optional.of(userHome("Documents/projects/net.splitcells.martins.avots.support.system/public/net.splitcells.network/projects/").toString()))
-                    .withDetailedXslMenu(Optional.of(readFileAsString(userHome("Documents/projects/net.splitcells.martins.avots.support.system/private/net.splitcells.martins.avots.website/src/main/resources/detailed-menu.xsl"))))
-                    .withCssFiles(list("net/splitcells/website/css/theme.white.variables.css"
-                            , "net/splitcells/website/css/basic.themed.css"
-                            , "net/splitcells/website/css/basic.css"
-                            , "net/splitcells/website/css/den.css"
-                            , "net/splitcells/website/css/layout.default.css"
-                            , "net/splitcells/martins/avots/website/css/theme.css"));
-            /* TODO The config should only be read during service start,
-             * in order to be sure, that the config is set up.
-             */
-            return projectsRenderer(publicProjectRepository, "public"
-                    , projectsRenderer -> projectRenderer(
-                            "public", fileSystemOnLocalHost(privateProjectRepository.resolve("net.splitcells.martins.avots.website/"))
-                            , fileSystemOnLocalHost(xslLib)
-                            , fileSystemOnLocalHost(privateProjectRepository
-                                    .resolve("net.splitcells.martins.avots.website/src/main/resources/html"))
-                            , "/"
-                            , validator
-                            , config
-                            , projectsRenderer)
-                    , projectsRenderer -> list(projectRenderer("public"
-                                    , fileSystemOnLocalHost(privateProjectRepository.resolve("net.splitcells.martins.avots.website/"))
-                                    , fileSystemOnLocalHost(xslLib)
-                                    , fileSystemOnLocalHost(privateProjectRepository
-                                            .resolve("net.splitcells.martins.avots.website/src/main/resources/html"))
-                                    , "/"
-                                    , validator
-                                    , config)
-                            , projectRenderer("public"
-                                    , fileSystemOnLocalHost(privateProjectRepository.resolve(
-                                            "/home/splitcells/Documents/projects/net.splitcells.martins.avots.support.system/private/net.splitcells.martins.avots.website/"))
-                                    , fileSystemOnLocalHost(xslLib)
-                                    , fileSystemOnLocalHost(privateProjectRepository
-                                            .resolve("net.splitcells.martins.avots.website/src/main/resources/html"))
-                                    , "/"
-                                    , validator
-                                    , config)
-                            , Dem.configValue(ObjectsRenderer.class)
-                            , Dem.configValue(ObjectsMediaRenderer.class))
-                    , validator
-                    , config)
-                    .httpServer();
+            final var config = Config.create();
+            return projectsRenderer(config).httpServer();
         });
+    }
+
+    private static ProjectsRendererI projectsRenderer(Config config) {
+        final var profile = "public";
+        final var validator = VOID_VALIDATOR;
+        return projectsRenderer(profile
+                , projectsRenderer -> fallbackProjectRenderer(projectsRenderer, profile, validator, config)
+                , projectsRenderer -> config.additionalProjects().stream()
+                        .map(project ->
+                                projectRenderer(profile
+                                        , project.projectFiles()
+                                        , configValue(WebsiteContentDefaultsFileSystem.class)
+                                                .subFileSystemView("src/main/xsl/net/splitcells/website/den/translation/to/html/")
+                                        , configValue(WebsiteContentDefaultsFileSystem.class)
+                                                .subFileSystemView("src/main/resources/html")
+                                        , project.rootPath()
+                                        , validator
+                                        , config
+                                        , projectsRenderer))
+                        .collect(toList())
+                        .withAppended(Dem.configValue(ObjectsRenderer.class)
+                                , Dem.configValue(ObjectsMediaRenderer.class))
+                , config);
+    }
+
+    private static ProjectsRendererI projectsRenderer(String profile
+            , Function<ProjectsRenderer, ProjectRenderer> fallbackProjectRenderer
+            , Function<ProjectsRenderer, List<ProjectRenderer>> additionalProjects
+            , Config config) {
+        return ProjectsRendererI.projectsRenderer(profile, fallbackProjectRenderer, additionalProjects, config);
+    }
+
+    private static ProjectRenderer fallbackProjectRenderer(ProjectsRenderer projectsRenderer, String profile
+            , SourceValidator sourceValidator
+            , Config config) {
+        return projectRenderer(profile
+                , configValue(WebsiteContentDefaultsFileSystem.class)
+                , configValue(WebsiteContentDefaultsFileSystem.class)
+                        .subFileSystemView("src/main/xsl/net/splitcells/website/den/translation/to/html/")
+                , configValue(WebsiteContentDefaultsFileSystem.class)
+                        .subFileSystemView("src/main/resources/html")
+                , "/"
+                , sourceValidator
+                , config
+                , projectsRenderer);
     }
 }
