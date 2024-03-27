2d9363bdae9b84c8d9dd3d3050965bc81d11c60b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-03-27 18:44:25 +0100

    #170 Define System Cell
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.system/src/main/java/net/splitcells/system/SystemCell.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.system;
+
+import net.splitcells.dem.environment.Cell;
+import net.splitcells.dem.environment.Environment;
+import net.splitcells.website.server.WebsiteServerCell;
+
+import static net.splitcells.dem.Dem.serve;
+
+public class SystemCell implements Cell {
+
+    public static void main(String... args) {
+        serve(SystemCell.class);
+    }
+    @Override
+    public String groupId() {
+        return "net.splitcells";
+    }
+
+    @Override
+    public String artifactId() {
+        return "system";
+    }
+
+    @Override
+    public void accept(Environment env) {
+        env.withCell(WebsiteServerCell.class);
+    }
+}
