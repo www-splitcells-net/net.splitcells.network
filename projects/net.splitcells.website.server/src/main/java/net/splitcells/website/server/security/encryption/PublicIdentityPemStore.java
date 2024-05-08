aca5003fd8bc87a5df8ea5bca6815096c6de6b20
Mārtiņš Avots <martins.avots@splitcells.net>
2024-05-09 00:28:19 +0200

    #c11 Create dedicated package for encryption
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/security/encryption/PublicIdentityPemStore.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/security/encryption/PublicIdentityPemStore.java
@@ -13,7 +13,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
-package net.splitcells.website.server.security;
+package net.splitcells.website.server.security.encryption;
 
 import net.splitcells.dem.environment.config.framework.Option;
 
