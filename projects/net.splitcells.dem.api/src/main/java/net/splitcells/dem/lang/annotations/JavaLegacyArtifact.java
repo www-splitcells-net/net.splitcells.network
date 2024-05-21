0a5c0013252fb0e710bdcbfa0b09ba010b598064
Mārtiņš Avots <martins.avots@splitcells.net>
2024-05-21 22:27:25 +0200

    #c11 Fix JavaDoc
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/annotations/JavaLegacyArtifact.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/annotations/JavaLegacyArtifact.java
@@ -17,7 +17,7 @@ package net.splitcells.dem.lang.annotations;
 
 /**
  * <p>Marks things that are only present for Java compatibility.
- * Code marked as such is not checked for portability automatically.</p
+ * Code marked as such is not checked for portability automatically.</p>
  */
 public @interface JavaLegacyArtifact {
 
