31987c3628f44d8afedd33cbed807e7ef886910b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-06-07 11:43:27 +0200

    #c24 Create query parser
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/no/code/editor/NoCodeQueryParser.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.ui.no.code.editor;
+
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.lang.perspective.Perspective;
+import net.splitcells.dem.lang.perspective.antlr4.DenParser;
+import net.splitcells.dem.lang.perspective.no.code.antlr4.NoCodeDenParser;
+import net.splitcells.dem.lang.perspective.no.code.antlr4.NoCodeDenParserBaseVisitor;
+import net.splitcells.dem.testing.Result;
+import net.splitcells.gel.constraint.Query;
+import net.splitcells.gel.data.assignment.Assignments;
+import net.splitcells.gel.data.table.attribute.Attribute;
+import net.splitcells.gel.data.table.attribute.Attributes;
+import net.splitcells.gel.ui.QueryParser;
+
+import java.util.Optional;
+
+import static java.util.stream.IntStream.range;
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.data.set.list.Lists.toList;
+import static net.splitcells.dem.lang.perspective.PerspectiveI.perspective;
+import static net.splitcells.dem.object.Discoverable.NO_CONTEXT;
+import static net.splitcells.dem.testing.Result.result;
+import static net.splitcells.gel.constraint.QueryI.query;
+import static net.splitcells.gel.constraint.type.ForAll.FOR_ALL_NAME;
+import static net.splitcells.gel.constraint.type.ForAlls.*;
+import static net.splitcells.gel.constraint.type.ForAlls.FOR_ALL_COMBINATIONS_OF;
+import static net.splitcells.gel.constraint.type.Then.THEN_NAME;
+import static net.splitcells.gel.ui.RaterParser.parseRater;
+import static net.splitcells.gel.ui.no.code.editor.NoCodeRaterParser.parseNoCodeRater;
+
+public class NoCodeQueryParser extends NoCodeDenParserBaseVisitor<Result<Query, Perspective>> {
+    private static final String AFFECTED_CONTENT = "affected content";
+
+    public static Result<Query, Perspective> parseQuery(NoCodeDenParser.Source_unitContext sourceUnit
+            , Assignments assignments) {
+        final var parser = new NoCodeQueryParser(assignments);
+        parser.visitSource_unit(sourceUnit);
+        return parser.nextConstraint;
+    }
+
+    private final Query parentConstraint;
+    private final Assignments assignments;
+    private Result<Query, Perspective> nextConstraint = result();
+
+    private NoCodeQueryParser(Assignments assignmentsArg) {
+        assignments = assignmentsArg;
+        parentConstraint = query(forAll(Optional.of(NO_CONTEXT)));
+    }
+
+    private NoCodeQueryParser(Assignments assignmentsArg, Query parentConstraintArg) {
+        assignments = assignmentsArg;
+        parentConstraint = parentConstraintArg;
+    }
+
+    @Override
+    public Result<Query, Perspective> visitVariable_definition(NoCodeDenParser.Variable_definitionContext ctx) {
+        if (ctx.variable_definition_name().getText().equals("constraints")) {
+            visitFunction_call(ctx.variable_definition_value().value().function_call(), 0);
+        }
+        return nextConstraint;
+    }
+
+    private Result<Query, Perspective> visitFunction_call(java.util.List<NoCodeDenParser.Function_callContext> functionCallChain
+            , int currentIndex) {
+        final var firstCall = functionCallChain.get(currentIndex);
+        nextConstraint = parseQuery(firstCall.function_call_name().getText()
+                , firstCall.function_call_argument());
+        if (currentIndex < functionCallChain.size() - 1 && nextConstraint.value().isPresent()) {
+            final var childConstraintParser = new NoCodeQueryParser(assignments, nextConstraint.value().orElseThrow());
+            final var intermediate = childConstraintParser.visitFunction_call(functionCallChain, currentIndex);
+            nextConstraint.errorMessages().withAppended(intermediate.errorMessages());
+        }
+        return nextConstraint;
+    }
+
+    private Result<Query, Perspective> parseQuery(String constraintType, java.util.List<NoCodeDenParser.Function_call_argumentContext> arguments) {
+        final Result<Query, Perspective> parsedConstraint = result();
+        if (constraintType.equals(FOR_ALL_NAME)) {
+            if (arguments != null && arguments.size() > 0) {
+                return parsedConstraint.withErrorMessage(perspective("ForAll does not support arguments.")
+                        .withProperty(AFFECTED_CONTENT
+                                , arguments.stream().map(a -> a.getText()).toList().toString()));
+            }
+            parsedConstraint.withValue(parentConstraint.forAll());
+        } else if (constraintType.equals(FOR_EACH_NAME)) {
+            if (arguments == null || arguments.isEmpty()) {
+                parsedConstraint.withValue(parentConstraint.forAll());
+                return parsedConstraint;
+            } else if (arguments.size() == 1) {
+                parsedConstraint.withValue(parentConstraint.forAll
+                        (assignments.attributeByName(arguments.get(0).value().string_value().getText())));
+                return parsedConstraint;
+            } else if (arguments.size() > 1) {
+                return parsedConstraint
+                        .withErrorMessage(perspective("ForEach does not support multiple arguments.")
+                                .withProperty(AFFECTED_CONTENT
+                                        , arguments.stream().map(a -> a.getText()).toList().toString()));
+            }
+            return parsedConstraint.withErrorMessage(perspective("Could not parse ForEach, because of unknown reason."));
+        } else if (constraintType.equals(FOR_ALL_COMBINATIONS_OF)) {
+            if (arguments.size() < 2) {
+                return parsedConstraint.withErrorMessage(perspective(FOR_ALL_COMBINATIONS_OF + " requires at least 2 arguments.")
+                        .withProperty(AFFECTED_CONTENT
+                                , arguments.stream().map(a -> a.getText()).toList().toString()));
+            }
+            final List<Attribute<? extends Object>> combination = list();
+            for (final var a : arguments) {
+                final var attributeName = a.value().variable_reference().Name().getText();
+                combination.add(assignments.attributeByName(attributeName));
+            }
+            parsedConstraint.withValue(parentConstraint.forAllCombinationsOf(combination));
+            return parsedConstraint;
+        } else if (constraintType.equals(THEN_NAME)) {
+            if (arguments == null && arguments.isEmpty()) {
+                return parsedConstraint
+                        .withErrorMessage(perspective("Then constraint requires at least one argument.")
+                                .withProperty(AFFECTED_CONTENT
+                                        , arguments.stream().map(a -> a.getText()).toList().toString()));
+            }
+            if (arguments.size() > 1) {
+                return parsedConstraint
+                        .withErrorMessage(perspective("Then constraint only support one argument at maximum")
+                                .withProperty(AFFECTED_CONTENT
+                                        , arguments.stream().map(a -> a.getText()).toList().toString()));
+            }
+
+            final var rater = parseNoCodeRater(arguments.get(0).value().function_call(0), assignments);
+            if (rater.defective()) {
+                parsedConstraint.errorMessages().withAppended(rater.errorMessages());
+                return parsedConstraint;
+            }
+            return parsedConstraint.withValue(parentConstraint.then(rater.value().orElseThrow()));
+        } else {
+            return parentConstraint.constraintResult(constraintType, list(), list());
+        }
+        return parsedConstraint;
+    }
+}
---
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/no/code/editor/NoCodeRaterParser.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.ui.no.code.editor;
+
+import net.splitcells.dem.data.atom.Integers;
+import net.splitcells.dem.lang.perspective.Perspective;
+import net.splitcells.dem.lang.perspective.antlr4.DenParser;
+import net.splitcells.dem.lang.perspective.antlr4.DenParserBaseVisitor;
+import net.splitcells.dem.lang.perspective.no.code.antlr4.NoCodeDenParser;
+import net.splitcells.dem.lang.perspective.no.code.antlr4.NoCodeDenParserBaseVisitor;
+import net.splitcells.dem.testing.Result;
+import net.splitcells.gel.data.assignment.Assignments;
+import net.splitcells.gel.data.table.attribute.Attribute;
+import net.splitcells.gel.rating.rater.framework.Rater;
+import net.splitcells.gel.ui.RaterParser;
+
+import static net.splitcells.dem.lang.perspective.PerspectiveI.perspective;
+import static net.splitcells.gel.rating.rater.lib.AllSame.ALL_SAME_NAME;
+import static net.splitcells.gel.rating.rater.lib.AllSame.allSame;
+import static net.splitcells.gel.rating.rater.lib.HasSize.HAS_SIZE_NAME;
+import static net.splitcells.gel.rating.rater.lib.HasSize.hasSize;
+import static net.splitcells.gel.rating.rater.lib.MinimalDistance.MINIMAL_DISTANCE_NAME;
+import static net.splitcells.gel.rating.rater.lib.MinimalDistance.has_minimal_distance_of;
+
+public class NoCodeRaterParser extends NoCodeDenParserBaseVisitor<Result<Rater, Perspective>> {
+    public static Result<Rater, Perspective> parseNoCodeRater(NoCodeDenParser.Function_callContext functionCall, Assignments assignments) {
+        return new NoCodeRaterParser(assignments).visitFunction_call(functionCall);
+    }
+
+    private final Assignments assignments;
+
+    private NoCodeRaterParser(Assignments assignmentsArg) {
+        assignments = assignmentsArg;
+    }
+
+    @Override
+    public Result<Rater, Perspective> visitFunction_call(NoCodeDenParser.Function_callContext functionCall) {
+        final Result<Rater, Perspective> rater = Result.result();
+        final var functionName = functionCall.function_call_name().string_value().getText();
+        if (functionName.equals(HAS_SIZE_NAME)) {
+            final int argument = Integers.parse(functionCall.function_call_argument()
+                    .get(0)
+                    .value()
+                    .string_value()
+                    .Integer()
+                    .getText());
+            return rater.withValue(hasSize(argument));
+        } else if (functionName.equals(ALL_SAME_NAME)) {
+            return rater.withValue(allSame(assignments.attributeByName(functionCall.function_call_argument()
+                    .get(0)
+                    .value()
+                    .string_value()
+                    .getText())));
+        } else if (functionName.equals(MINIMAL_DISTANCE_NAME)) {
+            if (functionCall.function_call_argument() == null || functionCall.function_call_argument().isEmpty()) {
+                return rater.withErrorMessage(perspective("Rater `" + MINIMAL_DISTANCE_NAME + "` requires exactly 2 arguments, but has none.")
+                        .withProperty("rater", functionCall.getText()));
+            }
+            if (functionCall.function_call_argument().size() != 2) {
+                return rater.withErrorMessage(perspective("Rater `" + MINIMAL_DISTANCE_NAME + "` requires exactly 2 arguments.")
+                        .withProperty("rater", functionCall.getText()));
+            }
+            final var attribute = assignments.attributeByName(functionCall.function_call_argument()
+                    .get(0)
+                    .value()
+                    .string_value()
+                    .getText());
+
+            final var minimumDistance = Double.parseDouble(functionCall.function_call_argument()
+                    .get(1)
+                    .value()
+                    .string_value()
+                    .Integer()
+                    .getText());
+            return rater.withValue(has_minimal_distance_of((Attribute<Integer>) attribute, minimumDistance));
+
+        }
+        return rater.withErrorMessage(perspective("Unknown rater function: " + functionCall.getText()));
+    }
+}
