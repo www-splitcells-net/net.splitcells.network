17367a09f59b3c551d0cab872bfb2a2cdd2afe0b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-09-06 22:14:47 +0200

    #41 Split up install method
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.os.state.interface/src/main/python/command/managed/command.managed.install.project.commands.py
+++ projects/net.splitcells.os.state.interface/src/main/python/command/managed/command.managed.install.project.commands.py
@@ -19,53 +19,59 @@ from pathlib import PosixPath
 from os import environ
 import logging
 def currentFolder():
-	return Path.cwd()
+    return Path.cwd()
 class Command:
-	def __init__(self
-			, projectPath
-			, targetFolder = Path.home().joinpath('bin', 'net.splitcells.os.state.interface.commands.managed')
-			):
-		projectPosixPath = PosixPath(projectPath)
-		self.projectName = projectPosixPath.name
-		self.binFolder = projectPosixPath.joinpath('bin')
-		self.targetFolder = targetFolder
-	def install(self):
-		logging.info("Installing project repository '" + self.projectName + "'.")
-		for projectCommand in self.binFolder.rglob("*"):
-			globalProjectCommandName = self.projectName + '.' + projectCommand.name
-			targetCommandFile = self.targetFolder.joinpath(globalProjectCommandName)
-			if targetCommandFile.exists():
-				executionCounter = 0
-				while True:
-					globalProjectCommandName = self.projectName + '.' + projectCommand.name + '.' + str(executionCounter)
-					targetCommandFile = self.targetFolder.joinpath(globalProjectCommandName)
-					executionCounter += 1
-					if not targetCommandFile.exists():
-						break
-			targetCommandFile.write_text("#!/usr/bin/env sh\n"
-			    + 'cd ' + str(projectCommand.resolve().parent.parent) + " \n"
-			    + './bin/' + projectCommand.name + " $@\n")
+    def __init__(self
+            , projectPath
+            , targetFolder = Path.home().joinpath('bin', 'net.splitcells.os.state.interface.commands.managed')
+            ):
+        projectPosixPath = PosixPath(projectPath)
+        self.projectName = projectPosixPath.name
+        self.binFolder = projectPosixPath.joinpath('bin')
+        self.targetFolder = targetFolder
+    def install(self):
+        logging.info("Installing project repository '" + self.projectName + "'.")
+        for projectCommand in self.binFolder.rglob("*"):
+            projectCommandContent = "#!/usr/bin/env sh\n" \
+                    + 'cd ' \
+                    + str(projectCommand.resolve().parent.parent) \
+                    + " \n" \
+                    + './bin/' + projectCommand.name + " $@\n"
+            self._installProjectCommand(projectCommand.name, projectCommandContent)
+        repoGui = self.binFolder.joinpath('repo.gui')
+    def _installProjectCommand(self, name, content):
+        globalProjectCommandName = self.projectName + '.' + name
+        targetCommandFile = self.targetFolder.joinpath(globalProjectCommandName)
+        if targetCommandFile.exists():
+            executionCounter = 0
+            while True:
+                globalProjectCommandName = self.projectName + '.' + name + '.' + str(executionCounter)
+                targetCommandFile = self.targetFolder.joinpath(globalProjectCommandName)
+                executionCounter += 1
+                if not targetCommandFile.exists():
+                    break
+        targetCommandFile.write_text(content)
 if __name__ == '__main__':
-	if environ.get('log_level') == 'debug':
-		logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-	else:
-		logging.basicConfig(format='%(message)s', level=logging.INFO)
-	parser = argparse.ArgumentParser(description='Installs projects command to ~/bin/net.splitcells.os.state.interface.commands.managed.')
-	parser.add_argument('--project', nargs='?', type=str, help='Path to the project to be installed.')
-	parsedArgs = parser.parse_args()
-	if parsedArgs.project is None:
-	    # A different file is used for the list of repos to be installed in this command and
-	    # 'command.repositories.install'. The reason for this is, that a project repo cannot be installed
-	    # like and command repo in OSSI, because not every file in the src folder of project repo can be used
-	    # as an independent command.
-		repoConfig = PosixPath('~/.config/net.splitcells.os.state.interface/project.repositories').expanduser()
-		if repoConfig.exists():
-			with open(repoConfig,'r') as projectRepositories:
-				for projectRepository in projectRepositories.read().splitlines():
-				    repoPrefix = 'repo='
-				    Command(projectRepository).install()
-		else:
-			logging.debug('Nothing was installed, because no project repositories are defined.')
-	else:
-		Command(parsedArgs.project).install()
-	
+    if environ.get('log_level') == 'debug':
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(message)s', level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Installs projects command to ~/bin/net.splitcells.os.state.interface.commands.managed.')
+    parser.add_argument('--project', nargs='?', type=str, help='Path to the project to be installed.')
+    parsedArgs = parser.parse_args()
+    if parsedArgs.project is None:
+        # A different file is used for the list of repos to be installed in this command and
+        # 'command.repositories.install'. The reason for this is, that a project repo cannot be installed
+        # like and command repo in OSSI, because not every file in the src folder of project repo can be used
+        # as an independent command.
+        repoConfig = PosixPath('~/.config/net.splitcells.os.state.interface/project.repositories').expanduser()
+        if repoConfig.exists():
+            with open(repoConfig,'r') as projectRepositories:
+                for projectRepository in projectRepositories.read().splitlines():
+                    repoPrefix = 'repo='
+                    Command(projectRepository).install()
+        else:
+            logging.debug('Nothing was installed, because no project repositories are defined.')
+    else:
+        Command(parsedArgs.project).install()
+    
