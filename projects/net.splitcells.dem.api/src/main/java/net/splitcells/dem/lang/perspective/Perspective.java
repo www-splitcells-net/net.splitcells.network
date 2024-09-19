190f160134b410790242eff63e00e21332ae2e9c
Mārtiņš Avots <martins.avots@splitcells.net>
2024-09-19 22:29:25 +0200

    #26 Render text and natural text appropriately
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/perspective/Perspective.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/perspective/Perspective.java
@@ -446,7 +446,7 @@ public interface Perspective extends PerspectiveView {
             xmlString += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
             childConfig.withPrintXmlDeclaration(false);
         }
-        if (nameSpace().equals(STRING)) {
+        if (nameSpace().equals(STRING) || nameSpace().equals(TEXT) || nameSpace().equals(NATURAL)) {
             return name();
         }
         if (name().isBlank()) {
