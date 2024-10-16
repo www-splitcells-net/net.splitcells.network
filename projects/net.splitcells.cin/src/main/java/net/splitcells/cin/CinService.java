f4d95713ef049d011a5a3091b4e10e0e5d259135
Mārtiņš Avots <martins.avots@splitcells.net>
2024-10-16 06:46:15 +0200

    #51 Create game service
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/java/net/splitcells/cin/CinService.java
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/CinService.java
@@ -1,4 +1,49 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
 package net.splitcells.cin;
 
-public class CinService {
+import net.splitcells.dem.Dem;
+import net.splitcells.dem.environment.resource.ResourceOption;
+import net.splitcells.dem.environment.resource.Service;
+
+public class CinService implements ResourceOption<Service> {
+    @Override
+    public Service defaultValue() {
+        return new Service() {
+            private volatile boolean isRunning = false;
+
+            @Override
+            public void start() {
+                isRunning = true;
+                Dem.executeThread("Cin", () -> {
+                    while (isRunning) {
+                        Dem.sleepAtLeast(1000);
+                    }
+                });
+            }
+
+            @Override
+            public void close() {
+                isRunning = false;
+            }
+
+            @Override
+            public void flush() {
+
+            }
+        };
+    }
 }
