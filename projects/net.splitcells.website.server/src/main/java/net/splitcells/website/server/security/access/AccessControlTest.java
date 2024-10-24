fbf7be2013659b42a3dc6af87be98942adab54df
Mārtiņš Avots <martins.avots@splitcells.net>
2024-10-24 19:06:52 +0200

    #37 Create basic access control test
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/security/access/AccessControlTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.website.server.security.access;
+
+import net.splitcells.dem.data.atom.Bools;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.dem.utils.StringUtils;
+import net.splitcells.website.server.security.authentication.Authentication;
+import net.splitcells.website.server.security.authentication.Authenticator;
+import net.splitcells.website.server.security.authentication.BasicLogin;
+import net.splitcells.website.server.security.authentication.UserSession;
+
+import java.util.function.BiConsumer;
+
+import static net.splitcells.dem.Dem.configValue;
+import static net.splitcells.dem.data.atom.Bools.requireNot;
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.resource.FileSystemViaMemory.fileSystemViaMemory;
+import static net.splitcells.dem.testing.Assertions.requireDistinct;
+import static net.splitcells.dem.testing.Assertions.requireEquals;
+import static net.splitcells.website.server.security.access.AccessControlImpl.accessControl;
+import static net.splitcells.website.server.security.authentication.AuthenticatorBasedOnFiles.PASSWORD_FILE;
+import static net.splitcells.website.server.security.authentication.AuthenticatorBasedOnFiles.authenticatorBasedOnFiles;
+import static net.splitcells.website.server.security.authentication.BasicLogin.login;
+import static net.splitcells.website.server.security.authentication.UserSession.*;
+
+public class AccessControlTest {
+    @UnitTest
+    public void test() {
+        final var username = "username-123";
+        final var password = "password-456";
+        final var userData = fileSystemViaMemory();
+        userData.createDirectoryPath(username);
+        userData.writeToFile(username + PASSWORD_FILE, StringUtils.toBytes(password));
+        final var authenticator = authenticatorBasedOnFiles(userData);
+        final List<Firewall> accessCounter = list();
+        final List<UserSession> userSessions = list();
+        final Firewall subjectAccessed = new Firewall() {
+        };
+        final var testSubject = accessControl(authenticator, c -> c.accept(subjectAccessed));
+        testSubject.access((u, a) -> {
+            accessCounter.requireEmpty();
+            userSessions.requireEmpty();
+            if (!isValidNoLoginStandard(u)) {
+                userSessions.add(u);
+                accessCounter.add(a);
+            }
+        }, login(username, password));
+        accessCounter.requireEquals(list(subjectAccessed));
+        userSessions.requireSizeOf(1);
+        requireNot(authenticator.isValid(userSessions.get(0)));
+    }
+}
