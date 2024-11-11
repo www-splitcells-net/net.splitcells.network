e3487d0d397f4dcba080219eda721fe8fdd8046e
Mārtiņš Avots <martins.avots@splitcells.net>
2024-11-11 10:48:22 +0100

    Fix command #41
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.project/src/main/python/net/splitcells/project/repo/repo.push.at.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+command.managed.execute disjunction repo.push.at $@
