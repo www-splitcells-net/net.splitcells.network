5e8dfc853b554990fe92ce6f1ce727b415c5393b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-11-15 22:44:15 +0100

    Migrate ExistenceCost to LineGroupRater #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.cin/src/main/java/net/splitcells/cin/raters/ExistenceCost.java
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/raters/ExistenceCost.java
@@ -16,91 +16,98 @@
 package net.splitcells.cin.raters;
 
 import net.splitcells.cin.EntityManager;
-import net.splitcells.cin.deprecated.raters.deprecated.PositionClustersCenterX;
-import net.splitcells.cin.deprecated.raters.deprecated.PositionClustersCenterY;
-import net.splitcells.dem.data.order.Comparators;
 import net.splitcells.dem.data.set.list.List;
-import net.splitcells.dem.data.set.list.Lists;
 import net.splitcells.dem.lang.dom.Domable;
 import net.splitcells.gel.constraint.Constraint;
 import net.splitcells.gel.constraint.GroupId;
 import net.splitcells.gel.data.view.Line;
 import net.splitcells.gel.data.view.View;
-import net.splitcells.gel.data.view.attribute.Attribute;
+import net.splitcells.gel.proposal.Proposal;
 import net.splitcells.gel.rating.rater.framework.Rater;
 import net.splitcells.gel.rating.rater.framework.RatingEvent;
-
-import java.util.Optional;
+import net.splitcells.gel.rating.rater.lib.GroupingRater;
 
 import static net.splitcells.cin.EntityManager.*;
 import static net.splitcells.dem.data.order.Comparators.ASCENDING_INTEGERS;
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.list.Lists.toList;
-import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.utils.ExecutionException.executionException;
-import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 import static net.splitcells.gel.constraint.Constraint.INCOMING_CONSTRAINT_GROUP;
 import static net.splitcells.gel.constraint.Constraint.LINE;
 import static net.splitcells.gel.rating.framework.LocalRatingI.localRating;
 import static net.splitcells.gel.rating.rater.framework.RatingEventI.ratingEvent;
-import static net.splitcells.gel.rating.rater.lib.RaterBasedOnLineGroup.groupRouter;
+import static net.splitcells.gel.rating.rater.lib.LineGroupRater.lineGroupRater;
 import static net.splitcells.gel.rating.type.Cost.cost;
 import static net.splitcells.gel.rating.type.Cost.noCost;
 
 /**
  * Decrements a {@link EntityManager#PLAYER_ENERGY} at every {@link EntityManager#TIME} by one.
  */
-public class ExistenceCost {
-
-
+public class ExistenceCost implements GroupingRater {
     public static Rater existenceCost() {
-        return groupRouter((lines, children) -> {
-            /* TODO Lines is used incorrectly, as it does not contain the allocation attributes
-             * It contains only the constraint meta data, so this draft is not correct.
-             */
-            final var ratingEvent = ratingEvent();
-            final var incomingConstraintGroup = lines.unorderedLinesStream().findFirst().orElseThrow()
-                    .value(INCOMING_CONSTRAINT_GROUP);
-            final var times = lines.columnView(LINE).values().stream().map(l -> l.value(TIME))
-                    .distinct()
-                    .sorted(ASCENDING_INTEGERS)
-                    .collect(toList());
-            if (times.size() < 2) {
-                return ratingEvent;
-            }
-            final int endTime = times.get(1);
-            lines.columnView(LINE).stream().map(l -> l.value(PLAYER)).distinct().forEach(player -> {
-                final var playerValues = lines.unorderedLinesStream()
-                        .filter(l -> player.equals(l.value(LINE).value(PLAYER)))
-                        .collect(toList());
-                final var energyUpdate = playerValues.stream()
-                        .filter(l -> l.value(LINE).value(PLAYER_ATTRIBUTE) == PLAYER_ENERGY)
-                        .filter(l -> l.value(LINE).value(EVENT_TYPE) == ADD_VALUE)
-                        .filter(l -> l.value(LINE).value(TIME) == endTime)
-                        .filter(l -> l.value(LINE).value(EVENT_SOURCE) == EXISTENCE_COST_EVENT_SOURCE)
-                        .filter(l -> l.value(LINE).value(PLAYER_VALUE) == -1f)
-                        .collect(toList());
-                if (energyUpdate.hasElements()) {
-                    if (energyUpdate.size() > 1) {
-                        throw executionException("Only one player energy result is valid.");
-                    }
-                    ratingEvent.additions().put(energyUpdate.get(0)
-                            , localRating()
-                                    .withPropagationTo(children)
-                                    .withResultingGroupId(incomingConstraintGroup)
-                                    .withRating(noCost()));
-                } else {
-                    ratingEvent.additions().put(playerValues.get(0)
-                            , localRating()
-                                    .withPropagationTo(children)
-                                    .withResultingGroupId(incomingConstraintGroup)
-                                    .withRating(cost(1)));
-                }
-            });
-            return ratingEvent;
-        });
+        return lineGroupRater(new ExistenceCost());
     }
 
     private ExistenceCost() {
+
+    }
+
+    @Override
+    public RatingEvent rating(View lines, List<Constraint> children) {
+        final var ratingEvent = ratingEvent();
+        final var incomingConstraintGroup = lines.unorderedLinesStream().findFirst().orElseThrow()
+                .value(INCOMING_CONSTRAINT_GROUP);
+        final var times = lines.columnView(LINE).values().stream().map(l -> l.value(TIME))
+                .distinct()
+                .sorted(ASCENDING_INTEGERS)
+                .collect(toList());
+        if (times.size() < 2) {
+            return ratingEvent;
+        }
+        final int endTime = times.get(1);
+        lines.columnView(LINE).stream().map(l -> l.value(PLAYER)).distinct().forEach(player -> {
+            final var playerValues = lines.unorderedLinesStream()
+                    .filter(l -> player.equals(l.value(LINE).value(PLAYER)))
+                    .collect(toList());
+            final var energyUpdate = playerValues.stream()
+                    .filter(l -> l.value(LINE).value(PLAYER_ATTRIBUTE) == PLAYER_ENERGY)
+                    .filter(l -> l.value(LINE).value(EVENT_TYPE) == ADD_VALUE)
+                    .filter(l -> l.value(LINE).value(TIME) == endTime)
+                    .filter(l -> l.value(LINE).value(EVENT_SOURCE) == EXISTENCE_COST_EVENT_SOURCE)
+                    .filter(l -> l.value(LINE).value(PLAYER_VALUE) == -1f)
+                    .collect(toList());
+            if (energyUpdate.hasElements()) {
+                if (energyUpdate.size() > 1) {
+                    throw executionException("Only one player energy result is valid.");
+                }
+                ratingEvent.additions().put(energyUpdate.get(0)
+                        , localRating()
+                                .withPropagationTo(children)
+                                .withResultingGroupId(incomingConstraintGroup)
+                                .withRating(noCost()));
+            } else {
+                ratingEvent.additions().put(playerValues.get(0)
+                        , localRating()
+                                .withPropagationTo(children)
+                                .withResultingGroupId(incomingConstraintGroup)
+                                .withRating(cost(1)));
+            }
+        });
+        return ratingEvent;
+    }
+
+    @Override
+    public String toSimpleDescription(Line line, View groupsLineProcessing, GroupId incomingGroup) {
+        return "all players loose one unit of energy every time step";
+    }
+
+    @Override
+    public List<Domable> arguments() {
+        return list();
+    }
+
+    @Override
+    public Proposal propose(Proposal proposal) {
+        return proposal;
     }
 }
