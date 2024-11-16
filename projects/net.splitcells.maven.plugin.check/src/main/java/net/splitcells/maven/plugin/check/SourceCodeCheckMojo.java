91f9568c681f72f189ea1e3ea2032ce04dbfc25e
Mārtiņš Avots <martins.avots@splitcells.net>
2024-11-16 09:21:33 +0100

    Draft source code check plugin structure #26
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.maven.plugin.check/src/main/java/net/splitcells/maven/plugin/check/SourceCodeCheckMojo.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.maven.plugin.check;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * <p>This goal should be executed in the validate phase.</p>
+ */
+@Mojo(name = "source-code-check")
+public class SourceCodeCheckMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        System.out.println("This is not implemented yet.");
+    }
+}
