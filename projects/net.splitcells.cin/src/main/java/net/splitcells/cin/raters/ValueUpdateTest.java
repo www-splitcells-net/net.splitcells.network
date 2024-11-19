373a8715e4c04f95348f893df9f11b462647873e
Mārtiņš Avots <martins.avots@splitcells.net>
2024-11-19 21:38:15 +0100

    Create test for value update #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.cin/src/main/java/net/splitcells/cin/raters/ValueUpdateTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.cin.raters;
+
+import net.splitcells.dem.testing.annotations.UnitTest;
+
+import static net.splitcells.cin.EntityManager.ADD_VALUE;
+import static net.splitcells.cin.EntityManager.EVENT_SOURCE;
+import static net.splitcells.cin.EntityManager.EVENT_TYPE;
+import static net.splitcells.cin.EntityManager.EXISTENCE_COST_EVENT_SOURCE;
+import static net.splitcells.cin.EntityManager.PLAYER_ATTRIBUTE;
+import static net.splitcells.cin.EntityManager.PLAYER_ENERGY;
+import static net.splitcells.cin.EntityManager.PLAYER_VALUE;
+import static net.splitcells.cin.EntityManager.RESULT_VALUE;
+import static net.splitcells.cin.EntityManager.SET_VALUE;
+import static net.splitcells.cin.EntityManager.TIME;
+import static net.splitcells.cin.raters.ExistenceCost.existenceCost;
+import static net.splitcells.cin.raters.TimeSteps.overlappingTimeSteps;
+import static net.splitcells.cin.raters.ValueUpdate.valueUpdate;
+import static net.splitcells.dem.data.atom.Bools.require;
+import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
+import static net.splitcells.gel.solution.SolutionBuilder.defineProblem;
+
+public class ValueUpdateTest {
+    @UnitTest
+    public void test() {
+        final var testSubject = defineProblem("entity-manager")
+                .withDemandAttributes(TIME)
+                .withNoDemands()
+                .withSupplyAttributes(PLAYER_ATTRIBUTE, PLAYER_VALUE, EVENT_TYPE)
+                .withSupplies()
+                .withConstraint(query -> {
+                    query.forAll(overlappingTimeSteps(TIME)).then(valueUpdate(PLAYER_ENERGY));
+                    return query;
+                })
+                .toProblem()
+                .asSolution();
+        final var demands = testSubject.demands();
+        final var supplies = testSubject.supplies();
+        final int startTime = 1;
+        final int endTime = 2;
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        testSubject.assign(demands.addTranslated(listWithValuesOf(startTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 0, RESULT_VALUE)));
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        {
+            final var tmp = testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                    , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 0, RESULT_VALUE)));
+            testSubject.constraint().rating().requireEqualsTo(noCost());
+            testSubject.remove(tmp);
+        }
+        testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 1, RESULT_VALUE)));
+        testSubject.constraint().rating().requireEqualsTo(cost(2));
+        testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 1, SET_VALUE)));
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 1, ADD_VALUE)));
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 1, ADD_VALUE)));
+        // There is no cost, because of 3/2 is rounded to 1.
+        testSubject.constraint().rating().requireEqualsTo(noCost());
+        testSubject.assign(demands.addTranslated(listWithValuesOf(endTime))
+                , supplies.addTranslated(listWithValuesOf(PLAYER_ENERGY, 1, ADD_VALUE)));
+        testSubject.constraint().rating().requireEqualsTo(cost(6));
+
+    }
+}
