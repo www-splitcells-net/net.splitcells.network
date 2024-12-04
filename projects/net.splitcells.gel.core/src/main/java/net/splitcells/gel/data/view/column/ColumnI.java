b25625ec55dcf23979cf6fa1b86fa1f614e909b9
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-04 10:55:44 +0100

    Implement temporary lookups #55
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/view/column/ColumnI.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/view/column/ColumnI.java
@@ -18,6 +18,7 @@ package net.splitcells.gel.data.view.column;
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
+import static net.splitcells.gel.data.table.Tables.table;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -34,171 +35,185 @@ import net.splitcells.gel.data.view.View;
 import net.splitcells.gel.data.view.attribute.Attribute;
 
 public class ColumnI<T> implements Column<T> {
-	public static <R> Column<R> column(View view, Attribute<R> attribute) {
-		return new ColumnI<>(view, attribute);
-	}
+    public static <R> Column<R> column(View view, Attribute<R> attribute) {
+        return new ColumnI<>(view, attribute);
+    }
 
-	private final List<T> content = list();
-	private final Attribute<T> attribute;
-	private final View view;
-	private Optional<Lookup<T>> lookup = Optional.empty();
+    private final List<T> content = list();
+    private final Attribute<T> attribute;
+    private final View view;
+    private Optional<Lookup<T>> lookup = Optional.empty();
 
-	private ColumnI(View view, Attribute<T> attribute) {
-		this.attribute = attribute;
-		this.view = view;
-	}
+    private ColumnI(View view, Attribute<T> attribute) {
+        this.attribute = attribute;
+        this.view = view;
+    }
 
-	private Lookup<T> ensureInitializedLookup() {
-		if (lookup.isEmpty()) {
-			lookup = Optional.of(Lookups.persistedLookup(view, attribute));
-		}
-		return lookup.get();
-	}
+    private Lookup<T> ensureInitializedLookup() {
+        if (lookup.isEmpty()) {
+            lookup = Optional.of(Lookups.persistedLookup(view, attribute));
+        }
+        return lookup.get();
+    }
 
-	@Override
-	public int size() {
-		return content.size();
-	}
+    @Override
+    public int size() {
+        return content.size();
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return content.isEmpty();
-	}
+    @Override
+    public boolean isEmpty() {
+        return content.isEmpty();
+    }
 
-	@Override
-	public boolean contains(Object o) {
-		return content.contains(o);
-	}
+    @Override
+    public boolean contains(Object o) {
+        return content.contains(o);
+    }
 
-	@Override
-	public Iterator<T> iterator() {
-		return content.iterator();
-	}
+    @Override
+    public Iterator<T> iterator() {
+        return content.iterator();
+    }
 
-	@Override
-	public Object[] toArray() {
-		return content.toArray();
-	}
+    @Override
+    public Object[] toArray() {
+        return content.toArray();
+    }
 
-	@Override
-	public <R> R[] toArray(R[] a) {
-		return content.toArray(a);
-	}
+    @Override
+    public <R> R[] toArray(R[] a) {
+        return content.toArray(a);
+    }
 
-	@Override
-	public boolean add(T e) {
-		return content.add(e);
-	}
+    @Override
+    public boolean add(T e) {
+        return content.add(e);
+    }
 
-	@Override
-	public boolean remove(Object o) {
-		throw notImplementedYet();
-	}
+    @Override
+    public boolean remove(Object o) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		return content.containsAll(c);
-	}
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return content.containsAll(c);
+    }
 
-	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		var contentChanged = false;
-		for (T e : c) {
-			contentChanged |= add(e);
-		}
-		return contentChanged;
-	}
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        var contentChanged = false;
+        for (T e : c) {
+            contentChanged |= add(e);
+        }
+        return contentChanged;
+    }
 
-	@Override
-	public boolean addAll(int index, Collection<? extends T> c) {
-		throw notImplementedYet();
-	}
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		throw notImplementedYet();
-	}
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		throw notImplementedYet();
-	}
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public void clear() {
-		content.clear();
-	}
+    @Override
+    public void clear() {
+        content.clear();
+    }
 
-	@Override
-	public T get(int index) {
-		return content.get(index);
-	}
+    @Override
+    public T get(int index) {
+        return content.get(index);
+    }
 
-	@Override
-	public T set(int index, T additionalElement) {
-		content.set(index, additionalElement);
-		return additionalElement;
-	}
+    @Override
+    public T set(int index, T additionalElement) {
+        content.set(index, additionalElement);
+        return additionalElement;
+    }
 
-	@Override
-	public void add(int index, T element) {
-		throw notImplementedYet();
-	}
+    @Override
+    public void add(int index, T element) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public T remove(int index) {
-		throw notImplementedYet();
-	}
+    @Override
+    public T remove(int index) {
+        throw notImplementedYet();
+    }
 
-	@Override
-	public int indexOf(Object o) {
-		return content.indexOf(o);
-	}
+    @Override
+    public int indexOf(Object o) {
+        return content.indexOf(o);
+    }
 
-	@Override
-	public int lastIndexOf(Object o) {
-		return content.lastIndexOf(o);
-	}
+    @Override
+    public int lastIndexOf(Object o) {
+        return content.lastIndexOf(o);
+    }
 
-	@Override
-	public ListIterator<T> listIterator() {
-		return content.listIterator();
-	}
+    @Override
+    public ListIterator<T> listIterator() {
+        return content.listIterator();
+    }
 
-	@Override
-	public ListIterator<T> listIterator(int index) {
-		return content.listIterator(index);
-	}
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return content.listIterator(index);
+    }
 
-	@Override
-	public List<T> subList(int startIndex, int endIndex) {
-		return listWithValuesOf(content.subList(startIndex, endIndex));
-	}
+    @Override
+    public List<T> subList(int startIndex, int endIndex) {
+        return listWithValuesOf(content.subList(startIndex, endIndex));
+    }
 
-	@Override
-	public View persistedLookup(T value) {
-		ensureInitializedLookup();
-		return lookup.get().persistedLookup(value);
-	}
+    @Override
+    public View persistedLookup(T value) {
+        ensureInitializedLookup();
+        return lookup.get().persistedLookup(value);
+    }
 
-	@Override
-	public View persistedLookup(Predicate<T> predicate) {
-		ensureInitializedLookup();
-		return lookup.get().persistedLookup(predicate);
-	}
+    @Override
+    public View persistedLookup(Predicate<T> predicate) {
+        ensureInitializedLookup();
+        return lookup.get().persistedLookup(predicate);
+    }
 
-	@Override
-	public void registerAddition(Line addition) {
-		lookup.ifPresent(i -> i.register_addition(addition.value(attribute), addition.index()));
-	}
+    @Override
+    public void registerAddition(Line addition) {
+        lookup.ifPresent(i -> i.register_addition(addition.value(attribute), addition.index()));
+    }
 
-	@Override
-	public void registerBeforeRemoval(Line removal) {
-		lookup.ifPresent(i -> i.register_removal(removal.value(attribute), removal.index()));
-	}
+    @Override
+    public void registerBeforeRemoval(Line removal) {
+        lookup.ifPresent(i -> i.register_removal(removal.value(attribute), removal.index()));
+    }
 
-	@Override
-	public Flow<T> stream() {
-		return content.stream();
-	}
+    @Override
+    public Flow<T> stream() {
+        return content.stream();
+    }
+
+    @Override
+    public View lookup(T value) {
+        return lookup(a -> a.equals(value));
+    }
+
+    @Override
+    public View lookup(Predicate<T> predicate) {
+        final var lookup = table(view.headerView2());
+        view.unorderedLinesStream()
+                .filter(l -> predicate.test(l.value(attribute)))
+                .forEach(lookup::add);
+        return lookup;
+    }
 }
