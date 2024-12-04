4c96064ebb30bd7631b298ee96761af18fd69616
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-04 10:46:12 +0100

    Note the caller usage of new lookup #55
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/lookup/LookupComponents.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/lookup/LookupComponents.java
@@ -22,45 +22,43 @@ import net.splitcells.gel.data.view.View;
 import net.splitcells.gel.data.view.column.Column;
 
 public interface LookupComponents<T> {
-	/**
-	 *
-	 * @param value The value, that each selected line should contain.
-	 * @return Returns a {@link View}, where one or more {@link Column} contain only the provided value.
-	 * The choice of {@link Column} for this is determined by the implementor.
-	 * The returned {@link View} is automatically updated,
-	 * when the underlying {@link View} is updated.
-	 */
-	View persistedLookup(T value);
+    /**
+     * @param value The value, that each selected line should contain.
+     * @return Returns a {@link View}, where one or more {@link Column} contain only the provided value.
+     * The choice of {@link Column} for this is determined by the implementor.
+     * The returned {@link View} is automatically updated,
+     * when the underlying {@link View} is updated.
+     */
+    View persistedLookup(T value);
 
-	/**
-	 *
-	 * @param predicate States which lines to select by returning true for such.
-	 * @return Returns a {@link View}, where all {@link Line} comply with the predicate.
-	 * The returned {@link View} is automatically updated,
-	 * when the underlying {@link View} is updated.
-	 */
-	View persistedLookup(Predicate<T> predicate);
+    /**
+     * @param predicate States which lines to select by returning true for such.
+     * @return Returns a {@link View}, where all {@link Line} comply with the predicate.
+     * The returned {@link View} is automatically updated,
+     * when the underlying {@link View} is updated.
+     */
+    View persistedLookup(Predicate<T> predicate);
 
-	/**
-	 *
-	 * @param value The value, that each selected line should contain.
-	 * @return Returns a {@link View}, where one or more {@link Column} contain only the provided value.
-	 * The choice of {@link Column} for this is determined by the implementor.
-	 * The returned {@link View} is automatically updated **or not**,
-	 * when the underlying {@link View} is updated.
-	 */
-	default View lookup(T value) {
-		return persistedLookup(value);
-	}
+    /**
+     * @param value The value, that each selected line should contain.
+     * @return Returns a {@link View}, where one or more {@link Column} contain only the provided value.
+     * The choice of {@link Column} for this is determined by the implementor.
+     * The returned {@link View} is automatically updated **or not**,
+     * when the underlying {@link View} is updated.
+     * The caller states, that this lookup is used only until the underlying {@link View} is not used.
+     */
+    default View lookup(T value) {
+        return persistedLookup(value);
+    }
 
-	/**
-	 *
-	 * @param predicate States which lines to select by returning true for such.
-	 * @return Returns a {@link View}, where all {@link Line} comply with the predicate.
-	 * The returned {@link View} is automatically updated **or not**,
-	 * when the underlying {@link View} is updated.
-	 */
-	default View lookup(Predicate<T> predicate) {
-		return persistedLookup(predicate);
-	}
+    /**
+     * @param predicate States which lines to select by returning true for such.
+     * @return Returns a {@link View}, where all {@link Line} comply with the predicate.
+     * The returned {@link View} is automatically updated **or not**,
+     * when the underlying {@link View} is updated.
+     * The caller states, that this lookup is used only until the underlying {@link View} is not used.
+     */
+    default View lookup(Predicate<T> predicate) {
+        return persistedLookup(predicate);
+    }
 }
