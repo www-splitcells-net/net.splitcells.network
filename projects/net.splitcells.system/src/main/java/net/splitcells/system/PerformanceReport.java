0474e7bf3eb537febbd6b273224adb070ec4442c
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-06 22:29:12 +0100

    Experiment rendering first counter in report #55
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.system/src/main/java/net/splitcells/system/PerformanceReport.java
+++ projects/net.splitcells.system/src/main/java/net/splitcells/system/PerformanceReport.java
@@ -15,11 +15,27 @@
  */
 package net.splitcells.system;
 
+import net.splitcells.dem.data.set.Set;
 import net.splitcells.dem.resource.Trail;
+import net.splitcells.gel.data.table.TableModificationCounter;
+import net.splitcells.website.server.projects.ProjectsRenderer;
+import net.splitcells.website.server.projects.ProjectsRendererI;
 import net.splitcells.website.server.projects.RenderRequest;
+import net.splitcells.website.server.projects.RenderResponse;
 import net.splitcells.website.server.projects.extension.ProjectsRendererExtension;
+import net.splitcells.website.server.projects.extension.impls.ProjectPathsRequest;
 
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static net.splitcells.dem.Dem.configValue;
+import static net.splitcells.dem.data.set.Sets.setOfUniques;
 import static net.splitcells.dem.resource.Trail.trail;
+import static net.splitcells.website.server.project.LayoutConfig.layoutConfig;
+import static net.splitcells.website.server.projects.RenderResponse.renderResponse;
+import static net.splitcells.website.server.security.authorization.AdminRole.ADMIN_ROLE;
+import static net.splitcells.website.server.security.authorization.Authorization.hasRole;
+import static net.splitcells.website.server.security.authorization.Authorization.missesRole;
 
 /**
  * Provides a page, that provides statistics of different counters,
@@ -31,12 +47,50 @@ public class PerformanceReport implements ProjectsRendererExtension {
     public static ProjectsRendererExtension performanceReport() {
         return new PerformanceReport();
     }
-    private static final Trail PATH = trail("/net/splitcells/system/performance-report.html");
+
+    private static final Trail PATH = trail("net/splitcells/system/performance-report.html");
+
     private PerformanceReport() {
 
     }
+
+    @Override
+    public RenderResponse render(RenderRequest request, ProjectsRenderer projectsRenderer) {
+        if (PATH.equals(request.trail())) {
+            if (missesRole(request.user(), ADMIN_ROLE)) {
+                return projectsRenderer.renderMissingAccessRights(request);
+            }
+            return renderResponse(projectsRenderer.renderContent(htmlReport()
+                    , layoutConfig(PATH.unixPathString())
+                            .withTitle("Performance Report")
+            ));
+        }
+        return renderResponse(Optional.empty());
+    }
+
+    private String htmlReport() {
+        return "<div xmlns=\"http://www.w3.org/1999/xhtml\"><ul>"
+                + "<li>Table modification counter sum: "
+                + configValue(TableModificationCounter.class).sumCounter().currentCount()
+                + "</li>"
+                + "</ul></div>";
+    }
+
     @Override
     public boolean requiresAuthentication(RenderRequest request) {
-        return true;
+        return request.trail().equalContents(PATH);
+    }
+
+    @Override
+    public Set<Path> projectPaths(ProjectsRendererI projectsRendererI) {
+        return setOfUniques(Path.of(PATH.unixPathString()));
+    }
+
+    @Override
+    public Set<Path> projectPaths(ProjectPathsRequest request) {
+        if (hasRole(request.user(), ADMIN_ROLE)) {
+            return setOfUniques(Path.of(PATH.unixPathString()));
+        }
+        return setOfUniques();
     }
 }
