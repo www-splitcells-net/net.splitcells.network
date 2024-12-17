c43e6afe9643c3dd11bb66b7d970d4bec940dbc3
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-17 21:03:59 +0100

    Make toString of NameSpace more descriptive #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/namespace/NameSpace.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/lang/namespace/NameSpace.java
@@ -57,4 +57,9 @@ public class NameSpace {
     public Optional<Boolean> isXmlAttribute() {
         return isXmlAttribute;
     }
+
+    @Override
+    public String toString() {
+        return uri;
+    }
 }
