10efeac6e8a5a6cd2caed8782de3aabf653298af
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-24 14:25:45 +0100

    Test invalid lookup column index #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/lookup/LookupColumnTest.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/lookup/LookupColumnTest.java
@@ -15,13 +15,17 @@
  */
 package net.splitcells.gel.data.lookup;
 
+import net.splitcells.dem.testing.Assertions;
 import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.dem.utils.ExecutionException;
 import net.splitcells.gel.data.view.attribute.AttributeI;
 
 import java.util.stream.IntStream;
 
 import static java.util.stream.IntStream.range;
 import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.testing.Assertions.requireEquals;
+import static net.splitcells.dem.testing.Assertions.requireThrows;
 import static net.splitcells.gel.data.lookup.LookupTables.lookupTable;
 import static net.splitcells.gel.data.table.Tables.table;
 
@@ -44,5 +48,17 @@ public class LookupColumnTest {
         lookupTable.removeRegistration(lookupTable.orderedLine(0));
         lookupTable.removeRegistration(lookupTable.orderedLine(0));
         testSubject.flow().toList().requireEmpty();
+    }
+
+    @UnitTest
+    public void testGetWithInvalidIndex() {
+        final var attribute = AttributeI.attribute(Integer.class, "attribute");
+        final var testData = table("test-data", attribute);
+        range(0, 10).forEach(i -> testData.addTranslated(list(i)));
+        final var lookupTable = lookupTable(testData, "test-subject");
+        final var testSubject = lookupTable.columnsView().get(0);
+        lookupTable.register(testData.orderedLine(0));
+        requireEquals(testSubject.get(0), 0);
+        requireThrows(() -> testSubject.get(1));
     }
 }
