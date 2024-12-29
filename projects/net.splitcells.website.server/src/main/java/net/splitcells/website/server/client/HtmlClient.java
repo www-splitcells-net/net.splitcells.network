7ae67fe6f9f69e69d89e9df72dd2fab5a26d2b1d
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-29 02:31:04 +0100

    Add TODO #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClient.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClient.java
@@ -18,5 +18,12 @@ package net.splitcells.website.server.client;
 import net.splitcells.dem.environment.resource.Resource;
 
 public interface HtmlClient extends Resource {
+    /**
+     * TODO Tab should be probably be renamed to something like site, page or session,
+     * as Tab is a GUI specific word.
+     *
+     * @param path
+     * @return
+     */
     Tab openTab(String path);
 }
