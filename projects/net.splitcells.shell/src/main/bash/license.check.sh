a0e05ce70712d199aa9ca827652049ba8d15520f
Mārtiņš Avots <martins.avots@splitcells.net>
2025-02-02 17:18:32 +0100

    Make license check optional #49
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.shell/src/main/bash/license.check.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+# By default no license check is done.
+# This way workflows can rely on optional license checks, that are injected into the workflow.
