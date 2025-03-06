7c2c6a95e8fbd087c51069e7bc0295fadd191fd5
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-06 20:19:20 +0100

    Migrate code parsing tests to new backend #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/code/editor/CodeSolutionCalculatorTest.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/code/editor/CodeSolutionCalculatorTest.java
@@ -15,14 +15,24 @@
  */
 package net.splitcells.gel.ui.code.editor;
 
+import net.splitcells.dem.Dem;
 import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.gel.constraint.type.ForAll;
+import net.splitcells.gel.constraint.type.Then;
+import net.splitcells.gel.ui.GelUiFileSystem;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 import static net.splitcells.dem.data.set.Sets.setOfUniques;
+import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.testing.Assertions.requireEquals;
+import static net.splitcells.dem.testing.Assertions.requirePresenceOf;
 import static net.splitcells.gel.data.view.attribute.AttributeI.integerAttribute;
 import static net.splitcells.gel.data.view.attribute.AttributeI.stringAttribute;
 import static net.splitcells.gel.rating.rater.lib.HasSize.hasSize;
+import static net.splitcells.gel.rating.rater.lib.classification.ForAllValueCombinations.FOR_ALL_VALUE_COMBINATIONS_NAME;
+import static net.splitcells.gel.ui.ProblemParser.parseProblem;
 import static net.splitcells.gel.ui.code.editor.CodeSolutionCalculator.PROBLEM_DEFINITION;
 import static net.splitcells.gel.ui.code.editor.CodeSolutionCalculator.solutionCalculator;
 import static net.splitcells.website.server.processor.Request.request;
@@ -61,5 +71,114 @@ public class CodeSolutionCalculatorTest {
                 , stringAttribute("b")
                 , integerAttribute("c"));
         solution.constraint().readQuery().forAll(solution.attributeByName("a")).forAll(solution.attributeByName("b")).then(hasSize(2));
+    }
+
+    @UnitTest
+    public void testGrammarAssumption() {
+        final var testData = "demands=forAll().then();";
+        final var lexer = new net.splitcells.dem.source.den.DenLexer(CharStreams.fromString(testData));
+        final var parser = new net.splitcells.dem.source.den.DenParser(new CommonTokenStream(lexer));
+        final var testResult = parser.source_unit();
+        requireEquals(testResult.statement().get(0).variable_definition().Name().getText()
+                , "demands");
+        requireEquals(testResult.statement().get(0).variable_definition().function_call().Name().getText()
+                , "forAll");
+        requireEquals(testResult.statement().get(0).variable_definition().function_call().access().Name().getText()
+                , "then");
+    }
+
+    @UnitTest
+    public void testOutputFormat() {
+        final var resultData = parseProblem(Dem.configValue(GelUiFileSystem.class)
+                .readString("src/main/resources/html/net/splitcells/gel/ui/examples/school-course-scheduling-problem.txt"));
+        resultData.value().orElseThrow().columnAttributesForOutputFormat().requireEqualityTo(list("roomNumber"));
+        resultData.value().orElseThrow().rowAttributesForOutputFormat().requireEqualityTo(list("date", "shift"));
+    }
+
+    @UnitTest
+    public void testParseProblem() {
+        final var testData = "demands={a=int();b=string()};\n"
+                + "supplies={c=float()};\n"
+                + "constraints=forEach(a).then(hasSize(2));\n"
+                + "constraints().forEach(b).then(allSame(c));\n"
+                + "name=\"testParseProblem\";\n";
+        final var testSubject = parseProblem(testData).value().orElseThrow().problem();
+        final var forEachA = testSubject.constraint().child(0);
+        requireEquals(forEachA.type(), ForAll.class);
+        requirePresenceOf(forEachA.arguments().get(0).toTree().pathOfValueTree(
+                "rater-based-on-grouping"
+                , "grouping"
+                , "for-all-attribute-values"
+                , "attribute"
+                , "Attribute"
+                , "name"
+                , "a"));
+        final var thenHasSize2 = forEachA.child(0);
+        requireEquals(thenHasSize2.type(), Then.class);
+        requirePresenceOf(thenHasSize2.arguments().get(0).toTree().pathOfValueTree(
+                "has-size"
+                , "target-size"
+                , "2"));
+        final var forEachB = testSubject.constraint().child(1);
+        requireEquals(forEachB.type(), ForAll.class);
+        requirePresenceOf(forEachB.arguments().get(0).toTree().pathOfValueTree(
+                "rater-based-on-grouping"
+                , "grouping"
+                , "for-all-attribute-values"
+                , "attribute"
+                , "Attribute"
+                , "name"
+                , "b"));
+        final var thenAllSameC = forEachB.child(0);
+        requireEquals(thenAllSameC.type(), Then.class);
+        requirePresenceOf(thenAllSameC.arguments().get(0).toTree().pathOfValueTree(
+                "all-same"
+                , "attribute"
+                , "Attribute"
+                , "name"
+                , "c"));
+    }
+
+    @UnitTest
+    public void testParseProblemWithForAllCombinationsOf() {
+        final var testData = "demands = {a = int(); b = string()};\n"
+                + "supplies = {c = float()};\n"
+                + "constraints = forAllCombinationsOf(a, b, c);\n"
+                + "name = \"testParseProblem\";\n";
+        final var testSubject = parseProblem(testData).value().orElseThrow().problem();
+        final var forAllCombinationsOf = testSubject.constraint().child(0);
+        requireEquals(forAllCombinationsOf.type(), ForAll.class);
+        requirePresenceOf(forAllCombinationsOf.arguments().get(0).toTree().pathOfValueTree(
+                "rater-based-on-grouping"
+                , "grouping"
+                , FOR_ALL_VALUE_COMBINATIONS_NAME
+                , "attributes"
+                , "Attribute"
+                , "name"
+                , "a"));
+        final var attributes = forAllCombinationsOf.arguments().get(0).toTree()
+                .subtreeByName("grouping"
+                        , FOR_ALL_VALUE_COMBINATIONS_NAME
+                        , "attributes");
+        requireEquals(attributes.child(1).subtreeByName("name", "b").name(), "b");
+        requireEquals(attributes.child(2).subtreeByName("name", "c").name(), "c");
+    }
+
+    @UnitTest
+    public void testInvalidDemandAttribute() {
+        final var testData = "demands={a=invalid_attribute()};"
+                + "supplies={};"
+                + "constraints=forEach(a);"
+                + "name=\"testInvalidDemandAttribute\";";
+        parseProblem(testData).errorMessages().requireAnyContent();
+    }
+
+    @UnitTest
+    public void testInvalidSupplyAttribute() {
+        final var testData = "demands={};"
+                + "supplies={a=invalid_attribute()};"
+                + "constraints=forEach(a);"
+                + "name=\"testInvalidDemandAttribute\";";
+        parseProblem(testData).errorMessages().requireAnyContent();
     }
 }
