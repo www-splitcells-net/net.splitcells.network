7d76a16e64df6f2e9d835f44383c3c4af01e7e0b
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-07 17:55:08 +0100

    Migrate output format parsing #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/nocode/NoCodeEditorLangParser.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/nocode/NoCodeEditorLangParser.java
@@ -20,6 +20,8 @@ import net.splitcells.dem.lang.tree.Tree;
 import net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenParserBaseVisitor;
 import net.splitcells.dem.testing.Result;
 import net.splitcells.gel.editor.lang.*;
+import net.splitcells.gel.ui.SolutionParameters;
+import net.splitcells.gel.ui.no.code.editor.NoCodeQueryParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
@@ -28,8 +30,18 @@ import java.util.Optional;
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.testing.Result.result;
+import static net.splitcells.gel.editor.lang.ReferenceDescription.referenceDescription;
 
 public class NoCodeEditorLangParser extends NoCodeDenParserBaseVisitor<Result<SolutionDescription, Tree>> {
+    private static final String ATTRIBUTE = "attribute";
+    private static final String CONSTRAINTS = "constraints";
+    private static final String CONTENT = "content";
+    private static final String DEMANDS = "demands";
+    private static final String TABLE = "table";
+    private static final String NAME = "name";
+    private static final String SOLUTION = "solution";
+    private static final String SUPPLIES = "supplies";
+
     private Result<SolutionDescription, Tree> noCodeEditorLangParsing(String arg) {
         final var lexer = new net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenLexer(CharStreams.fromString(arg));
         final var parser = new net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenParser(new CommonTokenStream(lexer));
@@ -72,5 +84,38 @@ public class NoCodeEditorLangParser extends NoCodeDenParserBaseVisitor<Result<So
 
     private NoCodeEditorLangParser() {
 
+    }
+
+    /**
+     * Nothing needs to be done for variable access for {@link #CONSTRAINTS},
+     * as this is handled by {@link NoCodeQueryParser}.
+     *
+     * @param ctx the parse tree
+     * @return
+     */
+    @Override
+    public Result<SolutionDescription, Tree> visitVariable_access(net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenParser.Variable_accessContext ctx) {
+        final var referencedName = ctx.variable_reference().Name().getText();
+        if (SOLUTION.equals(referencedName)) {
+            if (ctx.function_call().size() > 1) {
+                result.withErrorMessage(tree("Variable access for " + SOLUTION + " is only supported for 1 function call, but more where given.")
+                        .withProperty("variable access", ctx.getText()));
+            }
+            final var functionCallname = ctx.function_call(0).function_call_name().string_value().getText();
+            if ("columnAttributesForOutputFormat".equals(functionCallname)) {
+                ctx.function_call(0).function_call_argument()
+                        .forEach(arg -> columnAttributesForOutputFormat
+                                .add(referenceDescription(arg.value().variable_reference().Name().getText(), AttributeDescription.class)));
+            } else if ("rowAttributesForOutputFormat".equals(functionCallname)) {
+                ctx.function_call(0).function_call_argument()
+                        .forEach(arg -> rowAttributesForOutputFormat
+                                .add(referenceDescription(arg.value().variable_reference().Name().getText(), AttributeDescription.class)));
+            }
+        } else if (CONSTRAINTS.equals(referencedName)) {
+            return result;
+        } else {
+            result.withErrorMessage(tree("Only function calls"));
+        }
+        return result;
     }
 }
