237a8a98042f36e8165bc8daf3f986cff7d7c397
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-07 14:09:32 +0100

    Create ANTLR infrastructure #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/nocode/NoCodeEditorLangParser.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.ui.editor.nocode;
+
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.lang.tree.Tree;
+import net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenParserBaseVisitor;
+import net.splitcells.dem.testing.Result;
+import net.splitcells.gel.editor.lang.*;
+import net.splitcells.gel.ui.SolutionParameters;
+import net.splitcells.gel.ui.code.editor.CodeEditorLangParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
+import java.util.Optional;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.lang.tree.TreeI.tree;
+import static net.splitcells.dem.testing.Result.result;
+
+public class NoCodeEditorLangParser extends NoCodeDenParserBaseVisitor<Result<SolutionDescription, Tree>> {
+    private Result<SolutionDescription, Tree> editorLangParsing(String arg) {
+        final var lexer = new net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenLexer(CharStreams.fromString(arg));
+        final var parser = new net.splitcells.dem.lang.tree.no.code.antlr4.NoCodeDenParser(new CommonTokenStream(lexer));
+        final List<Tree> parsingErrors = list();
+        parser.addErrorListener(new BaseErrorListener() {
+            // Ensures, that error messages are not hidden.
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+                    throws ParseCancellationException {
+                if (offendingSymbol instanceof CommonToken) {
+                    final var token = (CommonToken) offendingSymbol;
+                    parsingErrors.add(tree("Could not parse problem definition:")
+                            .withProperty("line", "" + line)
+                            .withProperty("column", "" + charPositionInLine)
+                            .withProperty("invalid text", "`" + token.toString(recognizer) + "`")
+                            .withProperty("invalid token", "`" + token.getText() + "`")
+                            .withProperty("error", msg));
+                } else {
+                    parsingErrors.add(tree("Could not parse problem definition:")
+                            .withProperty("line", "" + line)
+                            .withProperty("column", "" + charPositionInLine)
+                            .withProperty("invalid text", "`" + offendingSymbol.toString() + "`")
+                            .withProperty("error", msg));
+                }
+            }
+        });
+        final var parsedProblem = new NoCodeEditorLangParser().visitSource_unit(parser.source_unit());
+        parsedProblem.errorMessages().withAppended(parsingErrors);
+        return parsedProblem;
+    }
+
+    private Optional<String> name = Optional.empty();
+    private List<AttributeDescription> attributes = list();
+    private Optional<TableDescription> demands = Optional.empty();
+    private Optional<TableDescription> supplies = Optional.empty();
+    private List<ConstraintDescription> constraints = list();
+    private Result<SolutionDescription, Tree> result = result();
+    private List<ReferenceDescription<AttributeDescription>> columnAttributesForOutputFormat = list();
+    private List<ReferenceDescription<AttributeDescription>> rowAttributesForOutputFormat = list();
+
+    private NoCodeEditorLangParser() {
+
+    }
+}
