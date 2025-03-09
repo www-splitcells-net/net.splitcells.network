3972f4b266e69c1c757bc045c2fb994f03c2a1ff
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-09 14:55:00 +0100

    Simplify code #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/linebased/LineBasedTable.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/linebased/LineBasedTable.java
@@ -255,9 +255,8 @@ public class LineBasedTable implements Table {
 
     @Override
     public boolean equals(Object arg) {
-        if (arg instanceof Table) {
-            final var castedArg = (Table) arg;
-            return identity() == castedArg.identity();
+        if (arg instanceof Table table) {
+            return identity() == table.identity();
         }
         throw ExecutionException.execException("Invalid argument type: " + arg);
     }
