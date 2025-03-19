cc3a3f30b0359515b9b962d15a0a9e0154d525d1
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-19 23:18:37 +0100

    Draft notification parser for CommonMark #60
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/extension/commonmark/NotificationParser.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.website.server.project.renderer.extension.commonmark;
+
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.lang.annotations.JavaLegacyArtifact;
+import net.splitcells.dem.utils.StringUtils;
+import net.splitcells.website.Formats;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.notify.Notification;
+import net.splitcells.website.server.project.ProjectRenderer;
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.ListItem;
+import org.commonmark.node.StrongEmphasis;
+import org.commonmark.node.Text;
+import org.commonmark.parser.Parser;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.website.server.project.renderer.extension.commonmark.CommonMarkIntegration.commonMarkIntegration;
+
+/**
+ * Extracts {@link Event} of changelogs written in CommonMark, by searching for list entries starting with {@link #DATE_TIME_PATTERN}.
+ * This can be seen as a not standardized extension of <a href="https://keepachangelog.com/en/1.0.0/">Keep a Changelog</a>.
+ */
+@JavaLegacyArtifact
+public class NotificationParser extends AbstractVisitor {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final Pattern DATE_TIME_PATTERN = Pattern.compile("([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])( .*)?");
+    private static final Path CHANGELOG = Path.of("CHANGELOG.md");
+
+    private static final Parser commonMarkParser = Parser.builder().build();
+    private final CommonMarkIntegration renderer = commonMarkIntegration();
+    private List<Notification> parsedNotifications = list();
+
+    private NotificationParser() {
+
+    }
+
+    public static List<Notification> parseNotifications(ProjectRenderer projectRenderer) {
+        if (projectRenderer.projectFileSystem().isFile(CHANGELOG)) {
+            return parseNotifications(projectRenderer.projectFileSystem().readString(CHANGELOG));
+        }
+        return list();
+    }
+
+    public static List<Notification> parseNotifications(String eventsAsCommonMark) {
+        final String contentToRender;
+        if (eventsAsCommonMark.startsWith("#")) {
+            final var titleLine = eventsAsCommonMark.split("[\r\n]+")[0];
+            contentToRender = eventsAsCommonMark.substring(titleLine.length());
+        } else {
+            contentToRender = eventsAsCommonMark;
+        }
+        final var parsing = commonMarkParser.parse(contentToRender);
+        final var parser = new NotificationParser();
+        parsing.accept(parser);
+        return parser.parsedNotifications;
+    }
+
+    @Override
+    public void visit(ListItem listItem) {
+        final var itemContent = listItem.getFirstChild();
+        if (itemContent.getFirstChild() == null) {
+            visitChildren(listItem);
+            return;
+        }
+        final var possibleDate = itemContent.getFirstChild();
+        if (possibleDate instanceof StrongEmphasis) {
+            final var possibleDateEmphasis = (StrongEmphasis) possibleDate;
+            final var possibleDateText = ((Text) possibleDateEmphasis.getFirstChild()).getLiteral();
+            final var dateMatcher = DATE_TIME_PATTERN.matcher(possibleDateText);
+            if (dateMatcher.matches()) {
+                final var dateTime = LocalDate.parse(dateMatcher.group(1), DATE_TIME_FORMATTER)
+                        .atStartOfDay(ZoneId.of("UTC"))
+                        .toInstant();
+                var content = StringUtils.stringBuilder();
+                var currentChild = listItem.getFirstChild();
+                while (currentChild != null) {
+                    content.append(renderer.render(currentChild));
+                    currentChild = currentChild.getNext();
+                }
+                parsedNotifications.add(Notification.notification(dateTime, Formats.HTML, content.toString()));
+            }
+        }
+        visitChildren(listItem);
+    }
+}
