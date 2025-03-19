524a75b431d00bcd236d532d3087bf6a2c4ccc24
Mārtiņš Avots <martins.avots@splitcells.net>
2025-03-19 21:14:47 +0100

    Define init method for renderers #60
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/ProjectRenderer.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/ProjectRenderer.java
@@ -117,6 +117,10 @@ public interface ProjectRenderer extends Renderer {
                 , Optional.of(projectsRenderer));
     }
 
+    default void init(ProjectsRenderer renderer) {
+        // Nothing needs to be done.
+    }
+
     FileSystemView projectFileSystem();
 
     default Optional<PageMetaData> metaData(String path, ProjectsRenderer projectsRenderer) {
