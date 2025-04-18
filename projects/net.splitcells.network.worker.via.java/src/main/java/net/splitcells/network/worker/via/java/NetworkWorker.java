c0846db87c2933dddc2ab203289d02846011a420
Mārtiņš Avots <martins.avots@splitcells.net>
2025-04-18 20:56:05 +0200

    Consolidate classes #60
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.network.worker.via.java/src/main/java/net/splitcells/network/worker/via/java/NetworkWorker.java
+++ projects/net.splitcells.network.worker.via.java/src/main/java/net/splitcells/network/worker/via/java/NetworkWorker.java
@@ -17,14 +17,57 @@ package net.splitcells.network.worker.via.java;
 
 import net.splitcells.network.worker.via.java.execution.WorkerExecution;
 import net.splitcells.network.worker.via.java.execution.WorkerExecutionConfig;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 import java.util.Optional;
 import java.util.function.Function;
 
+import static net.splitcells.dem.resource.communication.log.Logs.logs;
 import static net.splitcells.network.worker.via.java.execution.WorkerExecution.workerExecution;
 import static net.splitcells.network.worker.via.java.execution.WorkerExecutionConfig.workerExecutionConfig;
 
 public class NetworkWorker {
+
+    public static void main(String... args) {
+        final var options = new Options();
+        final var testAtRemote = Option.builder()
+                .argName("test-at-remote")
+                .hasArg()
+                .required(false)
+                .longOpt("test-at-remote")
+                .desc("This is the ssh address for testing in the form of `username@host`.")
+                .build();
+        final var bootstrapRemote = Option.builder()
+                .argName("bootstrap-remote")
+                .hasArg()
+                .required(false)
+                .longOpt("bootstrap-remote")
+                .desc("This is the ssh address for bootstrapping in the form of `username@host`.")
+                .build();
+        options.addOption(testAtRemote);
+        options.addOption(bootstrapRemote);
+        final var parser = new DefaultParser();
+        final var formatter = new HelpFormatter();
+        try {
+            final var cmd = parser.parse(options, args);
+            if (cmd.hasOption(testAtRemote)) {
+                networkWorker().testAtRemote(cmd.getParsedOptionValue(testAtRemote), c -> c.withDryRun(false));
+            } else if (cmd.hasOption(bootstrapRemote)) {
+                networkWorker().bootstrapRemote(cmd.getParsedOptionValue(bootstrapRemote), c -> c.withDryRun(false).withVerbose(true));
+            } else {
+                logs().append("No action to be done is present in the arguments.");
+                System.exit(1);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            formatter.printHelp("worker", options);
+            System.exit(1);
+        }
+    }
+
     public static NetworkWorker networkWorker() {
         return new NetworkWorker();
     }
