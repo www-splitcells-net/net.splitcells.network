51bf0686112d3c0e2574c7ffa86431de60af2081
Mārtiņš Avots <martins.avots@splitcells.net>
2025-06-09 15:03:20 +0200

    Define function call chains #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/lang/geal/FunctionCallChainDesc.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.lang.geal;
+
+import lombok.Getter;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.gel.editor.lang.SourceCodeQuotation;
+import net.splitcells.gel.editor.lang.SourceCodeQuote;
+
+public class FunctionCallChainDesc implements SourceCodeQuotation {
+    public static FunctionCallChainDesc functionCallChainDesc(ExpressionDesc expression
+            , List<FunctionCallDesc> functionCalls
+            , SourceCodeQuote quote) {
+        return new FunctionCallChainDesc(expression, functionCalls, quote);
+    }
+
+    private final @Getter ExpressionDesc expression;
+    private final @Getter List<FunctionCallDesc> functionCalls;
+    private final @Getter SourceCodeQuote sourceCodeQuote;
+
+    private FunctionCallChainDesc(ExpressionDesc argExpression, List<FunctionCallDesc> argFunctionCalls
+            , SourceCodeQuote quote) {
+        expression = argExpression;
+        functionCalls = argFunctionCalls;
+        sourceCodeQuote = quote;
+    }
+}
