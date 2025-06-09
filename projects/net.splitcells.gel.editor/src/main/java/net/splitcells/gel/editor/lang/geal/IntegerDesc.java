fa4aef5abcf39266a231e67dd70d8a2a499258c0
Mārtiņš Avots <martins.avots@splitcells.net>
2025-06-09 14:23:18 +0200

    Define primitive type #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/lang/geal/IntegerDesc.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.lang.geal;
+
+import lombok.Getter;
+import net.splitcells.gel.editor.lang.SourceCodeQuotation;
+import net.splitcells.gel.editor.lang.SourceCodeQuote;
+
+public class IntegerDesc implements SourceCodeQuotation {
+
+    public static IntegerDesc integerDesc(int value, SourceCodeQuote sourceCodeQuote) {
+        return new IntegerDesc(value, sourceCodeQuote);
+    }
+
+    private IntegerDesc(int argValue, SourceCodeQuote argSourceCodeQuote) {
+        value = argValue;
+        sourceCodeQuote = argSourceCodeQuote;
+    }
+
+    private @Getter final int value;
+    private @Getter final SourceCodeQuote sourceCodeQuote;
+}
---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/lang/geal/NameDesc.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.lang.geal;
+
+import lombok.Getter;
+import net.splitcells.gel.editor.lang.SourceCodeQuotation;
+import net.splitcells.gel.editor.lang.SourceCodeQuote;
+
+import java.util.regex.Pattern;
+
+import static net.splitcells.dem.lang.tree.TreeI.tree;
+import static net.splitcells.dem.utils.ExecutionException.execException;
+
+public class NameDesc implements SourceCodeQuotation {
+    public static NameDesc nameDesc(String value, SourceCodeQuote sourceCodeQuote) {
+        return new NameDesc(value, sourceCodeQuote);
+    }
+
+    private static final Pattern PATTERN = Pattern.compile("[a-zA-Z]+[a-zA-Z0-9]*");
+    private final String value;
+    private @Getter final SourceCodeQuote sourceCodeQuote;
+
+    private NameDesc(String argValue, SourceCodeQuote argSourceCodeQuote) {
+        if (!PATTERN.matcher(argValue).matches()) {
+            throw execException(tree("The name is invalid.").withProperty("value", argValue)
+                    .withProperty("source code quote", argSourceCodeQuote.toString()));
+        }
+        value = argValue;
+        sourceCodeQuote = argSourceCodeQuote;
+    }
+}
