274ae90c07889d0de035b569e4790a729d7cec57
Mārtiņš Avots <martins.avots@splitcells.net>
2025-06-10 21:25:11 +0200

    Draft attribute parsing #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/executors/AttributeExecutor.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/executors/AttributeExecutor.java
@@ -17,6 +17,8 @@ package net.splitcells.gel.editor.executors;
 
 import lombok.Getter;
 import lombok.Setter;
+import net.splitcells.gel.data.view.attribute.Attribute;
+import net.splitcells.gel.data.view.attribute.AttributeI;
 import net.splitcells.gel.editor.Editor;
 import net.splitcells.gel.editor.EditorParser;
 import net.splitcells.gel.editor.EditorTest;
@@ -30,6 +32,8 @@ import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.utils.ExecutionException.execException;
 import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 import static net.splitcells.dem.utils.NotImplementedYet.throwNotImplementedYet;
+import static net.splitcells.gel.data.view.attribute.AttributeI.integerAttribute;
+import static net.splitcells.gel.data.view.attribute.AttributeI.stringAttribute;
 import static net.splitcells.gel.editor.EditorParser.*;
 
 public class AttributeExecutor implements FunctionCallExecutor {
@@ -59,16 +63,21 @@ public class AttributeExecutor implements FunctionCallExecutor {
         final var first = functionCall.getArguments().get(0);
         final NameDesc firstName;
         switch (first) {
-            case NameDesc n -> {
-                firstName = n;
-            }
+            case NameDesc n -> firstName = n;
             default ->
                     throw execException("The first argument has to be a name, but " + first.getClass() + " was given.");
         }
+        final var second = functionCall.getArguments().get(1);
+        final NameDesc secondName;
+        switch (first) {
+            case NameDesc n -> secondName = n;
+            default ->
+                    throw execException("The second argument has to be a name, but " + first.getClass() + " was given.");
+        }
         if (firstName.getValue().equals(INTEGER_TYPE)) {
-            
+            result = Optional.of(integerAttribute(secondName.getValue()));
         } else if (firstName.getValue().equals(STRING_TYPE)) {
-
+            result = Optional.of(stringAttribute(secondName.getValue()));
         } else {
             throw execException("The first argument has to be a reference to the integer or the string type, but " + firstName.getValue() + " was given.");
         }
