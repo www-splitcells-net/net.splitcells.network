965e9702012f46c36fe308b89ddb5149fec90b7f
Mārtiņš Avots <martins.avots@splitcells.net>
2025-07-16 19:35:23 +0200

    Draft function call parser #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/parser/FunctionCallParser.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.geal.parser;
+
+import net.splitcells.dem.source.geal.GealParser;
+import net.splitcells.gel.editor.geal.lang.FunctionCallChainDesc;
+import net.splitcells.gel.editor.geal.lang.FunctionCallDesc;
+
+public class FunctionCallParser extends net.splitcells.dem.source.geal.GealParserBaseVisitor<FunctionCallDesc> {
+    public static FunctionCallDesc parseFunctionCall(GealParser.Function_callContext arg) {
+        return new FunctionCallParser().visitFunction_call(arg);
+    }
+
+    private FunctionCallParser() {
+
+    }
+}
