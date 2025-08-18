995fcf6ba8df57fdb457c08396842eeaaf7bb296
Mārtiņš Avots <martins.avots@splitcells.net>
2025-08-18 19:25:12 +0200

    Define format parsing method #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/Format.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/Format.java
@@ -16,18 +16,43 @@
 package net.splitcells.website;
 
 import net.splitcells.dem.resource.ContentType;
+import net.splitcells.website.server.project.renderer.extension.commonmark.CommonMarkIntegration;
+
+import static net.splitcells.dem.utils.ExecutionException.execException;
+import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 
 /**
  * TODO REMOVE This is a duplicate of {@link ContentType}.
  */
 public enum Format {
-    HTML("text/html")
-    , CSS("text/css")
-    , CSV("text/csv")
-    , TEXT_PLAIN("text/plain")
-    , COMMON_MARK("text/markdown")
-    , JSON("application/json")
-    , XML("application/xml");
+    HTML("text/html"),
+    CSS("text/css"),
+    CSV("text/csv"),
+    TEXT_PLAIN("text/plain"),
+    COMMON_MARK("text/markdown"),
+    JSON("application/json"),
+    XML("application/xml");
+
+    public static Format parse(String arg) {
+        switch (arg) {
+            case "text/html":
+                return HTML;
+            case "text/css":
+                return CSS;
+            case "text/csv":
+                return CSV;
+            case "text/plain":
+                return TEXT_PLAIN;
+            case "text/markdown":
+                return COMMON_MARK;
+            case "application/json":
+                return JSON;
+            case "application/xml":
+                return XML;
+            default:
+                throw execException("Unknown format: " + arg);
+        }
+    }
 
     private final String mimeTypes;
 
