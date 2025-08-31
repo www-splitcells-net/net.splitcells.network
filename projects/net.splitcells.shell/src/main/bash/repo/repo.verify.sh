ebc16bf3687fd5d4ee467dc88da7fc53278be462
Mārtiņš Avots <martins.avots@splitcells.net>
2025-08-31 16:47:33 +0200

    Note repo.verify functionality #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.shell/src/main/bash/repo/repo.verify.sh
+++ projects/net.splitcells.shell/src/main/bash/repo/repo.verify.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+# Checks the data integrity of the repo at the current folder.
+
 command.managed.execute conjunction repo.verify
