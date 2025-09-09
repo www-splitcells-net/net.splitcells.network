67367919307090f3cee0bf0f60540891ec1cd254
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-09 22:24:04 +0200

    Create helper constructor #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/communication/log/LogMessageI.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/communication/log/LogMessageI.java
@@ -20,34 +20,38 @@ import net.splitcells.dem.object.Discoverable;
 
 public class LogMessageI<T> implements LogMessage<T> {
 
-	public static <T> LogMessage<T> logMessage(T content, Discoverable context, LogLevel priority) {
-		return new LogMessageI<>(content, context, priority);
-	}
+    public static <T> LogMessage<T> logMessage(T content, LogLevel priority) {
+        return new LogMessageI<>(content, Discoverable.EXPLICIT_NO_CONTEXT, priority);
+    }
 
-	private final T content;
-	private final LogLevel priority;
-	private final Discoverable context;
+    public static <T> LogMessage<T> logMessage(T content, Discoverable context, LogLevel priority) {
+        return new LogMessageI<>(content, context, priority);
+    }
 
-	private LogMessageI(T content, Discoverable context, LogLevel priority) {
-		this.content = content;
-		this.context = context;
-		this.priority = priority;
+    private final T content;
+    private final LogLevel priority;
+    private final Discoverable context;
 
-	}
+    private LogMessageI(T content, Discoverable context, LogLevel priority) {
+        this.content = content;
+        this.context = context;
+        this.priority = priority;
 
-	@Override
-	public T content() {
-		return content;
-	}
+    }
 
-	@Override
-	public LogLevel priority() {
-		return priority;
-	}
+    @Override
+    public T content() {
+        return content;
+    }
 
-	@Override
-	public List<String> path() {
-		return context.path();
-	}
+    @Override
+    public LogLevel priority() {
+        return priority;
+    }
+
+    @Override
+    public List<String> path() {
+        return context.path();
+    }
 
 }
