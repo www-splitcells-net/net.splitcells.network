ff009db81057b8a4603f0346da9a7d3359040afe
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-23 22:17:09 +0200

    Define TreesException #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/utils/TreesException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.dem.utils;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.lang.annotations.JavaLegacyArtifact;
+import net.splitcells.dem.lang.tree.Tree;
+
+/**
+ * Provides a way to throw multiple {@link Tree} as an error without having to define a root {@link Tree}.
+ * This makes error messages, that are combined of multiple {@link Tree}, more compact.
+ */
+@JavaLegacyArtifact
+@Accessors(chain = true)
+public class TreesException extends RuntimeException {
+
+    public static TreesException treesException(List<Tree> argTrees) {
+        return new TreesException(argTrees);
+    }
+
+    @Getter private final List<Tree> trees;
+
+    private TreesException(List<Tree> argTrees) {
+        trees = argTrees;
+    }
+}
