2686972c0bebb8840d3e768361ab5790229c1cf0
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-28 00:47:29 +0200

    Use one browser per CPU core, #8
    in order to avoid blockage between each test thread,
    which wastes time.
    
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClients.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClients.java
@@ -28,7 +28,6 @@ import static net.splitcells.website.server.client.HtmlClientImpl.htmlClientImpl
 import static net.splitcells.website.server.client.HtmlClientSharer.htmlClientSharer;
 
 public class HtmlClients {
-    private static final HtmlClient ROOT_CLIENT = htmlClientImpl();
     private static final List<HtmlClient> FREE_HTML_CLIENT = list();
     private static final List<HtmlClient> USED_HTML_CLIENT = list();
     private static final Semaphore HTML_CLIENT_LOCK = new Semaphore(1);
@@ -70,7 +69,7 @@ public class HtmlClients {
                 return htmlClient;
             }
             if (MAX_HTML_CLIENT_COUNT > USED_HTML_CLIENT.size()) {
-                htmlClient = htmlClientSharer(ROOT_CLIENT, sharer -> {
+                htmlClient = htmlClientSharer(htmlClientImpl(), sharer -> {
                     try {
                         HTML_CLIENT_LOCK.acquireUninterruptibly();
                         FREE_HTML_CLIENT.add(sharer);
