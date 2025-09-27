b577c25e7681cab56957edcb5f894f9eba05d928
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-28 00:25:48 +0200

    Try out single browser instance #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClients.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/HtmlClients.java
@@ -63,7 +63,7 @@ public class HtmlClients {
                 return htmlClient;
             }
             if (MAX_HTML_CLIENT_COUNT > USED_HTML_CLIENT.size()) {
-                htmlClient = htmlClientSharer(htmlClientImpl(), sharer -> {
+                htmlClient = htmlClientSharer(ROOT_CLIENT, sharer -> {
                     try {
                         HTML_CLIENT_LOCK.acquireUninterruptibly();
                         FREE_HTML_CLIENT.add(sharer);
