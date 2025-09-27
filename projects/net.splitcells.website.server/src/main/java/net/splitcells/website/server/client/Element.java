caf950215ca13fc389eb410a9e8912dc203889ef
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-27 22:12:36 +0200

    Note optionality of browser Element #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/Element.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/client/Element.java
@@ -18,12 +18,35 @@ package net.splitcells.website.server.client;
 import java.util.Optional;
 import java.util.function.Function;
 
+/**
+ * This represents a Browser element, that may or may not exist.
+ */
 public interface Element {
+    /**
+     *
+     * @param evaluation This {@link Function} is executed given this {@link Element},
+     *                   if the underlying Browser element actually exists.
+     * @param <T>        The return type of the evaluation.
+     * @return Returns {@link Optional#empty()}, if this {@link Element} does not exist.
+     * Otherwise, this returns the result of the evaluation.
+     */
     <T> Optional<T> evalIfExists(Function<Element, T> evaluation);
 
+    /**
+     * This method only works, if the element actually exists.
+     * @see {@link #evalIfExists(Function)}
+     */
     void click();
 
+    /**
+     * This method only works, if the element actually exists.
+     * @see {@link #evalIfExists(Function)}
+     */
     String textContent();
 
+    /**
+     * This method only works, if the element actually exists.
+     * @see {@link #evalIfExists(Function)}
+     */
     String value();
 }
