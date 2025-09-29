25759899b4d98f93bd6c523ee00b128423d490b4
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-29 22:12:44 +0200

    Ensure that first mvn wrapper run works #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/env.setup.sh
+++ bin/env.setup.sh
@@ -4,7 +4,11 @@
 
 # Execute this command via `. bin/env.setup.sh`, in order to setup the environment of the current shell.
 
-mvn wrapper:wrapper -Dmaven=3.9.9 # Sets up the Maven version, in order to avoid build problems regarding the site goal.
-export PATH="$(realpath ./):$PATH"
-echo "cd '$(pwd)' && mvnw \$@" > mvn
-chmod +x mvn
+# Build BOMs first, as otherwise `mvn wrapper:wrapper` will not run, because the dependency BOM with the scope import cannot be read.
+  cd ../net.splitcells.network.bom.base && mvn clean install
+  cd ../net.splitcells.network.bom && mvn clean install
+cd ../net.splitcells.network
+  mvn wrapper:wrapper -Dmaven=3.9.9 # Sets up the Maven version, in order to avoid build problems regarding the site goal.
+  export PATH="$(realpath ./):$PATH"
+  echo "cd '$(pwd)' && mvnw \$@" > mvn
+  chmod +x mvn
