1d034bd7f6d8b7af418a265d4c4d7fd39a946d87
Mārtiņš Avots <martins.avots@splitcells.net>
2025-10-17 01:12:40 +0200

    Draft function call registry #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/FunctionCallRegistry.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.geal;
+
+import lombok.experimental.Accessors;
+import net.splitcells.dem.data.set.list.List;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
+
+@Accessors(chain = true)
+public class FunctionCallRegistry {
+    public static FunctionCallRegistry functionCallRegistry() {
+        return new FunctionCallRegistry();
+    }
+
+    private final List<FunctionCallRecord> functionCallRecords = list();
+
+    private FunctionCallRegistry() {
+
+    }
+
+    public FunctionCallRegistry addRecord(FunctionCallRecord record) {
+        if (true) {
+            throw notImplementedYet("Checking for duplicate records is not implemented yet.");
+        }
+        functionCallRecords.add(record);
+        return this;
+    }
+}
