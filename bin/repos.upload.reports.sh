e8be4d95a098cff9fd151b74d60c5169faf85df8
Mārtiņš Avots <martins.avots@splitcells.net>
2025-10-24 18:41:27 +0200

    Make upload script of workflow result upload #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ bin/repos.upload.reports.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+set +x # Prevent printing secrets.
+# -z is used because -v is not supported in Forgejo workflows.
+if [ -z "$NET_SPLITCELLS_MARTINS_AVOTS_WEBSITE_SFTP_PRIVATE_KEY" ]; then
+  echo Upload to https://splitcells.net is disabled, because the environment variable NET_SPLITCELLS_MARTINS_AVOTS_WEBSITE_SFTP_PRIVATE_KEY is not set.
+else
+  mkdir -p ~/.ssh-website-upload/
+  echo "$NET_SPLITCELLS_MARTINS_AVOTS_WEBSITE_SFTP_PRIVATE_KEY" > ~/.ssh-website-upload/id_rsa
+  chmod 700 ~/.ssh-website-upload/id_rsa # Otherwise, scp may not work.
+  mkdir -p target/website-upload/public_html/net/splitcells/martins/avots/website/jacoco-aggregate # Using subdirectories, ensures, that scp does not get a problem with missing directories.
+  cp -r target/site/jacoco-aggregate/* target/website-upload/public_html/net/splitcells/martins/avots/website/jacoco-aggregate
+  ls -al target/website-upload/public_html/net/splitcells/martins/avots/website/jacoco-aggregate # TODO This is debug code.
+  set -x
+  set -e
+  realpath target/website-upload/public_html/net/splitcells/martins/avots/website/jacoco-aggregate
+  # The targeted scp upload folder is dependent on the relative source path. Therefore, cd and the current folder are used, in order to avoid this strange mapping.
+  cd target/website-upload/
+  scp -v -i ~/.ssh-website-upload/id_rsa -o PubkeyAuthentication=yes -o StrictHostKeyChecking=no -r ./ splitcm@www322.your-server.de:
+  rm ~/.ssh-website-upload/id_rsa
+fi
