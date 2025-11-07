ec0e2d8bc5a10e324fb0c1167c5b33ffda35336f
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-08 00:27:51 +0100

    Test Not Rater #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/lib/NotTest.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/lib/NotTest.java
@@ -15,11 +15,38 @@
  */
 package net.splitcells.gel.rating.rater.lib;
 
+import lombok.val;
 import net.splitcells.dem.testing.annotations.UnitTest;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.gel.Gel.defineProblem;
+import static net.splitcells.gel.data.view.attribute.AttributeI.attribute;
+import static net.splitcells.gel.rating.rater.lib.HasSize.hasSize;
+import static net.splitcells.gel.rating.rater.lib.Not.not;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
 
 public class NotTest {
     @UnitTest
     public void test() {
-        
+        final var testData = defineProblem("test")
+                .withDemandAttributes()
+                .withDemands(list(list(), list()))
+                .withSupplyAttributes()
+                .withSupplies(list(list(), list()))
+                .withConstraint(r -> {
+                    r.then(not(hasSize(1)));
+                    return r;
+                }).toProblem()
+                .asSolution();
+        testData.constraint().rating().requireEqualsTo(noCost());
+        val first = testData.assign(testData.demandsFree().anyLine(), testData.suppliesFree().anyLine());
+        testData.constraint().rating().requireEqualsTo(cost(1));
+        val second = testData.assign(testData.demandsFree().anyLine(), testData.suppliesFree().anyLine());
+        testData.constraint().rating().requireEqualsTo(noCost());
+        testData.remove(first);
+        testData.constraint().rating().requireEqualsTo(cost(1));
+        testData.remove(second);
+        testData.constraint().rating().requireEqualsTo(noCost());
     }
 }
