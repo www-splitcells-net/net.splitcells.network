d6f2efef7baf248f7989662833d2929d56b37acd
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-07 17:20:49 +0100

    Move large string to dedicated constant #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/runners/ThenCallRunner.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/runners/ThenCallRunner.java
@@ -46,18 +46,20 @@ public class ThenCallRunner implements FunctionCallRunner {
         Rater rater;
     }
 
+    private static final String DESCRIPTION = """
+            Creates and returns a constraint node, that groups and rates incoming lines by the given rater.
+            Lines with a none zero cost are not propagated to the child constraints.
+            The constraint node is added to the receiver, which is the parent constraint.
+            The rating of a given line in a then constraint node is the sum of the constraint node's rating and its children's rating.
+            """;
+
     private static final FunctionCallRunnerParser<Args> PARSER = functionCallRunnerParser(THEN_NAME, 1
             , fcr -> {
                 val args = new Args();
                 args.subjectVal = fcr.parseQuerySubject();
                 fcr.requireArgumentCount(1);
                 args.rater = fcr.parseArgument(Rater.class, 0, "rater");
-                fcr.addDescription(tree("paragraph", SEW).withText("""
-                        Creates and returns a constraint node, that groups and rates incoming lines by the given rater.
-                        Lines with a none zero cost are not propagated to the child constraints.
-                        The constraint node is added to the receiver, which is the parent constraint.
-                        The rating of a given line in a then constraint node is the sum of the constraint node's rating and its children's rating.
-                        """));
+                fcr.addDescription(tree("paragraph", SEW).withText(DESCRIPTION));
                 return args;
             });
 
