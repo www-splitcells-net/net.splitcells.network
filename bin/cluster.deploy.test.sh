29411ba4f199e25ec4054b815b4f287b530665e8
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-09 01:40:05 +0100

    Create empty cluster test command #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ bin/cluster.deploy.test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+# Deploys `repos.test` on all available servers.
