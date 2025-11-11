7ade9432b8d5e64b342bbe6857a19cb5505f3733
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-11 18:27:40 +0100

    Draft Not parser #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/runners/NotRunner.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.geal.runners;
+
+import lombok.val;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.gel.editor.Editor;
+import net.splitcells.gel.editor.geal.lang.FunctionCallDesc;
+import net.splitcells.gel.rating.rater.framework.Rater;
+
+import java.util.Optional;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.gel.editor.geal.runners.FunctionCallRunnerParser.functionCallRunnerParser;
+
+public class NotRunner implements FunctionCallRunner {
+    private FunctionCallRunner notRunner() {
+        return new NotRunner();
+    }
+
+    private static class Args {
+        Rater rater;
+    }
+
+    private NotRunner() {
+
+    }
+
+    private static final FunctionCallRunnerParser<Args> PARSER = functionCallRunnerParser("not"
+            , 1
+            , fcr -> {
+                val args = new Args();
+                fcr.requireSubjectAbsence();
+                args.rater = fcr.parseArgument(Rater.class, 0, "rater");
+                fcr.requireArgumentCount(1);
+                return args;
+            });
+
+    @Override public FunctionCallRun execute(FunctionCallDesc functionCall, Optional<Object> subject, Editor context) {
+        return null;
+    }
+
+    @Override public List<FunctionCallRunnerParser<?>> parsers() {
+        return list(PARSER);
+    }
+}
