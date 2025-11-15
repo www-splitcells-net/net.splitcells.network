29f835ee0526861ddfb2f4bcbb86efdf2b1493d8
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-15 23:03:27 +0100

    Scan for test servers #30

--- bin/cluster.deploy.test.sh
+++ bin/cluster.deploy.test.sh
@@ -5,3 +5,6 @@
 # The word cluster and not network is used, in order to have a dedicated name for deployed server.
 # Thereby, the word network is not overloaded with different meanings.
 # Deploys `repos.test` on all available servers.
+
+../net.splitcells.network.hub/bin/net.splitcells.osi.repos.peers \
+  | xargs -i sh -c "cd ../{} && test -f bin/cluster.node.ssh.addresses.sh && bin/cluster.node.ssh.addresses.sh" \
