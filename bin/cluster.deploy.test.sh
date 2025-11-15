41676e4b04fa96ca631fbc172ef2dae48b82200e
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-15 22:47:28 +0100

    Reason the use of the word cluster #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/cluster.deploy.test.sh
+++ bin/cluster.deploy.test.sh
@@ -2,4 +2,6 @@
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 
+# The word cluster and not network is used, in order to have a dedicated name for deployed server.
+# Thereby, the word network is not overloaded with different meanings.
 # Deploys `repos.test` on all available servers.
