865633c0bd900c22bbc50bec31d4129c38649a49
Mārtiņš Avots <martins.avots@splitcells.net>
2025-12-12 23:27:30 +0100

    Define path of colloquium example #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/ColloquiumExample.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/ColloquiumExample.java
@@ -15,16 +15,36 @@
  */
 package net.splitcells.gel.ui.editor.geal;
 
+import net.splitcells.dem.utils.StringUtils;
+import net.splitcells.website.Format;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.processor.BinaryMessage;
+import net.splitcells.website.server.projects.ProjectsRendererI;
 import net.splitcells.website.server.projects.RenderRequest;
 import net.splitcells.website.server.projects.extension.ProjectsRendererExtension;
 
-public class ColloquiumExample implements ProjectsRendererExtension  {
+import java.util.Optional;
+
+import static net.splitcells.website.server.processor.BinaryMessage.binaryMessage;
+
+public class ColloquiumExample implements ProjectsRendererExtension {
+    private static final String PATH = "/net/splitcells/gel/ui/editor/geal/example/colloquium-planning.json";
+
     public static ColloquiumExample colloquiumExample() {
         return new ColloquiumExample();
     }
+
     private ColloquiumExample() {
 
     }
+
+    @Override
+    public Optional<BinaryMessage> renderFile(String path, ProjectsRendererI projectsRendererI, Config config) {
+        if (PATH.equals(path)) {
+        }
+        return Optional.empty();
+    }
+
     @Override public boolean requiresAuthentication(RenderRequest request) {
         return false;
     }
