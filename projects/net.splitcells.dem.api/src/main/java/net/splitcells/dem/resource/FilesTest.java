0fb25bd3e6611b5e6325edaa4a2e3d99db129c26
Mārtiņš Avots <martins.avots@splitcells.net>
2025-12-28 14:30:25 +0100

    Only use temp folder for tests #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FilesTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FilesTest.java
@@ -43,7 +43,7 @@ public class FilesTest {
     }
 
     @UnitTest public void testCreateDirectory() {
-        processInTemporaryFolder(p -> createDirectory(Path.of("test")));
+        processInTemporaryFolder(p -> createDirectory(p.resolve("test")));
     }
 
     @UnitTest public void testCreateDirectoryInvalid() {
