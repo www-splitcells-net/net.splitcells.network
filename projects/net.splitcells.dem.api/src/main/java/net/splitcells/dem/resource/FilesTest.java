bf13f1afc069ff311f10691cc46984f8caf6738d
Mārtiņš Avots <martins.avots@splitcells.net>
2026-01-02 20:37:34 +0100

    Try fixing CI error #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FilesTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FilesTest.java
@@ -55,8 +55,8 @@ public class FilesTest {
      */
     @UnitTest public void testCreateDirectoryInvalid() {
         requireThrow(() -> {
-            val testPath = Path.of("\\" + multiple("//\\", 2000));
-            logs().append(testPath.toString());
+            val testPath = Path.of("\\" + multiple("//\\\\", 2000));
+            System.out.println(testPath.toString());
             processInTemporaryFolder(p -> createDirectory(testPath));
         });
     }
