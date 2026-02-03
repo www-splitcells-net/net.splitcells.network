f45e02b453cfc01eba695bcd4a335d345c0022fe
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-04 00:32:19 +0100

    Avoid accidental test correctness #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/example/SportCourseExampleTest.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/example/SportCourseExampleTest.java
@@ -14,7 +14,7 @@ import static net.splitcells.gel.ui.editor.geal.example.SportCourseExample.stude
 
 public class SportCourseExampleTest {
     @UnitTest public void testStudentChoicesCsv() {
-        requireEquals(studentChoicesCsv(randomness(1), 1),
+        requireEquals(studentChoicesCsv(randomness(1), 2),
                 """
                         Student,Chosen Sport, Chosen Sport Type, Is Secondary Choice
                         1,Volleyball,Team sport,0
@@ -22,6 +22,11 @@ public class SportCourseExampleTest {
                         1,Parkour,Other sport,0
                         1,Handball,Team sport,1
                         1,Gymnastics,Individual sport,1
+                        2,Volleyball,Team sport,0
+                        2,Swimming,Individual sport,0
+                        2,Yoga,Other sport,0
+                        2,Basketball,Team sport,1
+                        2,Running,Individual sport,1
                         """
         );
     }
