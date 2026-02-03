ff3dbfc480d75c78db3875a7e6f93d071c374edf
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-04 00:48:55 +0100

    Test available courses factory #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/example/SportCourseExampleTest.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/example/SportCourseExampleTest.java
@@ -32,6 +32,33 @@ public class SportCourseExampleTest {
     }
 
     @UnitTest public void testAvailableCoursesCsv() {
-        System.out.println(availableCoursesCsv(randomness(1), 2, 1, 1, 2, 3));
+        requireEquals(availableCoursesCsv(randomness(1), 2, 2, 1, 2, 3),
+                """
+                        Assigned Course Number,Assigned Sport,Assigned Sport Type
+                        1,Team sport,Handball
+                        1,Team sport,Handball
+                        2,Individual sport,Swimming
+                        2,Individual sport,Swimming
+                        3,Individual sport,Running
+                        3,Individual sport,Running
+                        4,Individual sport,Climbing
+                        4,Individual sport,Climbing
+                        5,Individual sport,Parkour
+                        5,Individual sport,Parkour
+                        6,Individual sport,Parkour
+                        6,Individual sport,Parkour
+                        7,Team sport,Football
+                        7,Team sport,Football
+                        8,Individual sport,Gymnastics
+                        8,Individual sport,Gymnastics
+                        9,Individual sport,Badminton
+                        9,Individual sport,Badminton
+                        10,Individual sport,Yoga
+                        10,Individual sport,Yoga
+                        11,Individual sport,Parkour
+                        11,Individual sport,Parkour
+                        12,Individual sport,Parkour
+                        12,Individual sport,Parkour
+                        """);
     }
 }
