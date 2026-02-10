ed819b9d65463ed8c62e7c0159aa7eb8b7560d3e
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-10 05:59:16 +0100

    Add license info #68
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.project/src/main/python/net/splitcells/project/repo/repos.process.v2.py
+++ projects/net.splitcells.project/src/main/python/net/splitcells/project/repo/repos.process.v2.py
@@ -4,6 +4,11 @@ SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 """
 
+__author__ = "Mārtiņš Avots"
+__authors__ = ["Contributors To The `net.splitcells.*` Projects"]
+__copyright__ = "Copyright 2024"
+__license__ = "EPL-2.0 OR GPL-2.0-or-later"
+
 import argparse
 import logging
 import unittest
