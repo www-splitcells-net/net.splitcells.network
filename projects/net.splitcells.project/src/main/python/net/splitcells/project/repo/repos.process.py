708febc2363646a3060a61b9f997ef2f2d78c052
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-11 18:44:19 +0100

    Softly migrate to repo process v2 #68
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.project/src/main/python/net/splitcells/project/repo/repos.process.py
+++ projects/net.splitcells.project/src/main/python/net/splitcells/project/repo/repos.process.py
@@ -61,6 +61,7 @@ import argparse
 import json
 import logging
 import re
+import sys
 from os import (environ, getcwd)
 from pathlib import Path
 
@@ -175,6 +176,9 @@ def processSub(relativePath, host, command, commandForMissing, commandForUnknown
 		return False
 	return True
 if __name__ == '__main__':
+	if environ.get('repo_process_v2') == '1':
+		print('Using +' + str(['repos.process.v2'] + sys.argv[1:]))
+		exit(subprocess.call(['repos.process.v2'] + sys.argv[1:]))
 	if environ.get('log_level') == 'debug':
 		logging.basicConfig(level=logging.DEBUG)
 	parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
