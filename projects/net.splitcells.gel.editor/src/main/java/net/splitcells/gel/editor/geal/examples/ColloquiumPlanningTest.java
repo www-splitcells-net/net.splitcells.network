0288264aa6ac376460ebecec4264443a91bd1578
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-12 17:52:35 +0100

    Format code #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/examples/ColloquiumPlanningTest.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/geal/examples/ColloquiumPlanningTest.java
@@ -1,15 +1,4 @@
 /*
- * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
- * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
- *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
