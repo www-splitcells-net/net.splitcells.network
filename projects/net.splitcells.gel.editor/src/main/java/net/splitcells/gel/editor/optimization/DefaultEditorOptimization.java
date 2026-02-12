f46886e80b678dfd04f385f250d1af52eecba3ac
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-12 20:52:33 +0100

    Optimize with enable history #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/DefaultEditorOptimization.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/DefaultEditorOptimization.java
@@ -24,8 +24,9 @@ public class DefaultEditorOptimization implements EditorOptimization {
         solutionPaths.requireSizeOf(1, getClass().getName() + " only supports a list of solutions and not a full tree or even graph of interdependent solutions."
                 + " In other words, every solution is only allowed to have at most 1 solution as its demand or supply."
                 + " Furthermore, the solution's interdependencies are not allowed to form a circle.");
-            solutionPaths.get(0).get(i).optimize(defaultOptimization());
         for (int i = solutionPaths.get(0).size() - 1; i > -1; --i) {
+            val solution = solutionPaths.get(0).get(i);
+            solution.history().processWithHistory(() -> solution.optimize(defaultOptimization()));
         }
     }
 }
