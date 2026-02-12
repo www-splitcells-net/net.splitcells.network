39f885b1c5a76453b5cef3370c8fe539cabdd256
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-12 20:36:23 +0100

    Do simplest default editor optimization #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/DefaultEditorOptimization.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/DefaultEditorOptimization.java
@@ -6,6 +6,10 @@ package net.splitcells.gel.editor.optimization;
 import lombok.val;
 import net.splitcells.gel.editor.Editor;
 
+import java.util.stream.IntStream;
+
+import static net.splitcells.gel.solution.optimization.DefaultOptimization.defaultOptimization;
+
 public class DefaultEditorOptimization implements EditorOptimization {
     public static DefaultEditorOptimization defaultEditorOptimization() {
         return new DefaultEditorOptimization();
@@ -20,5 +24,8 @@ public class DefaultEditorOptimization implements EditorOptimization {
         solutionPaths.requireSizeOf(1, getClass().getName() + " only supports a list of solutions and not a full tree or even graph of interdependent solutions."
                 + " In other words, every solution is only allowed to have at most 1 solution as its demand or supply."
                 + " Furthermore, the solution's interdependencies are not allowed to form a circle.");
+        for (int i = solutionPaths.size() - 1; i > -1; --i) {
+            solutionPaths.get(1).get(i).optimize(defaultOptimization());
+        }
     }
 }
