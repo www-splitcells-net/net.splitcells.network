f11615e2a9e597dd9edf35401be6c62c9e8a8987
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-12 17:12:45 +0100

    Deprecate OfflineOptimization #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/OfflineOptimization.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/optimization/OfflineOptimization.java
@@ -24,8 +24,14 @@ import net.splitcells.gel.solution.SolutionView;
  * when complex queries are needed to be done on the {@link SolutionView} in order to determine the current state after applying all interim {@link OptimizationEvent}s.
  * Also, most of the time it is easier to implement an {@link OnlineOptimization}
  * than an {@link OfflineOptimization}.</p>
+ * 
+ * @deprecated This is deprecated and will be removed 2027, if no real use case is found in the meantime.
+ *             If such a case is found, it needs to noted here.
+ *             This way it will be kown, why this class is still required.
+ *             If this class is removed, arguments againts {@link OfflineOptimization} should be noted at {@link OnlineOptimization}. 
  */
 @FunctionalInterface
+@Deprecated
 public interface OfflineOptimization {
 
 	List<OptimizationEvent> optimize(SolutionView solution);
