b098e3b09ccb0b995b4770c477f6580ea898b783
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-16 21:40:33 +0100

    Load problem for testing #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/example/SportCoursePlanningTest.java
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.editor.example;
+
+import lombok.val;
+import net.splitcells.dem.object.Discoverable;
+import net.splitcells.dem.testing.annotations.UnitTest;
+import net.splitcells.gel.editor.GelEditorFileSystem;
+
+import static net.splitcells.dem.Dem.configValue;
+import static net.splitcells.dem.object.Discoverable.EXPLICIT_NO_CONTEXT;
+import static net.splitcells.gel.editor.Editor.editor;
+
+public class SportCoursePlanningTest {
+    @UnitTest
+    public void test() {
+        val editor = editor(getClass().getName(), EXPLICIT_NO_CONTEXT);
+        editor.interpret(configValue(GelEditorFileSystem.class)
+                .readString("src/main/resources/html/net/splitcells/gel/editor/geal/examples/sports-course-planning.txt"));
+    }
+}
