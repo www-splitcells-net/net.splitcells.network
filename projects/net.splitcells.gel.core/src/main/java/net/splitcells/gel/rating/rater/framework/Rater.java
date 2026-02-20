f8463299c8753d1c1e12aa65df3bf82b890836b4
Mārtiņš Avots <martins.avots@splitcells.net>
2026-02-20 17:53:39 +0100

    Discourage whitespaces for descrpitive paths #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/framework/Rater.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/framework/Rater.java
@@ -165,6 +165,7 @@ public interface Rater extends PubliclyTyped<Rater>
      * 
      * @return Returns a description on the {@link Rater}, that is used for {@link Constraint#path()}.
      * Only use upper and lower case characters, numbers and hyphens.
+     * Avoid whitespaces.
      * Never return a constraint type as the prefix of this name,
      * even though the {@link Rater} is only useful for 1 constraint type,
      * as other code assumes this in order to enable nice constraint tree renderings.
