ec73854cb82f4d40110115b50baa48d78d2c9441
Mārtiņš Avots <martins.avots@splitcells.net>
2026-03-28 20:14:02 +0100

    Simplify test class #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FileSystemViaMemoryTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FileSystemViaMemoryTest.java
@@ -1,5 +1,4 @@
-/*
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
 package net.splitcells.dem.resource;
@@ -7,17 +6,14 @@ package net.splitcells.dem.resource;
 import lombok.val;
 import net.splitcells.dem.testing.TestSuiteI;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.stream.Stream;
 
 import static net.splitcells.dem.resource.FileSystemViaMemory.fileSystemViaMemory;
-import static net.splitcells.dem.testing.TestTypes.UNIT_TEST;
-import static net.splitcells.dem.utils.StreamUtils.concat;
 
-public class FileSystemViaMemoryTest extends TestSuiteI {
-    @Tag(UNIT_TEST) @TestFactory public Stream<DynamicTest> fileSystemTests() {
+public class FileSystemViaMemoryTest {
+    @TestFactory public Stream<DynamicTest> fileSystemTests() {
         return new FileSystemTest().fileSystemWriteTests(() -> {
             val fileSystem = fileSystemViaMemory();
             return new FileSystemTest.FileSystemAccess(fileSystem, fileSystem);
