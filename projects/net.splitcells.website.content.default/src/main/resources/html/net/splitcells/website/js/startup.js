64f549af10e999dd09ad04337fd8d3f19bd5823c
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-08 14:13:21 +0200

    Log browser errors #43
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.content.default/src/main/resources/html/net/splitcells/website/js/startup.js
+++ projects/net.splitcells.website.content.default/src/main/resources/html/net/splitcells/website/js/startup.js
@@ -2,10 +2,17 @@
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
 // Detects whether there is a Javascript in the browser and makes an indicator visible.
-window.onerror = function(error) {
+window.onerror = function(message, source, lineno, colno, error) {
     var elementId_elements = document.getElementsByClassName('net-splitcells-error-status-indicator');
     for (var i = 0; i < elementId_elements.length; i++) {
         elementId_elements[i].style.display = "inherit";
         elementId_elements[i].style.visibility = 'inherit';
     }
+    console.log(`Error:`);
+    console.log(`message: ${message}`);
+    console.log(`source: ${source}`);
+    console.log(`lineno: ${lineno}`);
+    console.log(`colno: ${colno}`);
+    console.log(`error: ${error}`);
+    return true;
 };
