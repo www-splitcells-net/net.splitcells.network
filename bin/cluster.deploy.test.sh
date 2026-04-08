6b4bcbd200e791993cbe7813b053abb5cd070594
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-08 11:37:12 +0200

    Deploy tests in parallel #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/cluster.deploy.test.sh
+++ bin/cluster.deploy.test.sh
@@ -6,6 +6,7 @@
 # Thereby, the word network is not overloaded with different meanings.
 # Deploys `repos.test` on all available servers.
 
+mkdir -p target/cluster.deploy.test.sh
 ../net.splitcells.network.hub/bin/net.splitcells.shell.repos.peers \
   | xargs -i sh -c "cd ../{} && test -f bin/cluster.node.ssh.addresses.sh && bin/cluster.node.ssh.addresses.sh" \
-  | xargs -i sh -c "ssh -T {} echo 1 && bin/worker.test.at {} || echo Skipping {}"
+  | xargs -i -P 10 sh -c "ssh -T {} echo 1 && bin/worker.test.at {} > target/cluster.deploy.test.sh/{}.log 2> target/cluster.deploy.test.sh/{}.error.log || echo Skipping {}"
