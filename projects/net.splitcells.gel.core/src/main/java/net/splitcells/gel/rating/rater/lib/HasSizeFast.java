eb3b6855e1dfca00a2bab03687a5540615c0d060
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-08 21:01:58 +0200

    Implement faster rater #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/rating/rater/lib/HasSizeFast.java
@@ -0,0 +1,223 @@
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.rating.rater.lib;
+
+import lombok.val;
+import net.splitcells.dem.data.set.Set;
+import net.splitcells.dem.data.set.list.List;
+import net.splitcells.dem.lang.dom.Domable;
+import net.splitcells.dem.lang.tree.Tree;
+import net.splitcells.dem.object.Discoverable;
+import net.splitcells.dem.utils.CommonFunctions;
+import net.splitcells.gel.constraint.Constraint;
+import net.splitcells.gel.constraint.GroupId;
+import net.splitcells.gel.data.view.Line;
+import net.splitcells.gel.data.view.View;
+import net.splitcells.gel.rating.framework.Rating;
+import net.splitcells.gel.rating.rater.framework.Rater;
+import net.splitcells.gel.rating.rater.framework.RatingEvent;
+
+import java.util.Optional;
+
+import static net.splitcells.dem.data.set.Sets.toSetOfUniques;
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.lang.tree.TreeI.tree;
+import static net.splitcells.dem.utils.MathUtils.absolute;
+import static net.splitcells.dem.utils.MathUtils.distance;
+import static net.splitcells.gel.constraint.Constraint.*;
+import static net.splitcells.gel.rating.framework.LocalRatingI.localRating;
+import static net.splitcells.gel.rating.rater.framework.RatingEventI.ratingEvent;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
+
+
+public class HasSizeFast implements Rater {
+    public static final String HAS_SIZE_NAME = "hasSizeFast";
+
+    public static HasSizeFast hasSizeFast(int targetSize) {
+        return new HasSizeFast(targetSize);
+    }
+
+    private final int targetSize;
+    private final List<Discoverable> contexts = list();
+
+    private HasSizeFast(int targetSize) {
+        this.targetSize = targetSize;
+    }
+
+    @Override
+    public RatingEvent ratingAfterAddition(View lines, Line addition, List<Constraint> children
+            , View ratingsBeforeAddition) {
+        val previousGroupRating = previousGroupRating(lines, false);
+        val nextGroupRating = nextGroupRating(lines, false);
+        val ratingEvent = ratingEvent();
+        if (previousGroupRating.equalz(noCost()) || ratingsBeforeAddition.isEmpty()) {
+            val localRating = localRating()
+                    .withRating(nextGroupRating)
+                    .withResultingGroupId(addition.value(INCOMING_CONSTRAINT_GROUP));
+            if (nextGroupRating.equalz(noCost())) {
+                localRating.withPropagationTo(children);
+            } else {
+                localRating.withPropagationTo(list());
+            }
+            ratingEvent.additions().put(addition, localRating);
+        } else {
+            val previousRatingLineProcessing = previousRatingLine(ratingsBeforeAddition, Optional.empty())
+                    .orElseThrow()
+                    .value(LINE);
+            val localRating = localRating()
+                    .withRating(nextGroupRating)
+                    .withResultingGroupId(addition.value(INCOMING_CONSTRAINT_GROUP));
+            if (nextGroupRating.equalz(noCost())) {
+                localRating.withPropagationTo(children);
+            } else {
+                localRating.withPropagationTo(list());
+            }
+            ratingEvent.additions().put(addition, localRating);
+            val previousRatingLine = lines.unorderedLinesStream2()
+                    .filter(l -> l.value(LINE).equalsTo(previousRatingLineProcessing))
+                    .findFirst()
+                    .orElseThrow();
+            ratingEvent.updateRating_withReplacement(previousRatingLine,
+                    localRating().
+                            withPropagationTo(children).
+                            withRating(noCost()).
+                            withResultingGroupId(addition.value(INCOMING_CONSTRAINT_GROUP))
+            );
+        }
+        return ratingEvent;
+    }
+
+    @Override
+    public RatingEvent rating_before_removal(View lines
+            , Line removal
+            , List<Constraint> children
+            , View ratingsBeforeRemoval) {
+        val previousGroupRating = previousGroupRating(lines, true);
+        val nextGroupRating = nextGroupRating(lines, true);
+        val ratingEvent = ratingEvent();
+        if (lines.size() > 1 && !previousGroupRating.equalz(nextGroupRating)) {
+            if (previousGroupRating.equalz(noCost())) {
+                val nextRatingContainer = lines.unorderedLine(0);
+                val localRating = localRating()
+                        .withRating(nextGroupRating)
+                        .withResultingGroupId(nextRatingContainer.value(INCOMING_CONSTRAINT_GROUP));
+                if (nextGroupRating.equalz(noCost())) {
+                    localRating.withPropagationTo(children);
+                } else {
+                    localRating.withPropagationTo(list());
+                }
+                ratingEvent.updateRating_withReplacement(nextRatingContainer, localRating);
+            } else {
+                val previousRatingLineProcessing = previousRatingLine(ratingsBeforeRemoval
+                        , Optional.of(ratingsBeforeRemoval.lookup(LINE, removal.value(LINE)).anyLine()));
+                final Line previousRatingLine;
+                if (previousRatingLineProcessing.isPresent()) {
+                    previousRatingLine = lines.unorderedLinesStream2()
+                            .filter(l -> l.value(LINE).equalsTo(previousRatingLineProcessing.get().value(LINE)))
+                            .findFirst()
+                            .orElseThrow();
+                } else {
+                    previousRatingLine = lines.unorderedLinesStream2()
+                            .filter(l -> !l.value(LINE).equalsTo(removal.value(LINE)))
+                            .findFirst()
+                            .orElseThrow();
+                }
+                val localRating = localRating()
+                        .withRating(nextGroupRating)
+                        .withResultingGroupId(previousRatingLine.value(INCOMING_CONSTRAINT_GROUP));
+                if (nextGroupRating.equalz(noCost())) {
+                    localRating.withPropagationTo(children);
+                } else {
+                    localRating.withPropagationTo(list());
+                }
+                ratingEvent.updateRating_withReplacement(previousRatingLine, localRating);
+            }
+        }
+        return ratingEvent;
+    }
+
+    private Optional<Line> previousRatingLine(View lineProcessing, Optional<Line> beforeRemoval) {
+        if (beforeRemoval.isPresent()) {
+            if (!beforeRemoval.orElseThrow().value(RATING).equalz(noCost())) {
+                return Optional.empty();
+            }
+            val removal = beforeRemoval.get().index();
+            return lineProcessing.unorderedLinesStream2().filter(l -> l.value(LINE).index() != removal)
+                    .filter(l -> !l.value(RATING).equalz(noCost()))
+                    .findFirst();
+        }
+        return lineProcessing.unorderedLinesStream2()
+                .filter(l -> !l.value(RATING).equalz(noCost()))
+                .findFirst();
+    }
+
+    private Rating nextGroupRating(View lines, boolean beforeRemoval) {
+        var newSize = lines.size();
+        if (beforeRemoval) {
+            --newSize;
+        }
+        return cost(distance(targetSize, newSize));
+    }
+
+    private Rating previousGroupRating(View lines, boolean beforeRemoval) {
+        var newSize = lines.size();
+        if (!beforeRemoval) {
+            --newSize;
+        }
+        return cost(distance(targetSize, newSize));
+    }
+
+    @Override
+    public String toSimpleDescription(Line line, View groupsLineProcessing, GroupId incomingGroup) {
+        return "size should be " + targetSize + ", but is " + groupsLineProcessing.size();
+    }
+
+    @Override
+    public Class<? extends Rater> type() {
+        return HasSizeFast.class;
+    }
+
+    @Override
+    public List<Domable> arguments() {
+        return list(tree(HasSizeFast.class.getSimpleName()).withChild(tree("" + targetSize)));
+    }
+
+    @Override
+    public boolean equals(Object arg) {
+        if (arg instanceof HasSizeFast cArg) {
+            return this.targetSize == cArg.targetSize;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return CommonFunctions.hashCode(targetSize);
+    }
+
+    @Override
+    public void addContext(Discoverable context) {
+        contexts.add(context);
+    }
+
+    @Override
+    public Set<List<String>> paths() {
+        return contexts.stream().map(d -> d.path().shallowCopy()).collect(toSetOfUniques());
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ", " + targetSize;
+    }
+
+    @Override
+    public Tree toTree() {
+        return tree("has-size").withProperty("target-size", targetSize + "");
+    }
+
+    @Override public String descriptivePathName() {
+        return "has-size-of-" + targetSize;
+    }
+}
---
+++ projects/net.splitcells.gel.ext/src/main/java/net/splitcells/gel/rating/rater/HasSizeFastTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.rating.rater;
+
+import net.splitcells.dem.testing.TestSuiteI;
+import net.splitcells.gel.constraint.type.Then;
+import net.splitcells.gel.data.table.Tables;
+import org.junit.jupiter.api.Test;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.gel.rating.rater.lib.HasSizeFast.hasSizeFast;
+import static net.splitcells.gel.rating.type.Cost.cost;
+import static net.splitcells.gel.rating.type.Cost.noCost;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HasSizeFastTest extends TestSuiteI {
+    @Test
+    public void test_size_requirement_greater_than_one() {
+        final var lineSupplier = Tables.table();
+        final var testSubject = Then.then(hasSizeFast(2));
+        assertThat(testSubject.complying()).isEmpty();
+        assertThat(testSubject.defying()).isEmpty();
+        final var firstTestValue = lineSupplier.addTranslated(list());
+        final var secondTestValue = lineSupplier.addTranslated(list());
+        final var thirdTestValue = lineSupplier.addTranslated(list());
+        {
+            assertThat(testSubject.defying()).isEmpty();
+            assertThat(testSubject.complying()).isEmpty();
+            assertThat(testSubject.rating()).isEqualTo(noCost());
+        }
+        {
+            testSubject.register(firstTestValue);
+            assertThat(testSubject.defying()).hasSize(1);
+            assertThat(testSubject.complying()).isEmpty();
+            assertThat(testSubject.rating()).isEqualTo(cost(1.0));
+        }
+        {
+            testSubject.register(secondTestValue);
+            assertThat(testSubject.defying()).isEmpty();
+            assertThat(testSubject.complying()).hasSize(2);
+            assertThat(testSubject.rating()).isEqualTo(noCost());
+        }
+        {
+            testSubject.register(thirdTestValue);
+            assertThat(testSubject.defying()).hasSize(1);
+            assertThat(testSubject.complying()).hasSize(2);
+            assertThat(testSubject.rating()).isEqualTo(cost(1.0));
+        }
+        {
+            testSubject.registerBeforeRemoval(thirdTestValue);
+            assertThat(testSubject.defying()).isEmpty();
+            assertThat(testSubject.complying()).hasSize(2);
+            assertThat(testSubject.rating()).isEqualTo(noCost());
+        }
+        {
+            testSubject.registerBeforeRemoval(secondTestValue);
+            assertThat(testSubject.defying()).hasSize(1);
+            assertThat(testSubject.complying()).isEmpty();
+            assertThat(testSubject.rating()).isEqualTo(cost(1.0));
+        }
+        {
+            testSubject.registerBeforeRemoval(firstTestValue);
+            assertThat(testSubject.defying()).isEmpty();
+            assertThat(testSubject.complying()).isEmpty();
+            assertThat(testSubject.rating()).isEqualTo(noCost());
+        }
+    }
+
+    @Test
+    public void testRating() {
+        final var lineSupplier = Tables.table();
+        final var testValue = Then.then(hasSizeFast(1));
+        assertThat(testValue.complying()).isEmpty();
+        assertThat(testValue.defying()).isEmpty();
+        final var firstTestValue = lineSupplier.addTranslated(list());
+        final var secondTestValue = lineSupplier.addTranslated(list());
+        final var thirdTestVaLUE = lineSupplier.addTranslated(list());
+        {
+            assertThat(testValue.defying()).isEmpty();
+            assertThat(testValue.complying()).isEmpty();
+            assertThat(testValue.rating()).isEqualTo(noCost());
+        }
+        {
+            testValue.register(firstTestValue);
+            assertThat(testValue.defying()).isEmpty();
+            assertThat(testValue.complying()).hasSize(1);
+            assertThat(testValue.rating()).isEqualTo(noCost());
+        }
+        {
+            testValue.register(secondTestValue);
+            assertThat(testValue.defying()).hasSize(1);
+            assertThat(testValue.complying()).hasSize(1);
+            assertThat(testValue.rating()).isEqualTo(cost(1.0));
+        }
+        {
+            testValue.register(thirdTestVaLUE);
+            assertThat(testValue.defying()).hasSize(1);
+            assertThat(testValue.complying()).hasSize(2);
+            assertThat(testValue.rating()).isEqualTo(cost(2.0));
+        }
+        {
+            testValue.registerBeforeRemoval(thirdTestVaLUE);
+            assertThat(testValue.defying()).hasSize(1);
+            assertThat(testValue.complying()).hasSize(1);
+            assertThat(testValue.rating()).isEqualTo(cost(1.0));
+        }
+        {
+            testValue.registerBeforeRemoval(secondTestValue);
+            assertThat(testValue.defying()).isEmpty();
+            assertThat(testValue.complying()).hasSize(1);
+            assertThat(testValue.rating()).isEqualTo(noCost());
+        }
+        {
+            testValue.registerBeforeRemoval(firstTestValue);
+            assertThat(testValue.defying()).isEmpty();
+            assertThat(testValue.complying()).isEmpty();
+            assertThat(testValue.rating()).isEqualTo(noCost());
+        }
+    }
+}
