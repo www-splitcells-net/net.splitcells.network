319e5b5dc7b9f20e31d4ddf38b19e91c5d1c7879
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-08 08:39:55 +0200

    Test isFile #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FileSystemUnionViewTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/FileSystemUnionViewTest.java
@@ -6,6 +6,7 @@ package net.splitcells.dem.resource;
 import lombok.val;
 import net.splitcells.dem.testing.annotations.UnitTest;
 import net.splitcells.dem.testing.annotations.UnitTestFactory;
+import net.splitcells.dem.utils.StringUtils;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestFactory;
@@ -17,6 +18,7 @@ import static net.splitcells.dem.resource.FileSystemUnionView.fileSystemUnionVie
 import static net.splitcells.dem.resource.FileSystemViaMemory.fileSystemViaMemory;
 import static net.splitcells.dem.testing.Assertions.requireThrow;
 import static net.splitcells.dem.testing.TestTypes.UNIT_TEST;
+import static net.splitcells.dem.utils.StringUtils.toBytes;
 
 public class FileSystemUnionViewTest {
     @UnitTestFactory public Stream<DynamicTest> test() {
@@ -44,18 +46,16 @@ public class FileSystemUnionViewTest {
         val testDirectory = "test-directory/";
         fs1.createDirectoryPath(testDirectory);
         fs2.createDirectoryPath(testDirectory);
-        requireThrow(() -> {
-            testSubject.isDirectory(testDirectory);
-        });
+        requireThrow(() -> testSubject.isDirectory(testDirectory));
     }
-        requireThrow(() -> {
-            val fs1 = fileSystemViaMemory();
-            val fs2 = fileSystemViaMemory();
-            val testSubject = fileSystemUnionView(true, true, fs1, fs2);
-            val testDirectory = "test-directory/";
-            fs1.createDirectoryPath(testDirectory);
-            fs2.createDirectoryPath(testDirectory);
-            testSubject.isDirectory(testDirectory);
-        });
+
+    @UnitTest public void testIsFile() {
+        val fs1 = fileSystemViaMemory();
+        val fs2 = fileSystemViaMemory();
+        val testSubject = fileSystemUnionView(true, true, fs1, fs2);
+        val testFile = "test-file";
+        fs1.writeToFile(testFile, toBytes(testFile));
+        fs2.writeToFile(testFile, toBytes(testFile));
+        requireThrow(() -> testSubject.isFile(testFile));
     }
 }
