cd699f0be664f12d3966a615b144689c4cb32c98
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-13 23:15:31 +0200

    Define async option #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/EditorProcessor.java
+++ projects/net.splitcells.gel.ui/src/main/java/net/splitcells/gel/ui/editor/geal/EditorProcessor.java
@@ -1,16 +1,4 @@
-/*
- * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
- * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
 package net.splitcells.gel.ui.editor.geal;
@@ -40,6 +28,7 @@ import static net.splitcells.dem.object.Discoverable.EXPLICIT_NO_CONTEXT;
 import static net.splitcells.dem.resource.communication.log.LogMessageI.logMessage;
 import static net.splitcells.dem.testing.need.NeedsCheck.runWithCheckedNeeds;
 import static net.splitcells.dem.utils.ExecutionException.execException;
+import static net.splitcells.dem.utils.NotImplementedYet.notImplementedYet;
 import static net.splitcells.dem.utils.StringUtils.*;
 import static net.splitcells.gel.editor.Editor.editor;
 import static net.splitcells.gel.editor.EditorData.editorData;
@@ -50,6 +39,7 @@ import static net.splitcells.website.server.processor.Response.response;
 public class EditorProcessor implements Processor<Tree, Tree> {
 
     public static final String PROBLEM_DEFINITION = "Definition";
+    public static final String ASYNC = "async";
 
     public static final Trail PATH = Trail.trail("net/splitcells/gel/ui/editor/geal/form");
 
@@ -88,7 +78,12 @@ public class EditorProcessor implements Processor<Tree, Tree> {
             val dataTypes = tree(DATA_TYPES).withParent(formUpdate);
             val dataValues = tree(DATA_VALUES).withParent(formUpdate);
             val renderingTypes = tree(RENDERING_TYPES).withParent(formUpdate);
-            editor.optimize();
+            val async = request.data().namedChildren(ASYNC);
+            if (async.isEmpty() || !"true".equals(async.get(0).valueName())) {
+                editor.optimize();
+            } else {
+                throw notImplementedYet();
+            }
             editor.getTables().entrySet().forEach(e -> {
                 dataValues.withProperty(e.getKey(), e.getValue().toCSV(reportInvalidCsvData(e.getKey())));
                 dataTypes.withProperty(e.getKey(), CSV.mimeTypes());
