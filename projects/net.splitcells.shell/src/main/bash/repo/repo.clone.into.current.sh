d5022dde9114a39e2a512f5336534d3eba22157e
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-01 16:52:54 +0200

    Simplify repo.clone.into.current #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.shell/src/main/bash/repo/repo.clone.into.current.sh
+++ projects/net.splitcells.shell/src/main/bash/repo/repo.clone.into.current.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
-command.managed.execute disjunction repo.clone.into.current '$@'/$subRepo
+command.managed.execute disjunction repo.clone.into.current $@
