7d4d2b33723a90f50f86592569dabc910593b34b
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-03 15:59:02 +0200

    Create command to enable dominant config
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.shell/src/main/bash/user/user.management.dominant.enable.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+mkdir -p ~/.config/net.splitcells.network.worker/
+echo 1 > ~/.config/net.splitcells.network.worker/dominant.management
+
