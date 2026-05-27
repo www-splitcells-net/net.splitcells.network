ba4edb4a733e84ca40fd1635d76ce99898f2796e
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-27 21:47:25 +0200

    Show client side JavaScript errors #43
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.content.default/src/main/resources/html/net/splitcells/website/js/startup.js
+++ projects/net.splitcells.website.content.default/src/main/resources/html/net/splitcells/website/js/startup.js
@@ -14,5 +14,34 @@ window.onerror = function(message, source, lineno, colno, error) {
     console.log(`lineno: ${lineno}`);
     console.log(`colno: ${colno}`);
     console.log(`error: ${error}`);
+    console.log(document.readyState);
+    if (document.readyState == "complete") {
+        logError(message, source, lineno, colno, error);
+    } else {
+        window.addEventListener("load", function(event) {
+            logError(message, source, lineno, colno, error);
+        }, false);
+    }
     return true;
 };
+function logError(message, source, lineno, colno, error) {
+    var errorLogs = document.getElementsByClassName('net-splitcells-error-log');
+    for (var i = 0; i < errorLogs.length; i++) {
+        errorLogs[i].style.display = 'inherit';
+        errorLogs[i].style.visibility = 'visible';
+    }
+    var errorLogLists = document.getElementsByClassName('net-splitcells-error-log-list');
+    console.log(errorLogLists);
+    
+    var message = 'Client side JavaScript error: message = `' + message
+            + '` source = `' + source
+            + '` lineno = `' + lineno
+            + '` colno = `' + colno
+            + '` error = `' + error
+            + '`';
+    for (var i = 0; i < errorLogLists.length; i++) {
+        let item = document.createElement("li");
+        item.innerHTML = message;
+        errorLogLists[i].appendChild(item);
+    }
+}
