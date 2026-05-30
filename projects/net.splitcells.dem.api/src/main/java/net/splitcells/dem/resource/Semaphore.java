4ff2a6587c88a05974ee0e27c5f8bc0b8b1e6c1e
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-30 23:48:44 +0200

    Draft minimal Semaphore interface #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/Semaphore.java
@@ -0,0 +1,55 @@
+package net.splitcells.dem.resource;
+
+import lombok.val;
+import net.splitcells.dem.environment.config.framework.Variable;
+import net.splitcells.dem.lang.annotations.JavaLegacy;
+
+import java.util.function.Consumer;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static net.splitcells.dem.lang.tree.TreeI.tree;
+import static net.splitcells.dem.utils.ExecutionException.execException;
+
+@JavaLegacy
+public class Semaphore {
+
+    public static Semaphore semaphore() {
+        return new Semaphore(0);
+    }
+
+    public static Semaphore semaphore(int permits) {
+        return new Semaphore(permits);
+    }
+
+    private final java.util.concurrent.Semaphore impl;
+
+    private Semaphore(int permits) {
+        impl = new java.util.concurrent.Semaphore(permits, true);
+    }
+
+    public void acquire(Consumer<AutoCloseable> requestor) {
+        val lockClosed = Variable.<Boolean>create().withValue(FALSE);
+        AutoCloseable lock = new AutoCloseable() {
+            @Override public void close() throws Exception {
+                if (lockClosed.value().orElseThrow()) {
+                    throw execException(tree("Requestor requested more than one lock release.")
+                            .withProperty("requestor", requestor.toString())
+                            .withProperty("lock", this.toString()));
+                }
+                impl.release();
+            }
+        };
+        try {
+            impl.acquire();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw execException(e);
+        }
+        requestor.accept(lock);
+        if (!lockClosed.value().orElseThrow()) {
+            lockClosed.withValue(TRUE);
+            impl.release();
+        }
+    }
+}
