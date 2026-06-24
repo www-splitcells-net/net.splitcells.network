32db58daacb6fadc77cb5a0c67572b6fd69ee7be
Mārtiņš Avots <martins.avots@splitcells.net>
2026-06-24 20:04:03 +0200

    Define compact line column content #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/history/History.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/solution/history/History.java
@@ -69,8 +69,8 @@ public interface History extends Table, AfterAdditionSubscriber, BeforeRemovalSu
      */
     Attribute<Integer> EVENT_ID = attribute(Integer.class, "event-id");
     Attribute<TableEventType> EVENT_TYPE = attribute(TableEventType.class, "database-event-type");
-    Attribute<Line> DEMAND = attribute(Line.class, "demand");
-    Attribute<Line> SUPPLY = attribute(Line.class, "supply");
+    Attribute<Line> DEMAND = attribute(Line.class, "demand", d -> "" + d.index());
+    Attribute<Line> SUPPLY = attribute(Line.class, "supply", s -> "" + s.index());
     Attribute<MetaDataView> META_DATA = attribute(MetaDataView.class, "meta-data");
     String VALUE_PREFIX = "value-";
 
