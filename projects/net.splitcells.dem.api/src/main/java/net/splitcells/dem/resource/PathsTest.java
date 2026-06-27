37266330b27822fec735292af23a64fdfce75e49
Mārtiņš Avots <martins.avots@splitcells.net>
2026-06-27 16:43:01 +0200

    Fully test Paths #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/PathsTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/PathsTest.java
@@ -5,10 +5,28 @@ package net.splitcells.dem.resource;
 
 import net.splitcells.dem.testing.annotations.UnitTest;
 
+import java.nio.file.Path;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.resource.Paths.removeFileSuffix;
+import static net.splitcells.dem.testing.Assertions.requireEquals;
 import static net.splitcells.dem.testing.Assertions.requireIllegalDefaultConstructor;
 
 public class PathsTest {
     @UnitTest public void testIllegalConstructor() {
         requireIllegalDefaultConstructor(Paths.class);
+    }
+
+    @UnitTest public void testPath() {
+        requireEquals(Paths.path("test", "a" , "b"), Path.of("test/a/b"));
+        requireEquals(Paths.path(Path.of("/test"), list("a" , "b")), Path.of("/test/a/b"));
+    }
+
+    @UnitTest public void testRemoveFileSuffix() {
+        requireEquals(removeFileSuffix("test.txt"), "test");
+    }
+
+    @UnitTest public void testToString() {
+        requireEquals(Paths.toString(Path.of("\\a\\b")), "/a/b");
     }
 }
