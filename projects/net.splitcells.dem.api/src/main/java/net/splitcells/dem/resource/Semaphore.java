967badb22f5c628752b80d87cf90bebff168f2b9
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-11 17:56:13 +0200

    Note goal of Semaphore interface #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/Semaphore.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/Semaphore.java
@@ -14,6 +14,9 @@ import static java.lang.Boolean.TRUE;
 import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.utils.ExecutionException.execException;
 
+/**
+ * Provides a Semaphore interface, that is easier to use without deadlocks by default.
+ */
 @JavaLegacy
 public class Semaphore {
 
