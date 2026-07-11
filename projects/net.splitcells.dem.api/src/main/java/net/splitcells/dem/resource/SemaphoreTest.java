c3e578deffe3a7683a70ef593a8519e403bf0482
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-11 17:38:13 +0200

    Test Semaphore single permit #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/SemaphoreTest.java
+++ projects/net.splitcells.dem.api/src/main/java/net/splitcells/dem/resource/SemaphoreTest.java
@@ -5,8 +5,11 @@ package net.splitcells.dem.resource;
 
 import lombok.val;
 import net.splitcells.dem.Dem;
+import net.splitcells.dem.testing.annotations.IntegrationTest;
 import net.splitcells.dem.testing.annotations.UnitTest;
 
+import static net.splitcells.dem.data.atom.Bools.require;
+import static net.splitcells.dem.environment.config.framework.Variable.variable;
 import static net.splitcells.dem.resource.Semaphore.semaphore;
 
 public class SemaphoreTest {
@@ -19,5 +22,20 @@ public class SemaphoreTest {
         testSubject.acquire(a -> {
             // Nothing needs to be done, we just check if this test finishes at all.
         });
+    }
+
+    @IntegrationTest public void testOnePermit() {
+        val testSubject = semaphore(1);
+        val check = variable(false);
+        Dem.sleepAtLeast(1_000L);
+        Dem.executeThread(getClass().getName(), () -> testSubject.acquire(a -> {
+            Dem.sleepAtLeast(10_000L);
+            check.withValue(true);
+        }));
+        Dem.sleepAtLeast(1_000L);
+        testSubject.acquire(a -> {
+            // We wait, until the check is set to true.
+        });
+        require(check.val());
     }
 }
