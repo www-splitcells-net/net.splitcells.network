943f45e91f1ab21d206f792b77299b3d611b4523
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-14 00:26:46 +0200

    Add Maven integration test command #32
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.shell.lib/src/main/bash/shortcuts/mci.test.integration.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+mvn clean install -Dtest.groups=testing_integration
