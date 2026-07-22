e15b96e3ca2f58b2ceb5f2bd8e3cf5b04be79cb8
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-22 21:44:22 +0200

    Test integer generation #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/utils/random/RandomnessViaListTest.java
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.dem.utils.random;
+
+import lombok.val;
+import net.splitcells.dem.testing.annotations.UnitTest;
+
+import static net.splitcells.dem.data.set.list.Lists.list;
+import static net.splitcells.dem.testing.Assertions.requireEquals;
+import static net.splitcells.dem.utils.random.RandomnessViaList.randomnessViaList;
+
+public class RandomnessViaListTest {
+    @UnitTest public void testInteger() {
+        val testSubject = randomnessViaList(list(1d, 1.7d, 3.2d, 6d, 100d));
+        requireEquals(testSubject.integer(), 1);
+        requireEquals(testSubject.integer(), 2);
+        requireEquals(testSubject.integer(), 3);
+        requireEquals(testSubject.integer(1, 10), 6);
+        requireEquals(testSubject.integer(1, 10), 1);
+    }
+}
