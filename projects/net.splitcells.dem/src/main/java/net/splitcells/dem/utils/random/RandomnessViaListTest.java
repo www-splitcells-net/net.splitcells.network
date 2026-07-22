b11521ee94df837d4138f8f3c06fb689f88e1801
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-22 22:10:49 +0200

    Test random floats #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.dem/src/main/java/net/splitcells/dem/utils/random/RandomnessViaListTest.java
+++ projects/net.splitcells.dem/src/main/java/net/splitcells/dem/utils/random/RandomnessViaListTest.java
@@ -8,6 +8,7 @@ import net.splitcells.dem.testing.annotations.UnitTest;
 
 import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.testing.Assertions.requireEquals;
+import static net.splitcells.dem.utils.MathUtils.requireAcceptable;
 import static net.splitcells.dem.utils.random.RandomnessViaList.randomnessViaList;
 
 public class RandomnessViaListTest {
@@ -18,5 +19,13 @@ public class RandomnessViaListTest {
         requireEquals(testSubject.integer(), 3);
         requireEquals(testSubject.integer(1, 10), 6);
         requireEquals(testSubject.integer(1, 10), 1);
+    }
+
+    @UnitTest public void testFloat() {
+        val testSubject = randomnessViaList(list(1d, 1.7d, 3.2d, 5d));
+        requireAcceptable(testSubject.floating(1, 2), 1);
+        requireAcceptable(testSubject.floating(1, 3), 1.7);
+        requireAcceptable(testSubject.floating(1, 3), 1);
+        requireAcceptable(testSubject.floating(1, 3), 2);
     }
 }
