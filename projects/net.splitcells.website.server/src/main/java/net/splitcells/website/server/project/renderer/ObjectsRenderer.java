127bd79b29d7b72acacae4969c00695aa297f80d
Mārtiņš Avots <martins.avots@splitcells.net>
2026-07-27 23:58:44 +0200

    Remove deprecated usage #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/ObjectsRenderer.java
+++ projects/net.splitcells.website.server/src/main/java/net/splitcells/website/server/project/renderer/ObjectsRenderer.java
@@ -4,7 +4,7 @@
  */
 package net.splitcells.website.server.project.renderer;
 
-import net.splitcells.dem.environment.config.framework.OptionImpl;
+import net.splitcells.dem.environment.config.framework.Option;
 import net.splitcells.dem.lang.tree.Tree;
 
 import java.nio.file.Path;
@@ -13,10 +13,7 @@ import java.util.Optional;
 import static net.splitcells.dem.Dem.configValue;
 import static net.splitcells.website.server.project.renderer.ObjectsRendererI.objectsRenderer;
 
-public class ObjectsRenderer extends OptionImpl<ObjectsRendererI> {
-    public ObjectsRenderer() {
-        super(() -> objectsRenderer(Path.of("/")));
-    }
+public class ObjectsRenderer implements Option<ObjectsRendererI> {
 
     public static void registerObject(DiscoverableRenderer object) {
         configValue(ObjectsRenderer.class).withObject(object);
@@ -24,6 +21,10 @@ public class ObjectsRenderer extends OptionImpl<ObjectsRendererI> {
 
     public static void registerObject(CsvRenderer object) {
         configValue(ObjectsRenderer.class).withObject(object);
+    }
+
+    @Override public ObjectsRendererI defaultValue() {
+        return objectsRenderer(Path.of("/"));
     }
 
     @Override public Optional<Tree> serialize(ObjectsRendererI currentValue) {
