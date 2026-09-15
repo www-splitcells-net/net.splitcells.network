423175816ddd4df15238d6d59aae9c346675828b
Mārtiņš Avots <martins.avots@splitcells.net>
2026-09-15 17:58:52 +0200

    Add event to addition subscriber #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/AfterAdditionSubscriber.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/AfterAdditionSubscriber.java
@@ -4,14 +4,25 @@
 package net.splitcells.gel.data.table;
 
 import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
+import static net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY;
+import static net.splitcells.dem.utils.ExecutionException.execException;
 
 import net.splitcells.dem.data.set.SetT;
+import net.splitcells.dem.environment.config.StaticFlags;
+import net.splitcells.gel.data.table.history.TableEventType;
 import net.splitcells.gel.data.view.Line;
 
 @FunctionalInterface
 public interface AfterAdditionSubscriber {
 
     void registerAddition(Line addition);
+
+    default void registerAddition(TableEvent event) {
+        if (ENFORCING_UNIT_CONSISTENCY && event.getType() != TableEventType.ADDITION) {
+            throw execException("Only addition events are supported.");
+        }
+        registerAddition(event.getLine());
+    }
 
     default void registerAddition(SetT<Line> additions) {
         additions.forEach(addition -> registerAddition(addition));
