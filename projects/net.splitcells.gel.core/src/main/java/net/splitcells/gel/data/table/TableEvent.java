9d0168539d5e260da78c6e935236e5e32469a39a
Mārtiņš Avots <martins.avots@splitcells.net>
2026-09-15 17:47:03 +0200

    Declare new table event class #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/TableEvent.java
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+ */
+package net.splitcells.gel.data.table;
+
+public class TableEvent {
+    public static TableEvent tableEvent() {
+        return new TableEvent();
+    }
+    private TableEvent() {
+        
+    }
+}
