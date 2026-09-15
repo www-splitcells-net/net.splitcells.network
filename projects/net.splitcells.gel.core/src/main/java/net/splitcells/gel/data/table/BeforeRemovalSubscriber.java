1e352ba8acb59e6bf5acf4ca0a054be204fe5162
Mārtiņš Avots <martins.avots@splitcells.net>
2026-09-15 18:10:30 +0200

    Add removal event to subscriber API #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/BeforeRemovalSubscriber.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/BeforeRemovalSubscriber.java
@@ -5,11 +5,22 @@ package net.splitcells.gel.data.table;
 
 import java.util.Collection;
 
+import net.splitcells.gel.data.table.history.TableEventType;
 import net.splitcells.gel.data.view.Line;
+
+import static net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY;
+import static net.splitcells.dem.utils.ExecutionException.execException;
+import static net.splitcells.gel.data.table.history.TableEventType.REMOVAL;
 
 @FunctionalInterface
 public interface BeforeRemovalSubscriber {
 	void registerBeforeRemoval(Line removal);
+    default void registerBeforeRemoval(TableEvent event) {
+        if (ENFORCING_UNIT_CONSISTENCY && event.getType() != REMOVAL) {
+            throw execException("Only removal events are supported.");
+        }
+        registerBeforeRemoval(event.getLine());
+    }
 
 	default void registerBeforeRemoval(Collection<Line> removals) {
 		removals.forEach(line -> registerBeforeRemoval(line));
