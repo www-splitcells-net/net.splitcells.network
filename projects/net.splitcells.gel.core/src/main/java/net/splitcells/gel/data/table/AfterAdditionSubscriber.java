085d2599517720d02f74ae4c20e81007fc00fb5a
Mārtiņš Avots <martins.avots@splitcells.net>
2026-09-15 18:09:38 +0200

    Format code #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/AfterAdditionSubscriber.java
+++ projects/net.splitcells.gel.core/src/main/java/net/splitcells/gel/data/table/AfterAdditionSubscriber.java
@@ -6,6 +6,7 @@ package net.splitcells.gel.data.table;
 import static net.splitcells.dem.data.set.list.Lists.listWithValuesOf;
 import static net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY;
 import static net.splitcells.dem.utils.ExecutionException.execException;
+import static net.splitcells.gel.data.table.history.TableEventType.ADDITION;
 
 import net.splitcells.dem.data.set.SetT;
 import net.splitcells.dem.environment.config.StaticFlags;
@@ -18,7 +19,7 @@ public interface AfterAdditionSubscriber {
     void registerAddition(Line addition);
 
     default void registerAddition(TableEvent event) {
-        if (ENFORCING_UNIT_CONSISTENCY && event.getType() != TableEventType.ADDITION) {
+        if (ENFORCING_UNIT_CONSISTENCY && event.getType() != ADDITION) {
             throw execException("Only addition events are supported.");
         }
         registerAddition(event.getLine());
