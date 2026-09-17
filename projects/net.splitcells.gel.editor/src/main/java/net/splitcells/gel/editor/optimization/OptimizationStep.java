4c327463711c49b92eba96abb4a14c6e4f6a7284
Mārtiņš Avots <martins.avots@splitcells.net>
2026-09-17 18:57:05 +0200

    Note intent of OptimizationStep #37
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/OptimizationStep.java
+++ projects/net.splitcells.gel.editor/src/main/java/net/splitcells/gel/editor/optimization/OptimizationStep.java
@@ -5,12 +5,18 @@
 package net.splitcells.gel.editor.optimization;
 
 import net.splitcells.dem.lang.tree.Tree;
+import net.splitcells.gel.solution.optimization.OnlineOptimization;
 
 import java.util.Optional;
 
 /**
- * This API defines a step in an optimization chain and points to the next step after this one is done.
- * This is primarily used, in order to pause and generate status info about the progress. 
+ * <p>This API defines a step in an optimization chain and points to the next step after this one is done.
+ * This is primarily used, in order to pause and generate status info about the progress.</p>
+ * <p>Do not see this as a pure optimization API, but more as an execution control and report.
+ * It is preferred to implement this as a wrapper of {@link OnlineOptimization},
+ * in order to avoid duplicate optimization implementations.</p>
+ * <p>TODO Extend this interface to persist paused optimization,
+ * This also can be used to distribute optimization tasks to nodes via a command and state repo.</p>
  */
 public interface OptimizationStep {
     Optional<OptimizationStep> runAndProvideNextStep();
